e95a1fa1f5b85986248e57339e3f58342c32a940
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-12-19 14:35:57 -0800

    review feedback: - removed debugging helper

--- web/app/test/testHelpers.jsx
+++ web/app/test/testHelpers.jsx
@@ -3,15 +3,6 @@ import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Route, Router } from 'react-router';
 
-export function printStack() {
-  let e = new Error('dummy');
-  let stack = e.stack.replace(/^[^(]+?[\n$]/gm, '')
-    .replace(/^\s+at\s+/gm, '')
-    .replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
-    .split('\n');
-  console.log(stack);
-}
-
 export function routerWrap(Component, extraProps={}, route="/", currentLoc="/") {
   return (
     <Router history={createMemoryHistory(currentLoc)} createElement={(Component, props) => <Component {...(_.merge({}, props, extraProps))} />}>
