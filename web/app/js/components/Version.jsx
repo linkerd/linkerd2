449f306aeba16c2b8ef8dbc9faaed9262863d563
Andrew Seigner <andrew@sig.gy>
2018-01-02 16:07:50 -0800

    Implement client-side version checking (#79)
    Previously Conduit would render an iframe, received from
    versioncheck.conduit.io.
    
    Modify the client to retrieve the latest released version, via CORS.
    
    Signed-off-by: Andrew Seigner <andrew@sig.gy>

---
+++ web/app/js/components/Version.jsx
@@ -0,0 +1,80 @@
+import { ApiHelpers } from './util/ApiHelpers.js';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import './../../css/version.css';
+
+export default class Version extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.loadFromServer = this.loadFromServer.bind(this);
+    this.handleApiError = this.handleApiError.bind(this);
+    this.state = {
+      error: null,
+      latest: null,
+      loaded: false,
+      pendingRequests: false
+    };
+  }
+
+  componentDidMount() {
+    this.loadFromServer();
+  }
+
+  loadFromServer() {
+    if (this.state.pendingRequests) {
+      return; // don't make more requests if the ones we sent haven't completed
+    }
+    this.setState({ pendingRequests: true });
+
+    let versionUrl = `https://versioncheck.conduit.io/version.json?version=${this.props.releaseVersion}?uuid=${this.props.uuid}`;
+    let versionFetch = ApiHelpers("").fetch(versionUrl);
+    // expose serverPromise for testing
+    this.serverPromise = Promise.all([versionFetch])
+      .then(([resp]) => {
+        this.setState({
+          latest: resp.version,
+          loaded: true,
+          pendingRequests: false,
+        });
+      }).catch(this.handleApiError);
+  }
+
+  handleApiError(e) {
+    this.setState({
+      loaded: true,
+      pendingRequests: false,
+      error: e
+    });
+  }
+
+  renderVersionCheck() {
+    if (!this.state.loaded || this.state.pendingRequests) {
+      return "Performing version check...";
+    }
+
+    if (!this.state.latest) {
+      return (<div>
+        Version check failed
+        {this.state.error ? ": "+this.state.error : null}
+      </div>);
+    } else if (this.state.latest === this.props.releaseVersion) {
+      return "Conduit is up to date";
+    } else {
+      return (<div>
+        A new version ({this.state.latest}) is available<br />
+        <Link to="https://versioncheck.conduit.io/update" className="button primary" target="_blank">Update Now</Link>
+      </div>);
+    }
+  }
+
+  render() {
+    return (
+      <div className="version">
+        Currently running Conduit {this.props.releaseVersion}<br />
+        {this.renderVersionCheck()}
+      </div>
+    );
+  }
+
+}
---
+++ web/app/test/VersionTest.jsx
@@ -0,0 +1,103 @@
+import { BrowserRouter } from 'react-router-dom';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+import sinonStubPromise from 'sinon-stub-promise';
+import Version from '../js/components/Version.jsx';
+
+sinonStubPromise(sinon);
+
+describe('Version', () => {
+  let curVer = "v1.2.3";
+  let newVer = "v2.3.4";
+
+  let component, fetchStub;
+
+  function withPromise(fn) {
+    return component.find("Version").get(0).serverPromise.then(fn);
+  }
+
+  beforeEach(() => {
+    fetchStub = sinon.stub(window, 'fetch');
+  });
+
+  afterEach(() => {
+    window.fetch.restore();
+  });
+
+  it('renders initial loading message', () => {
+    fetchStub.returnsPromise().resolves({
+      ok: true,
+    });
+
+    component = mount(
+      <BrowserRouter>
+        <Version />
+      </BrowserRouter>
+    );
+
+    expect(component.find("Version")).to.have.length(1);
+    expect(component.html()).to.include("Performing version check...");
+  });
+
+  it('renders up to date message when versions match', () => {
+    fetchStub.returnsPromise().resolves({
+      ok: true,
+      json: () => Promise.resolve({ version: curVer })
+    });
+
+    component = mount(
+      <BrowserRouter>
+        <Version
+          releaseVersion={curVer}
+          uuid="fakeuuid" />
+      </BrowserRouter>
+    );
+
+    return withPromise(() => {
+      expect(component.html()).to.include("Conduit is up to date");
+    });
+  });
+
+  it('renders update message when versions do not match', () => {
+    fetchStub.returnsPromise().resolves({
+      ok: true,
+      json: () => Promise.resolve({ version: newVer })
+    });
+
+    component = mount(
+      <BrowserRouter>
+        <Version
+          releaseVersion={curVer}
+          uuid="fakeuuid" />
+      </BrowserRouter>
+    );
+
+    return withPromise(() => {
+      expect(component.html()).to.include("A new version (");
+      expect(component.html()).to.include(newVer);
+      expect(component.html()).to.include(") is available");
+    });
+  });
+
+  it('renders error when version check fails', () => {
+    let errMsg = "Fake error";
+
+    fetchStub.returnsPromise().resolves({
+      ok: false,
+      statusText: errMsg
+    });
+
+    component = mount(
+      <BrowserRouter>
+        <Version />
+      </BrowserRouter>
+    );
+
+    return withPromise(() => {
+      expect(component.html()).to.include("Version check failed");
+      expect(component.html()).to.include(errMsg);
+    });
+  });
+});
