1d1b3c5cd410334c80cdeeb0d1a9d2fb36f6fd84
Andrew Seigner <andrew@sig.gy>
2018-01-10 11:14:41 -0800

    Fail the build if dep images are out of sync. (#123)
    Previously if dependencies changed but dep image SHAs were not updated,
    the build could succeed, creating docker images with indeterminate
    dependencies.
    
    This change checks the dependency image SHAs hard-coded in Dockerfile's
    against the current source tree. If the SHAs do not match, the build
    fails.
    
    Fixes #118
    
    Signed-off-by: Andrew Seigner <andrew@sig.gy>

--- bin/_tag.sh
+++ bin/_tag.sh
@@ -31,3 +31,38 @@ clean_head_root_tag() {
 master_root_tag() {
     echo "git-$(git_sha master)"
 }
+
+validate_tag() {
+    file="$1"
+    shift
+
+    image="$1"
+    shift
+
+    sha="$1"
+    shift
+
+    dockerfile_tag=$(grep -oe $image':[^ ]*' $file) || true
+    deps_tag="$image:$sha"
+    if [ "$dockerfile_tag" != "" ] && [ "$dockerfile_tag" != "$deps_tag" ]; then
+        echo "Tag in "$file" does not match source tree:"
+        echo $dockerfile_tag" ("$file")"
+        echo $deps_tag" (source)"
+        exit 3
+    fi
+}
+
+# These functions should be called by any docker-build-* script that relies on
+# Go or Rust dependencies. To confirm the set of scripts that should call this
+# function, run:
+# $ grep -ER 'docker-build-(go|proxy)-deps' .
+
+validate_go_deps_tag() {
+    file="$1"
+    validate_tag "$file" "gcr.io/runconduit/go-deps" "$(go_deps_sha)"
+}
+
+validate_proxy_deps_tag() {
+    file="$1"
+    validate_tag "$file" "gcr.io/runconduit/proxy-deps" "$(proxy_deps_sha)"
+}
