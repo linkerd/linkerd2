6b18b330b85b2c83bed0927a4dc06233fea29f5c
Dennis Adjei-Baah <dennis@buoyant.io>
2018-01-11 10:43:04 -0800

    change a few tests to be more descriptive with clear intent
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- pkg/k8s/api_test.go
+++ pkg/k8s/api_test.go
@@ -8,59 +8,45 @@ import (
 	"net/url"
 )
 
-type TestFixture struct{
-	testInput string
-	expectedOutput string
-}
 func TestKubernetesApiUrlFor(t *testing.T) {
 	const namespace = "some-namespace"
 	const extraPath = "/some/extra/path"
-	testData := []TestFixture{
-		{
-			testInput: "https://35.184.231.31",
-			expectedOutput:  fmt.Sprintf("https://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-		},
-		{
-			testInput: "35.184.231.31",
-			expectedOutput:  fmt.Sprintf("https://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-		},
-		{
-			testInput: "http://35.184.231.31",
-			expectedOutput:  fmt.Sprintf("http://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-		},
-	}
 
 	t.Run("Returns URL from base URL overridden in construction", func(t *testing.T) {
-		for _, data := range testData {
-			actualUrl := generateURL(data, t, namespace, extraPath)
-			if actualUrl.String() != data.expectedOutput {
-				t.Fatalf("Expected generated URL to be [%s], but got [%s]", data.expectedOutput, actualUrl.String())
+		testData := map[string]string{
+			"https://35.184.231.31": fmt.Sprintf("https://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
+			"http://35.184.231.31":  fmt.Sprintf("http://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
+		}
+		for testInput, expected := range testData {
+			actualUrl := generateURL(testInput, t, namespace, extraPath)
+			if actualUrl.String() != expected {
+				t.Fatalf("Expected generated URL to be [%s], but got [%s]", expected, actualUrl.String())
 			}
 		}
 	})
 
-	t.Run("Returns unmodified URL from base URL overridden in construction", func(t *testing.T){
-		/* NewK8sAPI is agnostic to the scheme used for the override API. It is only contains code that conveniently
-		adds the default scheme used to connect with the k8s API (https). Supplying a URL with a scheme other than https
-		should return an unmodified override URL */
-		testData := []TestFixture {
-			{
-				testInput: "htp://35.184.231.31",
-				expectedOutput:  fmt.Sprintf("htp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-			},
-			{
-				testInput: "tcp://35.184.231.31",
-				expectedOutput:  fmt.Sprintf("tcp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-			},
-			{
-				testInput: "htttpp://35.184.231.31",
-				expectedOutput:  fmt.Sprintf("htttpp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
-			},
+	t.Run("Return URL with prepended scheme from a base URL with no scheme specified", func(t *testing.T) {
+		testData := map[string]string{
+			"35.184.231.31": fmt.Sprintf("https://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
 		}
-		for _, data := range testData {
-			actualUrl := generateURL(data, t, namespace, extraPath)
-			if actualUrl.String() != data.expectedOutput {
-				t.Fatalf("Expected generated URL to be [%s], but got [%s]", data.expectedOutput, actualUrl.String())
+		for testInput, expected := range testData {
+			actualUrl := generateURL(testInput, t, namespace, extraPath)
+			if actualUrl.String() != expected {
+				t.Fatalf("Expected generated URL to be [%s], but got [%s]", expected, actualUrl.String())
+			}
+		}
+	})
+
+	t.Run("Return unmodified URL from a base URL with an existing scheme", func(t *testing.T) {
+		testData := map[string]string{
+			"htp://35.184.231.31":    fmt.Sprintf("htp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
+			"tcp://35.184.231.31":    fmt.Sprintf("tcp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
+			"htttpp://35.184.231.31": fmt.Sprintf("htttpp://35.184.231.31/api/v1/namespaces/%s%s", namespace, extraPath),
+		}
+		for testInput, expected := range testData {
+			actualUrl := generateURL(testInput, t, namespace, extraPath)
+			if actualUrl.String() != expected {
+				t.Fatalf("Expected generated URL to be [%s], but got [%s]", expected, actualUrl.String())
 			}
 
 		}
@@ -76,8 +62,9 @@ func TestKubernetesApiUrlFor(t *testing.T) {
 		}
 	})
 }
-func generateURL(data TestFixture, t *testing.T, namespace string, extraPath string) *url.URL {
-	api, err := NewK8sAPI(shell.NewUnixShell(), "testdata/config.test", data.testInput)
+
+func generateURL(hostPort string, t *testing.T, namespace string, extraPath string) *url.URL {
+	api, err := NewK8sAPI(shell.NewUnixShell(), "testdata/config.test", hostPort)
 	if err != nil {
 		t.Fatalf("Unexpected error starting proxy: %v", err)
 	}
