971eecade640984453649f5adf2efcb1082b08ef
Kevin Lingerfelt <kl@buoyant.io>
2018-01-25 16:35:31 -0800

    Rename git_sha => git_sha_head
    Signed-off-by: Kevin Lingerfelt <kl@buoyant.io>

--- bin/_tag.sh
+++ bin/_tag.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-git_sha() {
-    git rev-parse "$1" | cut -c 1-8
+git_sha_head() {
+    git rev-parse HEAD | cut -c 1-8
 }
 
 proxy_deps_sha() {
@@ -19,14 +19,14 @@ clean_head() {
 }
 
 named_tag() {
-    echo "$(git name-rev --tags --name-only $(git_sha HEAD))"
+    echo "$(git name-rev --tags --name-only $(git_sha_head))"
 }
 
 head_root_tag() {
     if clean_head ; then
         clean_head_root_tag
     else
-        echo "dev-$(git_sha HEAD)-$USER"
+        echo "dev-$(git_sha_head)-$USER"
     fi
 }
 
@@ -35,7 +35,7 @@ clean_head_root_tag() {
         if [ "$(named_tag)" != "undefined" ]; then
             echo "$(named_tag)"
         else
-            echo "git-$(git_sha HEAD)"
+            echo "git-$(git_sha_head)"
         fi
     else
         echo "Commit unstaged changes." >&2
