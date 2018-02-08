c9a9db45d9469bb9ebc90b03869cdb8dc714afe7
Eliza Weisman <eliza@buoyant.io>
2018-02-08 13:18:12 -0800

    Remove entire pods list from deployment detail page
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- web/app/js/components/DeploymentDetail.jsx
+++ web/app/js/components/DeploymentDetail.jsx
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import BarChart from './BarChart.jsx';
 import ConduitSpinner from "./ConduitSpinner.jsx";
 import ErrorBanner from './ErrorBanner.jsx';
 import { incompleteMeshMessage } from './util/CopyUtils.jsx';
@@ -12,7 +11,7 @@ import { rowGutter } from './util/Utils.js';
 import TabbedMetricsTable from './TabbedMetricsTable.jsx';
 import UpstreamDownstream from './UpstreamDownstream.jsx';
 import { Col, Row } from 'antd';
-import { emptyMetric, getPodsByDeployment, processRollupMetrics, processTimeseriesMetrics } from './util/MetricUtils.js';
+import { emptyMetric, processRollupMetrics, processTimeseriesMetrics } from './util/MetricUtils.js';
 import './../../css/deployment.css';
 import 'whatwg-fetch';
 
@@ -51,7 +50,7 @@ export default class DeploymentDetail extends React.Component {
       pollingInterval: 10000,
       deploy: deployment,
       metrics: [],
-      pods: [],
+      // pods: [],
       upstreamMetrics: [],
       downstreamMetrics: [],
       pathMetrics: [],
@@ -70,35 +69,24 @@ export default class DeploymentDetail extends React.Component {
     let urls = this.api.urlsForResource;
 
     let deployMetricsUrl = urls["deployment"].url(this.state.deploy).ts;
-    let podRollupUrl = urls["pod"].url(this.state.deploy).rollup;
     let upstreamRollupUrl = urls["upstream_deployment"].url(this.state.deploy).rollup;
     let downstreamRollupUrl = urls["downstream_deployment"].url(this.state.deploy).rollup;
     let pathMetricsUrl = urls["path"].url(this.state.deploy).rollup;
 
     let deployFetch = this.api.fetchMetrics(deployMetricsUrl);
-    let podListFetch = this.api.fetchPods();
-    let podRollupFetch = this.api.fetchMetrics(podRollupUrl);
     let upstreamFetch = this.api.fetchMetrics(upstreamRollupUrl);
     let downstreamFetch = this.api.fetchMetrics(downstreamRollupUrl);
     let pathsFetch = this.api.fetchMetrics(pathMetricsUrl);
 
     // expose serverPromise for testing
-    this.serverPromise = Promise.all([deployFetch, podRollupFetch, upstreamFetch, downstreamFetch, podListFetch, pathsFetch])
-      .then(([deployMetrics, podRollup, upstreamRollup, downstreamRollup, podList, paths]) => {
+    this.serverPromise = Promise.all([deployFetch, upstreamFetch, downstreamFetch, pathsFetch])
+      .then(([deployMetrics,upstreamRollup, downstreamRollup, paths]) => {
         let tsByDeploy = processTimeseriesMetrics(deployMetrics.metrics, "targetDeploy");
-        let podMetrics = processRollupMetrics(podRollup.metrics, "targetPod");
         let upstreamMetrics = processRollupMetrics(upstreamRollup.metrics, "sourceDeploy");
         let downstreamMetrics = processRollupMetrics(downstreamRollup.metrics, "targetDeploy");
         let pathMetrics = processRollupMetrics(paths.metrics, "path");
 
-        let deploy = _.find(getPodsByDeployment(podList.pods), ["name", this.state.deploy]);
-        let totalRequestRate = _.sumBy(podMetrics, "requestRate");
-        _.each(podMetrics, datum => datum.totalRequests = totalRequestRate);
-
         this.setState({
-          metrics: podMetrics,
-          pods: deploy.pods,
-          added: deploy.added,
           deployTs: _.get(tsByDeploy, this.state.deploy, {}),
           upstreamMetrics: upstreamMetrics,
           downstreamMetrics: downstreamMetrics,
@@ -160,39 +148,15 @@ export default class DeploymentDetail extends React.Component {
   }
 
   renderMidsection() {
-    let podTableData = this.state.metrics;
-    if (_.isEmpty(this.state.metrics)) {
-      podTableData = _.map(this.state.pods, po => emptyMetric(po.name));
-    }
 
     return (
       <Fragment key="deployment-pod-summary">
         <Row gutter={rowGutter}>
-          <Col span={16}>
-            <div className="pod-summary">
-              <div className="border-container border-neutral subsection-header">
-                <div className="border-container-content subsection-header">Pod summary</div>
-              </div>
-              {
-                _.isEmpty(this.state.metrics) ? null :
-                  <div className="pod-distribution-chart">
-                    <div className="bar-chart-title">
-                      <div>Request load by pod</div>
-                      <div className="bar-chart-tooltip" />
-                    </div>
-                    <BarChart
-                      data={this.state.metrics}
-                      lastUpdated={this.state.lastUpdated}
-                      containerClassName="pod-distribution-chart" />
-                  </div>
-              }
-            </div>
-          </Col>
+
           <Col span={8}>
             <div className="border-container border-neutral deployment-details">
               <div className="border-container-content">
                 <div className=" subsection-header">Deployment details</div>
-                <Metric title="Pods" value={_.size(podTableData)} />
                 <Metric title="Upstream deployments" value={this.numUpstreams()} />
                 <Metric title="Downstream deployments" value={this.numDownstreams()} />
               </div>
@@ -200,15 +164,6 @@ export default class DeploymentDetail extends React.Component {
           </Col>
         </Row>
 
-        <Row>
-          <Col span={24}>
-            <TabbedMetricsTable
-              resource="pod"
-              resourceName={this.state.deploy}
-              metrics={podTableData}
-              api={this.api} />
-          </Col>
-        </Row>
       </Fragment>
     );
   }
