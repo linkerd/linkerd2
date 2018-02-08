c8243df05402452b739ab10145c900712342ee3f
Eliza Weisman <eliza@buoyant.io>
2018-02-08 11:15:26 -0800

    Remove link to pods in TabbedMetricsTable
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- web/app/js/components/TabbedMetricsTable.jsx
+++ web/app/js/components/TabbedMetricsTable.jsx
@@ -11,10 +11,10 @@ import { Table } from 'antd';
 const resourceInfo = {
   "upstream_deployment": { title: "deployment", url: "/deployment?deploy=" },
   "downstream_deployment": { title: "deployment", url: "/deployment?deploy=" },
-  "upstream_pod": { title: "upstream pod", url: "/pod?pod=" },
-  "downstream_pod": { title: "downstream pod", url: "/pod?pod=" },
+  "upstream_pod": { title: "upstream pod", url: null },
+  "downstream_pod": { title: "downstream pod", url: null },
   "deployment": { title: "deployment", url: "/deployment?deploy=" },
-  "pod": { title: "pod", url: "/pod?pod=" },
+  "pod": { title: "pod", url: null },
   "path": { title: "path", url: null }
 };
 
