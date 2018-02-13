65b216b0ebf9a3c3e2a58637b998fd30a66abd56
Brian Smith <brian@briansmith.org>
2018-02-12 14:49:12 -1000

    WIP: Cross-compile Go executables locally, not in Docker.
    Avoid all the complexity of the go-deps docker image by doing the
    (cross-)compilation to Linux locally. This will reuse the results of
    the `dep ensure` that everybody already runs after pulling changes
    from Git. When Go 1.10 is used then its build cache will cache
    dependencies in a much finer-grained way than we got with the
    go-deps-based building. Since most Conduit contributors have to run
    Linux-taretted Docker builds in a VM, this combination of using the
    native Go execuitable + the Go 1.10 build cache should result in
    substantially faster incremental rebuilds of these containers.
    
    Expand .dockerignore so that we send even fewer files to Docker
    during the build, which speeds thigns up further.
    
    Next we can split the components of the controller into multiple
    containers to further improve incremental build performance when
    only a single comnponent of the controller is being modified.
    
    Further, this is a step towards using lighter and faster container
    building tools (e.g. Smith or Source-to-Image).
    
    This should work just fine even for Go releases prior to 1.10, however
    `go build` won't be incremental in those older versions.
    
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- bin/_tag.sh
+++ bin/_tag.sh
@@ -6,10 +6,6 @@ git_sha_head() {
     git rev-parse --short=8 HEAD
 }
 
-go_deps_sha() {
-    cat Gopkg.lock Dockerfile-go-deps | shasum - | awk '{print $1}' |cut -c 1-8
-}
-
 clean_head() {
     git diff-index --quiet HEAD --
 }
@@ -37,34 +33,4 @@ clean_head_root_tag() {
         echo "Commit unstaged changes." >&2
         exit 3
     fi
-}
-
-validate_tag() {
-    file="$1"
-    shift
-
-    image="$1"
-    shift
-
-    sha="$1"
-    shift
-
-    dockerfile_tag=$(grep -oe $image':[^ ]*' $file) || true
-    deps_tag="$image:$sha"
-    if [ "$dockerfile_tag" != "" ] && [ "$dockerfile_tag" != "$deps_tag" ]; then
-        echo "Tag in "$file" does not match source tree:"
-        echo $dockerfile_tag" ("$file")"
-        echo $deps_tag" (source)"
-        return 3
-    fi
-}
-
-# These functions should be called by any docker-build-* script that relies on
-# Go or Rust dependencies. To confirm the set of scripts that should call this
-# function, run:
-# $ grep -ER 'docker-build-go-deps' .
-
-validate_go_deps_tag() {
-    file="$1"
-    validate_tag "$file" "gcr.io/runconduit/go-deps" "$(go_deps_sha)"
 }
