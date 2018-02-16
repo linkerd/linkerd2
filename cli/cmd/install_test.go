8835030ee749f3e05a43fe13e6ff3510d387013c
Brian Smith <brian@briansmith.org>
2018-02-16 09:22:50 -1000

    B/inject proxy into control plane (#371)
    * Simplify install test.
    
    Signed-off-by: Brian Smith <brian@briansmith.org>
    
    * Restore previous name of install_output.golden.
    
    Signed-off-by: Brian Smith <brian@briansmith.org>
    
    * Use naming convention more like existing practice for test files.
    
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- cli/cmd/install_test.go
+++ cli/cmd/install_test.go
@@ -10,13 +10,11 @@ import (
 
 func TestRender(t *testing.T) {
 	t.Run("Should render an install config", func(t *testing.T) {
-		goldenFileBytes, err := ioutil.ReadFile("testdata/render_install_output.golden")
+		goldenFileBytes, err := ioutil.ReadFile("testdata/install_output.golden")
 		if err != nil {
 			t.Fatalf("Unexpected error: %v", err)
 		}
 		expectedContent := string(goldenFileBytes)
-
-		var buf bytes.Buffer
 
 		config := installConfig{
 			Namespace:                "Namespace",
@@ -34,46 +32,11 @@ func TestRender(t *testing.T) {
 			CreatedByAnnotation:      "CreatedByAnnotation",
 		}
 
-		err = render(config, &buf)
-		if err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		content := buf.String()
-
-		if content != expectedContent {
-			dmp := diffmatchpatch.New()
-			diffs := dmp.DiffMain(content, expectedContent, true)
-			patches := dmp.PatchMake(expectedContent, diffs)
-			patchText := dmp.PatchToText(patches)
-			t.Fatalf("Unexpected output:\n%+v", patchText)
-		}
-	})
-}
-
-func TestConduitInstall(t *testing.T) {
-	t.Run("Test conduit install command to have proxy injected", func(t *testing.T) {
-		goldenFileBytes, err := ioutil.ReadFile("testdata/TestConduitInstall_install_output.golden")
-		if err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		expectedContent := string(goldenFileBytes)
-		config := installConfig{
-			Namespace:                "Namespace",
-			ControllerImage:          "ControllerImage",
-			WebImage:                 "WebImage",
-			PrometheusImage:          "PrometheusImage",
-			ControllerReplicas:       1,
-			WebReplicas:              2,
-			PrometheusReplicas:       3,
-			ImagePullPolicy:          "ImagePullPolicy",
-			UUID:                     "UUID",
-			CliVersion:               "CliVersion",
-			ControllerLogLevel:       "ControllerLogLevel",
-			ControllerComponentLabel: "ControllerComponentLabel",
-			CreatedByAnnotation:      "CreatedByAnnotation",
-		}
 		buf := new(bytes.Buffer)
-		renderWithInjectYAML(config, buf)
+		err = render(config, buf)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
 		content := buf.String()
 
 		if content != expectedContent {
