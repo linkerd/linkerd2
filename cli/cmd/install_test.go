2ea4e0c056d8954f40c67d87cce19c2e201cbc42
Brian Smith <brian@briansmith.org>
2018-02-16 10:41:46 -1000

    Refactor `conduit install` test into a data-driven test
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- cli/cmd/install_test.go
+++ cli/cmd/install_test.go
@@ -2,42 +2,56 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
+
 	"io/ioutil"
 	"testing"
 )
 
 func TestRender(t *testing.T) {
-	t.Run("Should render an install config", func(t *testing.T) {
-		goldenFileBytes, err := ioutil.ReadFile("testdata/install_output.golden")
-		if err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		expectedContent := string(goldenFileBytes)
+	// A configuration that shows that all config setting strings are honored
+	// by `render()`.
+	metaConfig := installConfig{
+		Namespace:                "Namespace",
+		ControllerImage:          "ControllerImage",
+		WebImage:                 "WebImage",
+		PrometheusImage:          "PrometheusImage",
+		ControllerReplicas:       1,
+		WebReplicas:              2,
+		PrometheusReplicas:       3,
+		ImagePullPolicy:          "ImagePullPolicy",
+		UUID:                     "UUID",
+		CliVersion:               "CliVersion",
+		ControllerLogLevel:       "ControllerLogLevel",
+		ControllerComponentLabel: "ControllerComponentLabel",
+		CreatedByAnnotation:      "CreatedByAnnotation",
+	}
 
-		testNamespace := "Namespace"
-		controlPlaneNamespace = testNamespace // Set control plane namespace to enforce localhost DNS override.
-		config := installConfig{
-			Namespace:                testNamespace,
-			ControllerImage:          "ControllerImage",
-			WebImage:                 "WebImage",
-			PrometheusImage:          "PrometheusImage",
-			ControllerReplicas:       1,
-			WebReplicas:              2,
-			PrometheusReplicas:       3,
-			ImagePullPolicy:          "ImagePullPolicy",
-			UUID:                     "UUID",
-			CliVersion:               "CliVersion",
-			ControllerLogLevel:       "ControllerLogLevel",
-			ControllerComponentLabel: "ControllerComponentLabel",
-			CreatedByAnnotation:      "CreatedByAnnotation",
-		}
+	testCases := []struct {
+		config                installConfig
+		controlPlaneNamespace string
+		goldenFileName        string
+	}{
+		{metaConfig, metaConfig.Namespace, "testdata/install_output.golden"},
+	}
 
-		buf := &bytes.Buffer{}
-		err = render(config, buf)
-		if err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-		content := buf.String()
-		diffCompare(t, content, expectedContent)
-	})
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("%d: %s", i, tc.goldenFileName), func(t *testing.T) {
+			controlPlaneNamespace = tc.controlPlaneNamespace
+
+			var buf bytes.Buffer
+			err := render(tc.config, &buf)
+			if err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+			content := buf.String()
+
+			goldenFileBytes, err := ioutil.ReadFile(tc.goldenFileName)
+			if err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+			expectedContent := string(goldenFileBytes)
+			diffCompare(t, content, expectedContent)
+		})
+	}
 }
