1fef95a166a02e381a39fb2f4615663770599cf7
Brian Smith <brian@briansmith.org>
2018-04-05 14:42:02 -1000

    Fix egress_test.go
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- test/egress/egress_test.go
+++ test/egress/egress_test.go
@@ -33,6 +33,39 @@ func TestEgressHttp(t *testing.T) {
 
 	prefixedNs := TestHelper.GetTestNamespace("egress-test")
 	out, err = TestHelper.KubectlApply(out, prefixedNs)
+
+	func test(serviceName, protocolToUse, methodToUse string) {
+		testName := fmt.Sprintf("Can use egress to send %s request to %s (%s)", methodToUse, protocolToUse, serviceName)
+		t.Run(testName, func(t *testing.T) {
+			expectedURL := fmt.Sprintf("%s://www.httpbin.org/%s", protocolToUse, strings.ToLower(methodToUse))
+
+			svcURL, err := TestHelper.GetURLForService(prefixedNs, serviceName)
+			if err != nil {
+				t.Fatalf("Failed to get service URL: %v", err)
+			}
+
+			output, err := TestHelper.HTTPGetURL(svcURL)
+			if err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+			var jsonResponse map[string]interface{}
+			json.Unmarshal([]byte(output), &jsonResponse)
+
+			payloadText := jsonResponse["payload"]
+			if payloadText == nil {
+				t.Fatalf("Expected [%s] request to [%s] to return a payload, got nil. Response:\n%s\n", methodToUse, expectedURL, output)
+			}
+
+			var messagePayload map[string]interface{}
+			json.Unmarshal([]byte(payloadText.(string)), &messagePayload)
+
+			actualURL := messagePayload["url"]
+			if actualURL != expectedURL {
+				t.Fatalf("Expecting response to say egress sent [%s] request to URL [%s] but got [%s]. Response:\n%s\n", methodToUse, expectedURL, actualURL, output)
+			}
+		})
+
+	}
 	if err != nil {
 		t.Fatalf("Unexpected error: %v output:\n%s", err, out)
 	}
@@ -40,52 +73,11 @@ func TestEgressHttp(t *testing.T) {
 	methods := []string{"GET", "POST"}
 	for _, protocolToUse := range supportedProtocols {
 		for _, methodToUse := range methods {
-
-			testName := fmt.Sprintf("Can use egress to send %s request to %s", methodToUse, protocolToUse)
-			t.Run(testName, func(t *testing.T) {
-				expectedURL := fmt.Sprintf("%s://www.httpbin.org/%s", protocolToUse, strings.ToLower(methodToUse))
-				serviceName := fmt.Sprintf("egress-test-%s-%s-svc", protocolToUse, strings.ToLower(methodToUse))
-
-				svcURL, err := TestHelper.GetURLForService(prefixedNs, serviceName)
-				if err != nil {
-					t.Fatalf("Failed to get service URL: %v", err)
-				}
-
-				output, err := TestHelper.HTTPGetURL(svcURL)
-				if err != nil {
-					t.Fatalf("Unexpected error: %v", err)
-				}
-				var jsonResponse map[string]interface{}
-				json.Unmarshal([]byte(output), &jsonResponse)
-
-				payloadText := jsonResponse["payload"]
-				if payloadText == nil {
-					t.Fatalf("Expected [%s] request to [%s] to return a payload, got nil. Response:\n%s\n", methodToUse, expectedURL, output)
-				}
-
-				var messagePayload map[string]interface{}
-				json.Unmarshal([]byte(payloadText.(string)), &messagePayload)
-
-				actualURL := messagePayload["url"]
-				if actualURL != expectedURL {
-					t.Fatalf("Expecting response to say egress sent [%s] request to URL [%s] but got [%s]. Response:\n%s\n", methodToUse, expectedURL, actualURL, output)
-				}
-			})
+			serviceName := fmt.Sprintf("egress-test-%s-%s-svc", protocolToUse, strings.ToLower(methodToUse))
+			test(serviceName, protocolToUse, methodToUse)
 		}
 	}
 
-	t.Run("Can access egress for domains with fewer than 3 segments", func(t *testing.T) {
-		serviceName := "egress-test-not-www-get-svc"
-
-		svcURL, err := TestHelper.GetURLForService(prefixedNs, serviceName)
-		if err != nil {
-			t.Fatalf("Failed to get service URL: %v", err)
-		}
-
-		output, err := TestHelper.HTTPGetURL(svcURL)
-		if err != nil {
-			t.Fatalf("Unexpected error: %v", err)
-		}
-
-	})
+	// Test egress for a domain with fewer than 3 segments.
+	test("egress-test-not-www-get-svc", "https", 'get')
 }
