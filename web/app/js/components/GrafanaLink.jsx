d205033beb1a10a4e7e299c38bdf905b40c6ecb9
Andrew Seigner <siggy@buoyant.io>
2018-04-06 14:31:03 -0700

    Namespace-aware Grafana dashboards
    The Grafana dashboards key off of deployment, but had no awareness of
    namespaces, causing incorrect metrics aggregation and display.
    
    This change makes the Grafana dashboards key off of namespaces, and also
    modifies the Grafana links in the Conduit dashboard to link to
    namespace+deployment.
    
    Fixes #704
    Part of #420
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- web/app/js/components/GrafanaLink.jsx
+++ web/app/js/components/GrafanaLink.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 
 export default class GrafanaLink extends React.Component {
   render() {
-    // TODO: pass along namespace information as well, once grafana supports it, part of:
-    // https://github.com/runconduit/conduit/issues/420
-    let deployment = this.props.name.split("/")[1];
+    let ownerInfo = this.props.name.split("/");
+    let namespace = ownerInfo[0];
+    let deployment = ownerInfo[1];
     return (
       <this.props.conduitLink
-        to={`/dashboard/db/conduit-deployment?var-deployment=${deployment}`}
+        to={`/dashboard/db/conduit-deployment?var-namespace=${namespace}&var-deployment=${deployment}`}
         deployment={"grafana"}
         targetBlank={true}>
         <img
           src={grafanaicon}
           width={this.props.size}
           height={this.props.size}
-          title={`${deployment} grafana dashboard`}
-          alt={`link to ${deployment} grafana dashboard`} />
+          title={`${namespace}/${deployment} grafana dashboard`}
+          alt={`link to ${namespace}/${deployment} grafana dashboard`} />
       </this.props.conduitLink>
     );
   }
