15037d961809b3aa2509884935c0435d96944ba4
Brian Smith <brian@briansmith.org>
2018-04-06 08:45:18 -1000

    Proxy: Improve DNS name parsing (#708)
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- proxy/src/dns.rs
+++ proxy/src/dns.rs
@@ -3,6 +3,7 @@ use abstract_ns::HostResolve;
 use domain;
 use futures::prelude::*;
 use ns_dns_tokio;
+use std::fmt;
 use std::net::IpAddr;
 use std::path::Path;
 use std::str::FromStr;
@@ -18,13 +19,44 @@ pub struct Resolver(ns_dns_tokio::DnsResolver);
 pub enum IpAddrFuture {
     DNS(ns_dns_tokio::HostFuture),
     Fixed(IpAddr),
-    InvalidDNSName(String),
 }
 
 pub enum Error {
-    InvalidDNSName(String),
     NoAddressesFound,
     ResolutionFailed(<ns_dns_tokio::HostFuture as Future>::Error),
+}
+
+/// A DNS name.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Name(abstract_ns::Name);
+
+impl fmt::Display for Name {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        self.0.fmt(f)
+    }
+}
+
+impl Name {
+    /// Parses the input string as a DNS name, normalizing it to lowercase.
+    pub fn normalize(s: &str) -> Result<Self, ()> {
+        // XXX: `abstract_ns::Name::from_str()` wrongly accepts IP addresses as
+        // domain names. Protect against this. TODO: Fix abstract_ns.
+        if let Ok(_) = IpAddr::from_str(s) {
+            return Err(());
+        }
+        // XXX: `abstract_ns::Name::from_str()` doesn't accept uppercase letters.
+        //  TODO: Avoid this extra allocation.
+        let s = s.to_ascii_lowercase();
+        abstract_ns::Name::from_str(&s)
+            .map(Name)
+            .map_err(|_| ())
+    }
+}
+
+impl AsRef<str> for Name {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
 }
 
 impl Config {
@@ -49,10 +81,7 @@ impl Resolver {
         match *host {
             transport::Host::DnsName(ref name) => {
                 trace!("resolve {}", name);
-                match abstract_ns::Name::from_str(name) {
-                    Ok(name) => IpAddrFuture::DNS(self.0.resolve_host(&name)),
-                    Err(_) => IpAddrFuture::InvalidDNSName(name.clone()),
-                }
+                IpAddrFuture::DNS(self.0.resolve_host(&name.0))
             }
             transport::Host::Ip(addr) => IpAddrFuture::Fixed(addr),
         }
@@ -74,7 +103,55 @@ impl Future for IpAddrFuture {
                 Err(e) => Err(Error::ResolutionFailed(e)),
             },
             IpAddrFuture::Fixed(addr) => Ok(Async::Ready(addr)),
-            IpAddrFuture::InvalidDNSName(ref name) => Err(Error::InvalidDNSName(name.clone())),
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::Name;
+
+    #[test]
+    fn test_dns_name_parsing() {
+        struct Case {
+            input: &'static str,
+            output: &'static str,
+        }
+
+        static VALID: &[Case] = &[
+            // Almost all digits and dots, similar to IPv4 addresses.
+            Case { input: "1.2.3.x", output: "1.2.3.x", },
+            Case { input: "1.2.3.x", output: "1.2.3.x", },
+            Case { input: "1.2.3.4A", output: "1.2.3.4a", },
+            Case { input: "a.1.2.3", output: "a.1.2.3", },
+            Case { input: "1.2.x.3", output: "1.2.x.3", },
+            Case { input: "a.b.c.d", output: "a.b.c.d", },
+
+            // Uppercase letters in labels
+            Case { input: "A.b.c.d", output: "a.b.c.d", },
+            Case { input: "a.mIddle.c", output: "a.middle.c", },
+            Case { input: "a.b.c.D", output: "a.b.c.d", },
+        ];
+
+        for case in VALID {
+            let name = Name::normalize(case.input).expect("is a valid DNS name");
+            assert_eq!(name.as_ref(), case.output);
+        }
+
+        static INVALID: &[&str] = &[
+            "",
+            "1.2.3.4",
+            "::1",
+            "[::1]",
+            ":1234",
+            "1.2.3.4:11234",
+            "abc.com:1234",
+        ];
+
+        for case in INVALID {
+            assert!(Name::normalize(case).is_err(),
+                    "{} is invalid", case);
         }
     }
 }
