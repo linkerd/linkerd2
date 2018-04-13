f0a1a68b5e0c30d90e7080c92171d851cf5003aa
Eliza Weisman <eliza@buoyant.io>
2018-04-13 13:43:53 -0700

    Remove dead code

--- proxy/src/control/cache.rs
+++ proxy/src/control/cache.rs
@@ -1,6 +1,5 @@
 use indexmap::{IndexMap, IndexSet};
 use indexmap::map::{self, Entry};
-use std::borrow::Borrow;
 use std::hash::Hash;
 use std::iter::IntoIterator;
 use std::mem;
@@ -121,11 +120,6 @@ where
                     k
                 })
                 .collect();
-            // This retain is similar to `update_intersection`'s removal of
-            // non-present keys but without updating values, since we've
-            // already updated any values present in `to_insert`. We do this
-            // instead of just calling `update_intersection` so we don't have
-            // to clone the values we already know were inserted here.
             self.inner.retain(|key, _| if retained_keys.contains(key) {
                 true
             } else {
@@ -160,50 +154,6 @@ where
         for (key, _) in self.inner.drain(..) {
             on_change(CacheChange::Removal { key })
         };
-        self.reset_on_next_modification = false;
-    }
-
-    /// Update the cache to contain the intersection of its current contents
-    /// and the key-value pairs in `to_update`. Pairs not present in
-    /// `to_update` will be removed from the cache, and any keys in the cache
-    /// with different inner from those in `to_update` will be ovewritten to
-    /// match `to_update`.
-    pub fn update_intersection<F, Q>(
-        &mut self,
-        mut to_update: IndexMap<Q, V>,
-        mut on_change: F
-    )
-    where
-        F: for<'value> FnMut(CacheChange<'value, K, V>),
-        K: Borrow<Q>,
-        Q: Hash + Eq,
-        V: Clone,
-    {
-        self.inner.retain(|key, value| {
-            match to_update.remove(key.borrow()) {
-                // New value matches old value. Do nothing.
-                Some(ref new_value) if new_value == value => true,
-                // If the new value isn't equal to the old value, overwrite
-                // the old value.
-                Some(new_value) =>  {
-                    // TODO: ideally, we wouldn't clone here, but we can't
-                    // borrow the value from `self.inner` after inserting it,
-                    // as `self.inner` is already borrowed mutably by `retain`.
-                    // It would be nice to figure this out.
-                    let _ = mem::replace(value, new_value.clone());
-                    on_change(CacheChange::Modification {
-                        key: *key,
-                        new_value: &new_value,
-                    });
-                    true
-                },
-                // Key doesn't exist, remove it from the map.
-                None => {
-                    on_change(CacheChange::Removal { key: *key });
-                    false
-                }
-            }
-        });
         self.reset_on_next_modification = false;
     }
 }
@@ -290,68 +240,6 @@ mod tests {
         );
         assert_eq!(insertions, 3);
         assert_eq!(modifications, 3);
-    }
-
-    #[test]
-    fn update_intersection_fires_events() {
-        let mut cache = Cache {
-            inner: indexmap!{ 1 => "one",  2 => "two", 3 => "three" },
-            reset_on_next_modification: false,
-        };
-
-        let mut removals = 0;
-        let mut modifications = 0;
-
-        cache.update_intersection(
-            indexmap!{ 1 => "one", 3 => "three"},
-            &mut |change: CacheChange<usize, &str>| {
-                removals += 1;
-                assert_eq!(change, CacheChange::Removal{ key: 2, });
-            },
-        );
-        assert_eq!(cache.inner, indexmap!{ 1 => "one", 3 => "three"});
-        assert_eq!(removals, 1);
-
-        cache.update_intersection(
-            indexmap!{ 3 => "3", 4 => "four" },
-            &mut |change: CacheChange<usize, &str>| match change {
-                CacheChange::Removal { key } => {
-                    removals += 1;
-                    assert_eq!(key, 1);
-                }
-                CacheChange::Insertion { ..  } => {
-                    panic!("no insertion events should be fired");
-                }
-                CacheChange::Modification { key, new_value} => {
-                    modifications += 1;
-                    assert_eq!(key, 3);
-                    assert_eq!(new_value, &"3")
-                },
-            }
-        );
-        assert_eq!(cache.inner, indexmap!{ 3 => "3" });
-        assert_eq!(removals, 2);
-        assert_eq!(modifications, 1);
-
-        cache.update_intersection(
-            indexmap!{ 5 => "five", 6 => "six" },
-            &mut |change: CacheChange<usize, &str>| match change {
-                CacheChange::Removal { key } => {
-                    removals += 1;
-                    assert_eq!(key, 3);
-                }
-                CacheChange::Insertion { ..  } => {
-                    panic!("no insertion events should be fired");
-                }
-                CacheChange::Modification { ..  } => {
-                    panic!("no more modification events should be fired");
-                }
-            }
-        );
-
-        assert!(cache.inner.is_empty());
-        assert_eq!(removals, 3);
-        assert_eq!(modifications, 1);
     }
 
     #[test]
