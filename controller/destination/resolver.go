ed8408826d7cf5764006d6dc154552e43ebe98d4
Brian Smith <brian@briansmith.org>
2018-04-16 13:15:31 -1000

    Remove echoIpV4Resolver.
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- controller/destination/resolver.go
+++ controller/destination/resolver.go
@@ -1,43 +1,6 @@
 package destination
 
-import (
-	"fmt"
-
-	common "github.com/runconduit/conduit/controller/gen/common"
-	"github.com/runconduit/conduit/controller/util"
-)
-
 type streamingDestinationResolver interface {
 	canResolve(host string, port int) (bool, error)
 	streamResolution(host string, port int, listener updateListener) error
-}
-
-// implements the streamingDestinationResolver interface
-type echoIpV4Resolver struct{}
-
-func (i *echoIpV4Resolver) canResolve(host string, port int) (bool, error) {
-	isIP, _ := isIPAddress(host)
-	return isIP, nil
-}
-
-func (i *echoIpV4Resolver) streamResolution(host string, port int, listener updateListener) error {
-	isIP, ip := isIPAddress(host)
-	if !isIP {
-		return fmt.Errorf("host [%s] isn't an IP address", host)
-	}
-
-	address := common.TcpAddress{
-		Ip:   ip,
-		Port: uint32(port),
-	}
-
-	listener.Update([]common.TcpAddress{address}, []common.TcpAddress{})
-
-	<-listener.Done()
-	return nil
-}
-
-func isIPAddress(host string) (bool, *common.IPAddress) {
-	ip, err := util.ParseIPV4(host)
-	return err == nil, ip
 }
