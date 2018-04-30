c5299e6d0ae9178f3ee5a0212216bf36eea2dfc2
Oliver Gould <ver@buoyant.io>
2018-04-30 18:21:46 +0000

    make histogram labels lazily-formatted

--- proxy/src/telemetry/metrics/histogram.rs
+++ proxy/src/telemetry/metrics/histogram.rs
@@ -46,7 +46,13 @@ pub enum Bucket {
 pub struct Bounds(pub &'static [Bucket]);
 
 /// Helper that lazily formats metric keys as {0}_{1}.
-struct Key<'a, P: Display + 'a>(&'a P, &'a str);
+struct Key<A: Display, B: Display>(A, B);
+
+/// Helper that lazily formats comma-separated labels `A,B`.
+struct Labels<A: Display, B: Display>(A, B);
+
+/// Helper that lazily formats an `{K}="{V}"`" label.
+struct Label<K: Display, V: Display>(K, V);
 
 // ===== impl Histogram =====
 
@@ -101,7 +107,7 @@ impl<V: Into<u64>> FmtMetric for Histogram<V> {
         let mut total = Counter::default();
         for (le, count) in self {
             total += *count;
-            total.fmt_metric_labeled(f, Key(&name, "bucket"), format!("le=\"{}\"", le))?;
+            total.fmt_metric_labeled(f, Key(&name, "bucket"), Label("le", le))?;
         }
         total.fmt_metric(f, Key(&name, "count"))?;
         self.sum.fmt_metric(f, Key(&name, "sum"))?;
@@ -117,8 +123,7 @@ impl<V: Into<u64>> FmtMetric for Histogram<V> {
         let mut total = Counter::default();
         for (le, count) in self {
             total += *count;
-            total.fmt_metric_labeled(f, Key(&name, "bucket"),
-                format!("{},le=\"{}\"", labels, le))?;
+            total.fmt_metric_labeled(f, Key(&name, "bucket"), Labels(&labels, Label("le", le)))?;
         }
         total.fmt_metric_labeled(f, Key(&name, "count"), &labels)?;
         self.sum.fmt_metric_labeled(f, Key(&name, "sum"), &labels)?;
@@ -129,9 +134,25 @@ impl<V: Into<u64>> FmtMetric for Histogram<V> {
 
 // ===== impl Key =====
 
-impl<'a, P: Display> fmt::Display for Key<'a, P> {
+impl<A: Display, B: Display> fmt::Display for Key<A, B> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}_{}", self.0, self.1)
+    }
+}
+
+// ===== impl Label =====
+
+impl<K: Display, V: Display> fmt::Display for Label<K, V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}=\"{}\"", self.0, self.1)
+    }
+}
+
+// ===== impl Labels =====
+
+impl<A: Display, B: Display> fmt::Display for Labels<A, B> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{},{}", self.0, self.1)
     }
 }
 
