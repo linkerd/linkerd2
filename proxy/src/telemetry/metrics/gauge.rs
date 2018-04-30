2466b009387b8631609498d1b49affa363bfa4f0
Oliver Gould <ver@buoyant.io>
2018-04-30 20:10:37 +0000

    use associated KIND `const` instead of `fn`

--- proxy/src/telemetry/metrics/gauge.rs
+++ proxy/src/telemetry/metrics/gauge.rs
@@ -39,9 +39,7 @@ impl Into<u64> for Gauge {
 }
 
 impl FmtMetric for Gauge {
-    fn kind() -> &'static str {
-        "gauge"
-    }
+    const KIND: &'static str = "gauge";
 
     fn fmt_metric<N: Display>(&self, f: &mut fmt::Formatter, name: N) -> fmt::Result {
         writeln!(f, "{} {}", name, self.0)
