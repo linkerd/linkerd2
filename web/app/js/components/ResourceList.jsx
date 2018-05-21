e0ea63e64b94bee18ea6bae47e376bf2ec0aebf1
grampelberg <thomas@saunter.org>
2018-05-21 15:46:27 -0700

    Add a HOC for the REST API tooling
    We're copying and duplicating logic all over the place with components that need to talk to the API.
    
    Moves most of the REST API tooling into a HOC that can be used by other components. Now, a component can use `withREST`, pass in the promises that it would like resolved and receive the responses as props.

--- web/app/js/components/ResourceList.jsx
+++ web/app/js/components/ResourceList.jsx
@@ -5,107 +5,39 @@ import MetricsTable from './MetricsTable.jsx';
 import PageHeader from './PageHeader.jsx';
 import { processSingleResourceRollup } from './util/MetricUtils.js';
 import React from 'react';
-import { withContext } from './util/AppContext.jsx';
+import withREST from './util/withREST.jsx';
 import './../../css/list.css';
 import 'whatwg-fetch';
 
-class ResourceList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.api = this.props.api;
-    this.handleApiError = this.handleApiError.bind(this);
-    this.loadFromServer = this.loadFromServer.bind(this);
-
-    this.state = this.getInitialState(this.props);
-  }
-
-  getInitialState() {
-    return {
-      pollingInterval: 2000, // TODO: poll based on metricsWindow size
-      metrics: [],
-      pendingRequests: false,
-      loaded: false,
-      error: ''
-    };
-  }
-
-  componentDidMount() {
-    this.startServerPolling(this.props.resource);
-  }
-
-  componentWillReceiveProps(newProps) {
-    // React won't unmount this component when switching resource pages so we need to clear state
-    this.stopServerPolling();
-    this.setState(this.getInitialState(newProps));
-    this.startServerPolling(newProps.resource);
-  }
-
-  componentWillUnmount() {
-    this.stopServerPolling();
-  }
-
-  startServerPolling(resource) {
-    this.loadFromServer(resource);
-    this.timerId = window.setInterval(this.loadFromServer, this.state.pollingInterval, resource);
-  }
-
-  stopServerPolling() {
-    window.clearInterval(this.timerId);
-    this.api.cancelCurrentRequests();
-  }
-
-  loadFromServer(resource) {
-    if (this.state.pendingRequests) {
-      return; // don't make more requests if the ones we sent haven't completed
-    }
-    this.setState({ pendingRequests: true });
-
-    this.api.setCurrentRequests([
-      this.api.fetchMetrics(this.api.urlsForResource(resource))
-    ]);
-
-    Promise.all(this.api.getCurrentPromises())
-      .then(([rollup]) => {
-        let processedMetrics = processSingleResourceRollup(rollup);
-
-        this.setState({
-          metrics: processedMetrics,
-          loaded: true,
-          pendingRequests: false,
-          error: ''
-        });
-      })
-      .catch(this.handleApiError);
-  }
-
-  handleApiError(e) {
-    if (e.isCanceled) {
-      return;
-    }
-
-    this.setState({
-      pendingRequests: false,
-      error: `Error getting data from server: ${e.message}`
-    });
-  }
-
+export class ResourceList extends React.Component {
   render() {
-    let friendlyTitle = _.startCase(this.props.resource);
+    const {api, data, error, loading, resource} = this.props;
 
+    if (error) return  <ErrorBanner message={error} />;
+    if (loading) return <ConduitSpinner />;
+
+    const processedMetrics = processSingleResourceRollup(data[0]);
+
+    const friendlyTitle = _.startCase(resource);
     return (
       <div className="page-content">
-        { !this.state.error ? null : <ErrorBanner message={this.state.error} /> }
-        { !this.state.loaded ? <ConduitSpinner />  :
-          <div>
-            <PageHeader header={friendlyTitle + "s"} />
+        <div>
+          <PageHeader header={`${friendlyTitle}s`} api={api} />
+          { _.isEmpty(processedMetrics) ?
+            this.renderEmptyMessage(processedMetrics) :
             <MetricsTable
               resource={friendlyTitle}
-              metrics={this.state.metrics}
+              metrics={processedMetrics}
               linkifyNsColumn={true} />
-          </div>
-        }
-      </div>);
+          }
+        </div>
+      </div>
+    );
   }
 }
 
-export default withContext(ResourceList);
+export default withREST(
+  ResourceList,
+  ({api, resource}) => [api.fetchMetrics(api.urlsForResource(resource))],
+  ['resource'],
+);
