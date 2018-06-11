a67ba3e0ca30a4d600aeb73d89f7c62627b3000a
Risha Mars <mars@buoyant.io>
2018-06-11 16:02:30 -0700

    Add a prod and dev webpack configuration, based off NODE_ENV

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -1,55 +1,5 @@
 /* global require, module, __dirname */
 
-const path = require('path');
-
-module.exports = {
-  entry: './js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/',
-    filename: 'index_bundle.js'
-  },
-  devtool: 'cheap-module-source-map',
-  externals: {
-    cheerio: 'window',
-    'react/addons': 'react',
-    'react/lib/ExecutionEnvironment': 'react',
-    'react/lib/ReactContext': 'react',
-    'react-addons-test-utils': 'react-dom',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'eslint-loader',
-            options: {
-              fix: true,
-              emitWarning: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|eot|svg|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: { publicPath: 'dist/' }
-          }
-        ]
-      }
-    ]
-  }
-}
+module.exports = process.env.NODE_ENV === 'production'
+  ? require('./webpack.prod.js')
+  : require('./webpack.dev.js')
---
+++ web/app/webpack.prod.js
@@ -0,0 +1,24 @@
+/* global require, module, __dirname */
+
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base');
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+});
