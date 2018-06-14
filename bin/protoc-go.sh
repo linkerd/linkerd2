2a4f38b9e71315e74a0000338094d968249228dd
Oliver Gould <ver@buoyant.io>
2018-06-14 14:03:00 -0700

    proto: Use explicit `go_package` option (#1120)
    protobuf has a `go_package` option that can be used to explicitly name
    Go packages such that they can be imported without additional rewrites.
    
    This allows us to store proto files without additional, redundant
    directories (which were used for packaging hints, previously).
    
    This change adds an explicit `go_package` to all .proto files and
    updates `bin/protoc-go.sh` to ensure these packages are output into
    $GOPATH (so that the go_package can be absolute). This removes the need
    to manually rewrite imports in bin/protoc-go.sh.

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -2,21 +2,21 @@
 
 set -eu
 
+gen() {
+    find controller/gen -name \*.pb.go -exec git rm -f '{}' +
+
+    for f in $@; do
+        bin/protoc -I proto --go_out="plugins=grpc:$GOPATH/src" "$f"
+    done
+
+    git add controller/gen
+}
+
 go install ./vendor/github.com/golang/protobuf/protoc-gen-go
 
-rm -rf controller/gen
-mkdir controller/gen
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/public/api.proto
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/common/common.proto
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/common/healthcheck/healthcheck.proto
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/proxy/destination/destination.proto
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/proxy/tap/tap.proto
-bin/protoc -I proto --go_out=plugins=grpc:controller/gen proto/controller/tap/tap.proto
-
-# Manually fix imports
-find controller/gen -type f -exec sed -i.bak 's:"common":"github.com\/runconduit\/conduit\/controller\/gen\/common":g' {} +
-find controller/gen -type f -exec sed -i.bak 's:"common/healthcheck":"github.com\/runconduit\/conduit\/controller\/gen\/common\/healthcheck":g' {} +
-find controller/gen -type f -exec sed -i.bak 's:"proxy/tap":"github.com\/runconduit\/conduit\/controller\/gen\/proxy\/tap":g' {} +
-find controller/gen -type f -exec sed -i.bak 's:"controller/tap":"github.com\/runconduit\/conduit\/controller\/gen\/controller\/tap":g' {} +
-find controller/gen -type f -exec sed -i.bak 's:"public":"github.com\/runconduit\/conduit\/controller\/gen\/public":g' {} +
-find controller/gen -name '*.bak' -delete
+gen proto/public.proto \
+    proto/common.proto \
+    proto/common/healthcheck.proto \
+    proto/controller/tap.proto \
+    proto/proxy/destination.proto \
+    proto/proxy/tap.proto
