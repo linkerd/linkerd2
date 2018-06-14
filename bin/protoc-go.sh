e02cfd70022f97f92587d481e6169be6c95bf43b
Oliver Gould <ver@buoyant.io>
2018-06-14 16:03:59 +0000

    less boilerplate; git-aware protoc-go.sh

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -2,16 +2,21 @@
 
 set -eu
 
+gen() {
+    find controller/gen -name \*.pb.go -exec git rm -f '{}' +
+
+    for f in $@; do
+        bin/protoc -I proto --go_out="plugins=grpc:$GOPATH/src" "$f"
+    done
+
+    git add controller/gen
+}
+
 go install ./vendor/github.com/golang/protobuf/protoc-gen-go
 
-GO_OUT="plugins=grpc:$GOPATH/src"
-
-rm -rf controller/gen
-mkdir -p controller/gen
-
-bin/protoc -I proto --go_out="$GO_OUT" proto/public.proto
-bin/protoc -I proto --go_out="$GO_OUT" proto/common.proto
-bin/protoc -I proto --go_out="$GO_OUT" proto/common/healthcheck.proto
-bin/protoc -I proto --go_out="$GO_OUT" proto/controller/tap.proto
-bin/protoc -I proto --go_out="$GO_OUT" proto/proxy/destination.proto
-bin/protoc -I proto --go_out="$GO_OUT" proto/proxy/tap.proto
+gen proto/public.proto \
+    proto/common.proto \
+    proto/common/healthcheck.proto \
+    proto/controller/tap.proto \
+    proto/proxy/destination.proto \
+    proto/proxy/tap.proto
