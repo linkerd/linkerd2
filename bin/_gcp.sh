c34925bc088f3b8d9451fa09a4303346a69a376c
Kevin Lingerfelt <kl@buoyant.io>
2018-06-15 13:05:42 -0700

    Add gcp helper funcs
    Signed-off-by: Kevin Lingerfelt <kl@buoyant.io>

--- bin/_gcp.sh
+++ bin/_gcp.sh
@@ -1,5 +1,31 @@
 set -eu
 
+install_gcloud_kubectl() {
+    dir="$1"
+
+    export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+    if [ -d "$dir/bin" ]; then
+        . "$dir/path.bash.inc"
+        gcloud components update
+    else
+        rm -rf "$dir"
+        curl https://sdk.cloud.google.com | bash
+        . "$dir/path.bash.inc"
+    fi
+    gcloud components install kubectl
+}
+
+set_gcloud_config() {
+    project="$1"
+    zone="$2"
+    cluster="$3"
+
+    gcloud auth activate-service-account --key-file .gcp.json
+    gcloud config set core/project "$project"
+    gcloud config set compute/zone "$zone"
+    gcloud config set container/cluster "$cluster"
+}
+
 get_k8s_ctx() {
     project="$1"
     zone="$2"
