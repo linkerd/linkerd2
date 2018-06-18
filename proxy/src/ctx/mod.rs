ninokop/conduit

Add `is_tls` field to Client context

proxy/src/ctx/mod.rs
@@ -109,7 +109,7 @@ pub mod test_util {
         S: fmt::Display,
     {
         let labels = DstLabels::new(labels);
-        ctx::transport::Client::new(&proxy, &addr(), labels)
+        ctx::transport::Client::new(&proxy, &addr(), labels, false)
     }
 
     pub fn request(

linkerd/linkerd2

proxy/src/ctx/mod.rs
@@ -110,7 +110,7 @@ pub mod test_util {
         S: fmt::Display,
     {
         let meta = destination::Metadata::new(DstLabels::new(labels), None);
-        ctx::transport::Client::new(&proxy, &addr(), meta)
+        ctx::transport::Client::new(&proxy, &addr(), meta, false)
     }
 
     pub fn request(