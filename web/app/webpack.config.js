bef98e3ba72efba0702cb814cd4b3d04ab676e9c
Risha Mars <mars@buoyant.io>
2018-06-18 14:14:24 -0700

    Remove unused const

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -1,7 +1,6 @@
 /* global require, module, __dirname */
 
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
