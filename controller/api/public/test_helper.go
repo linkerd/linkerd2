linkerd/linkerd2

commit 7d4c4aa290d2499c53dcf17dc387f5b5546f0411

CLI: print resources in the same order every time stat all is run (#1088)
Previously, in conduit stat all we would just print the map of stat results, which 
resulted in the order in which stats were displayed varying between prints.

Fix:
Define an array, k8s.StatAllResourceTypes and use the order in this array to print 
the map; ensuring a consistent print order every time the command is run.

cli/cmd/stat.go
@@ -224,15 +224,18 @@ func writeStatsToBuffer(resp *pb.StatSummaryResponse, reqResourceType string, w 
 	}
 
 	lastDisplayedStat := true // don't print a newline after the final stat
-	for resourceType, stats := range statTables {
-		if !lastDisplayedStat {
-			fmt.Fprint(w, "\n")
-		}
-		lastDisplayedStat = false
-		if reqResourceType == k8s.All {
-			printStatTable(stats, resourceType, w, maxNameLength, maxNamespaceLength, options)
-		} else {
-			printStatTable(stats, "", w, maxNameLength, maxNamespaceLength, options)
+
+	for _, resourceType := range k8s.StatAllResourceTypes {
+		if stats, ok := statTables[resourceType]; ok {
+			if !lastDisplayedStat {
+				fmt.Fprint(w, "\n")
+			}
+			lastDisplayedStat = false
+			if reqResourceType == k8s.All {
+				printStatTable(stats, resourceType, w, maxNameLength, maxNamespaceLength, options)
+			} else {
+				printStatTable(stats, "", w, maxNameLength, maxNamespaceLength, options)
+			}
 		}
 	}
 }

controller/api/public/stat_summary.go
@@ -47,9 +47,6 @@ const (
 
 var promTypes = []promType{promRequests, promLatencyP50, promLatencyP95, promLatencyP99}
 
-// resources to query when Resource.Type is "all"
-var resourceTypes = []string{k8s.Pods, k8s.Deployments, k8s.ReplicationControllers, k8s.Services}
-
 type podCount struct {
 	inMesh uint64
 	total  uint64
@@ -83,7 +80,7 @@ func (s *grpcServer) StatSummary(ctx context.Context, req *pb.StatSummaryRequest
 
 	var resourcesToQuery []string
 	if req.Selector.Resource.Type == k8s.All {
-		resourcesToQuery = resourceTypes
+		resourcesToQuery = k8s.StatAllResourceTypes
 	} else {
 		resourcesToQuery = []string{req.Selector.Resource.Type}
 	}

pkg/k8s/k8s.go
@@ -27,6 +27,9 @@ var ResourceTypesToProxyLabels = map[string]string{
 	Services:               "service",
 }
 
+// resources to query in StatSummary when Resource.Type is "all"
+var StatAllResourceTypes = []string{Deployments, ReplicationControllers, Pods, Services}
+
 func generateKubernetesApiBaseUrlFor(schemeHostAndPort string, namespace string, extraPathStartingWithSlash string) (*url.URL, error) {
 	if string(extraPathStartingWithSlash[0]) != "/" {
 		return nil, fmt.Errorf("Path must start with a [/], was [%s]", extraPathStartingWithSlash)

commit 46c99febf22082233f503f10b140b08ece75c0a0

Don't panic on stats that aren't included in StatAllResourceTypes (#1154)
Problem
`conduit stat` would cause a panic for any resource that wasn't in the list 
of StatAllResourceTypes
This bug was introduced by https://github.com/runconduit/conduit/pull/1088/files

Solution
Fix writeStatsToBuffer to not depend on what resources are in StatAllResourceTypes
Also adds a unit test and integration test for `conduit stat ns`

cli/cmd/stat.go
@@ -223,19 +223,21 @@ func writeStatsToBuffer(resp *pb.StatSummaryResponse, reqResourceType string, w 
 		os.Exit(0)
 	}
 
-	lastDisplayedStat := true // don't print a newline after the final stat
-
-	for _, resourceType := range k8s.StatAllResourceTypes {
-		if stats, ok := statTables[resourceType]; ok {
-			if !lastDisplayedStat {
-				fmt.Fprint(w, "\n")
-			}
-			lastDisplayedStat = false
-			if reqResourceType == k8s.All {
+	switch reqResourceType {
+	case k8s.All:
+		firstDisplayedStat := true // don't print a newline before the first stat
+		for _, resourceType := range k8s.StatAllResourceTypes {
+			if stats, ok := statTables[resourceType]; ok {
+				if !firstDisplayedStat {
+					fmt.Fprint(w, "\n")
+				}
+				firstDisplayedStat = false
 				printStatTable(stats, resourceType, w, maxNameLength, maxNamespaceLength, options)
-			} else {
-				printStatTable(stats, "", w, maxNameLength, maxNamespaceLength, options)
 			}
+		}
+	default:
+		if stats, ok := statTables[reqResourceType]; ok {
+			printStatTable(stats, "", w, maxNameLength, maxNamespaceLength, options)
 		}
 	}
 }

cli/cmd/stat_test.go
@@ -1,0 +1,37 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/runconduit/conduit/controller/api/public"
+)
+
+func TestStat(t *testing.T) {
+	t.Run("Returns namespace stats", func(t *testing.T) {
+		mockClient := &public.MockConduitApiClient{}
+
+		response := public.GenStatSummaryResponse("emoji", "namespaces", "emojivoto", 1, 2, 0)
+
+		mockClient.StatSummaryResponseToReturn = &response
+
+		expectedOutput := `NAME    MESHED   SUCCESS      RPS   LATENCY_P50   LATENCY_P95   LATENCY_P99   SECURED
+emoji      1/2   100.00%   2.0rps         123ms         123ms         123ms      100%
+`
+
+		options := newStatOptions()
+		args := []string{"ns"}
+		req, err := buildStatSummaryRequest(args, options)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+
+		output, err := requestStatsFromAPI(mockClient, req, options)
+		if err != nil {
+			t.Fatalf("Unexpected error: %v", err)
+		}
+
+		if output != expectedOutput {
+			t.Fatalf("Wrong output:\n expected: \n%s\n, got: \n%s", expectedOutput, output)
+		}
+	})
+}

controller/api/public/stat_summary_test.go
@@ -43,44 +43,6 @@ func genPromSample(resName string, resType string, resNs string, classification 
 	}
 }
 
-func genStatSummaryResponse(resName, resType, resNs string, meshedPods uint64, runningPods uint64, failedPods uint64) pb.StatSummaryResponse {
-	return pb.StatSummaryResponse{
-		Response: &pb.StatSummaryResponse_Ok_{ // https://github.com/golang/protobuf/issues/205
-			Ok: &pb.StatSummaryResponse_Ok{
-				StatTables: []*pb.StatTable{
-					&pb.StatTable{
-						Table: &pb.StatTable_PodGroup_{
-							PodGroup: &pb.StatTable_PodGroup{
-								Rows: []*pb.StatTable_PodGroup_Row{
-									&pb.StatTable_PodGroup_Row{
-										Resource: &pb.Resource{
-											Namespace: resNs,
-											Type:      resType,
-											Name:      resName,
-										},
-										Stats: &pb.BasicStats{
-											SuccessCount:    123,
-											FailureCount:    0,
-											LatencyMsP50:    123,
-											LatencyMsP95:    123,
-											LatencyMsP99:    123,
-											TlsRequestCount: 123,
-										},
-										TimeWindow:      "1m",
-										MeshedPodCount:  meshedPods,
-										RunningPodCount: runningPods,
-										FailedPodCount:  failedPods,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
 func genEmptyResponse() pb.StatSummaryResponse {
 	return pb.StatSummaryResponse{
 		Response: &pb.StatSummaryResponse_Ok_{ // https://github.com/golang/protobuf/issues/205
@@ -229,7 +191,7 @@ status:
 					},
 					TimeWindow: "1m",
 				},
-				expectedResponse: genStatSummaryResponse("emoji", "deployments", "emojivoto", 1, 2, 0),
+				expectedResponse: GenStatSummaryResponse("emoji", "deployments", "emojivoto", 1, 2, 0),
 			},
 		}
 
@@ -271,7 +233,7 @@ status:
 					`histogram_quantile(0.99, sum(irate(response_latency_ms_bucket{direction="inbound", namespace="emojivoto", pod="emojivoto-1"}[1m])) by (le, namespace, pod))`,
 					`sum(increase(response_total{direction="inbound", namespace="emojivoto", pod="emojivoto-1"}[1m])) by (namespace, pod, classification, tls)`,
 				},
-				expectedResponse: genStatSummaryResponse("emojivoto-1", "pods", "emojivoto", 1, 1, 0),
+				expectedResponse: GenStatSummaryResponse("emojivoto-1", "pods", "emojivoto", 1, 1, 0),
 			},
 		}
 
@@ -763,7 +725,7 @@ status:
 						},
 						TimeWindow: "1m",
 					},
-					expectedResponse: genStatSummaryResponse("emoji", "deployments", "emojivoto", 1, 2, 1),
+					expectedResponse: GenStatSummaryResponse("emoji", "deployments", "emojivoto", 1, 2, 1),
 				},
 			}
 

controller/api/public/test_helper.go
@@ -125,3 +125,41 @@ func (m *MockProm) LabelValues(ctx context.Context, label string) (model.LabelVa
 func (m *MockProm) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, error) {
 	return nil, nil
 }
+
+func GenStatSummaryResponse(resName, resType, resNs string, meshedPods uint64, runningPods uint64, failedPods uint64) pb.StatSummaryResponse {
+	return pb.StatSummaryResponse{
+		Response: &pb.StatSummaryResponse_Ok_{ // https://github.com/golang/protobuf/issues/205
+			Ok: &pb.StatSummaryResponse_Ok{
+				StatTables: []*pb.StatTable{
+					&pb.StatTable{
+						Table: &pb.StatTable_PodGroup_{
+							PodGroup: &pb.StatTable_PodGroup{
+								Rows: []*pb.StatTable_PodGroup_Row{
+									&pb.StatTable_PodGroup_Row{
+										Resource: &pb.Resource{
+											Namespace: resNs,
+											Type:      resType,
+											Name:      resName,
+										},
+										Stats: &pb.BasicStats{
+											SuccessCount:    123,
+											FailureCount:    0,
+											LatencyMsP50:    123,
+											LatencyMsP95:    123,
+											LatencyMsP99:    123,
+											TlsRequestCount: 123,
+										},
+										TimeWindow:      "1m",
+										MeshedPodCount:  meshedPods,
+										RunningPodCount: runningPods,
+										FailedPodCount:  failedPods,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}

test/stat/stat_test.go
@@ -45,41 +45,69 @@ var controllerDeployments = []string{"controller", "grafana", "prometheus", "web
 // failed attempts will eventually be recorded in the stats that we're
 // requesting, and the test will pass.
 func TestCliStatForConduitNamespace(t *testing.T) {
+	t.Run("test conduit stat deploy", func(t *testing.T) {
+		err := TestHelper.RetryFor(20*time.Second, func() error {
+			out, err := TestHelper.ConduitRun("stat", "deploy", "-n", TestHelper.GetConduitNamespace())
+			if err != nil {
+				t.Fatalf("Unexpected stat error: %v", err)
+			}
 
-	err := TestHelper.RetryFor(20*time.Second, func() error {
-		out, err := TestHelper.ConduitRun("stat", "deploy", "-n", TestHelper.GetConduitNamespace())
-		if err != nil {
-			t.Fatalf("Unexpected stat error: %v", err)
-		}
-
-		rowStats, err := parseRows(out)
-		if err != nil {
-			return err
-		}
-
-		for _, name := range controllerDeployments {
-			if err := validateRowStats(name, rowStats); err != nil {
+			rowStats, err := parseRows(out, 4)
+			if err != nil {
 				return err
 			}
+
+			for _, name := range controllerDeployments {
+				if err := validateRowStats(name, "1/1", rowStats); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		})
+		if err != nil {
+			t.Fatal(err.Error())
 		}
-
-		return nil
 	})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
 
+	t.Run("test conduit stat namespace", func(t *testing.T) {
+		err := TestHelper.RetryFor(20*time.Second, func() error {
+			out, err := TestHelper.ConduitRun("stat", "ns", TestHelper.GetConduitNamespace())
+			if err != nil {
+				t.Fatalf("Unexpected stat error: %v", err)
+			}
+
+			rowStats, err := parseRows(out, 1)
+			if err != nil {
+				return err
+			}
+
+			return validateRowStats(TestHelper.GetConduitNamespace(), "4/4", rowStats)
+		})
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
 }
 
-func parseRows(out string) (map[string]*rowStat, error) {
+// check that expectedRowCount rows have been returned
+func checkRowCount(out string, expectedRowCount int) ([]string, error) {
 	rows := strings.Split(out, "\n")
 	rows = rows[1 : len(rows)-1] // strip header and trailing newline
 
-	expectedRowCount := 4
 	if len(rows) != expectedRowCount {
 		return nil, fmt.Errorf(
 			"Expected [%d] rows in stat output, got [%d]; full output:\n%s",
 			expectedRowCount, len(rows), strings.Join(rows, "\n"))
+	}
+
+	return rows, nil
+}
+
+func parseRows(out string, expectedRowCount int) (map[string]*rowStat, error) {
+	rows, err := checkRowCount(out, expectedRowCount)
+	if err != nil {
+		return nil, err
 	}
 
 	rowStats := make(map[string]*rowStat)
@@ -108,13 +136,12 @@ func parseRows(out string) (map[string]*rowStat, error) {
 	return rowStats, nil
 }
 
-func validateRowStats(name string, rowStats map[string]*rowStat) error {
+func validateRowStats(name, expectedMeshCount string, rowStats map[string]*rowStat) error {
 	stat, ok := rowStats[name]
 	if !ok {
 		return fmt.Errorf("No stats found for [%s]", name)
 	}
 
-	expectedMeshCount := "1/1"
 	if stat.meshed != expectedMeshCount {
 		return fmt.Errorf("Expected mesh count [%s] for [%s], got [%s]",
 			expectedMeshCount, name, stat.meshed)