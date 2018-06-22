1f1968ad4d7fa90799eddc272882ec8b8763a5a9
Kevin Lingerfelt <kl@buoyant.io>
2018-06-22 12:52:42 -0700

    Add --registry flag support for inject command (#1188)
    Signed-off-by: Kevin Lingerfelt <kl@buoyant.io>

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/runconduit/conduit/controller/api/public"
@@ -18,17 +19,31 @@ var apiAddr string // An empty value means "use the Kubernetes configuration"
 var kubeconfigPath string
 var verbose bool
 
+var (
+	// These regexs are not as strict as they could be, but are a quick and dirty
+	// sanity check against illegal characters.
+	alphaNumDash         = regexp.MustCompile("^[a-zA-Z0-9-]+$")
+	alphaNumDashDot      = regexp.MustCompile("^[\\.a-zA-Z0-9-]+$")
+	alphaNumDashDotSlash = regexp.MustCompile("^[\\./a-zA-Z0-9-]+$")
+)
+
 var RootCmd = &cobra.Command{
 	Use:   "conduit",
 	Short: "conduit manages the Conduit service mesh",
 	Long:  `conduit manages the Conduit service mesh.`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		// enable / disable logging
 		if verbose {
 			log.SetLevel(log.DebugLevel)
 		} else {
 			log.SetLevel(log.PanicLevel)
 		}
+
+		if !alphaNumDash.MatchString(controlPlaneNamespace) {
+			return fmt.Errorf("%s is not a valid namespace", controlPlaneNamespace)
+		}
+
+		return nil
 	},
 }
 
@@ -64,6 +79,7 @@ type proxyConfigOptions struct {
 	conduitVersion   string
 	proxyImage       string
 	initImage        string
+	dockerRegistry   string
 	imagePullPolicy  string
 	proxyUID         int64
 	proxyLogLevel    string
@@ -74,11 +90,17 @@ type proxyConfigOptions struct {
 	tls              string
 }
 
+const (
+	optionalTLS           = "optional"
+	defaultDockerRegistry = "gcr.io/runconduit"
+)
+
 func newProxyConfigOptions() *proxyConfigOptions {
 	return &proxyConfigOptions{
 		conduitVersion:   version.Version,
-		proxyImage:       "gcr.io/runconduit/proxy",
-		initImage:        "gcr.io/runconduit/proxy-init",
+		proxyImage:       defaultDockerRegistry + "/proxy",
+		initImage:        defaultDockerRegistry + "/proxy-init",
+		dockerRegistry:   defaultDockerRegistry,
 		imagePullPolicy:  "IfNotPresent",
 		proxyUID:         2102,
 		proxyLogLevel:    "warn,conduit_proxy=info",
@@ -90,13 +112,12 @@ func newProxyConfigOptions() *proxyConfigOptions {
 	}
 }
 
-const optionalTLS = "optional"
-
-var alphaNumDashDot = regexp.MustCompile("^[\\.a-zA-Z0-9-]+$")
-
 func (options *proxyConfigOptions) validate() error {
 	if !alphaNumDashDot.MatchString(options.conduitVersion) {
 		return fmt.Errorf("%s is not a valid version", options.conduitVersion)
+	}
+	if !alphaNumDashDotSlash.MatchString(options.dockerRegistry) {
+		return fmt.Errorf("%s is not a valid Docker registry", options.dockerRegistry)
 	}
 	if options.imagePullPolicy != "Always" && options.imagePullPolicy != "IfNotPresent" && options.imagePullPolicy != "Never" {
 		return fmt.Errorf("--image-pull-policy must be one of: Always, IfNotPresent, Never")
@@ -114,10 +135,21 @@ func (options *proxyConfigOptions) enableTLS() bool {
 	return options.tls == optionalTLS
 }
 
+func (options *proxyConfigOptions) taggedProxyImage() string {
+	image := strings.Replace(options.proxyImage, defaultDockerRegistry, options.dockerRegistry, 1)
+	return fmt.Sprintf("%s:%s", image, options.conduitVersion)
+}
+
+func (options *proxyConfigOptions) taggedProxyInitImage() string {
+	image := strings.Replace(options.initImage, defaultDockerRegistry, options.dockerRegistry, 1)
+	return fmt.Sprintf("%s:%s", image, options.conduitVersion)
+}
+
 func addProxyConfigFlags(cmd *cobra.Command, options *proxyConfigOptions) {
 	cmd.PersistentFlags().StringVarP(&options.conduitVersion, "conduit-version", "v", options.conduitVersion, "Tag to be used for Conduit images")
 	cmd.PersistentFlags().StringVar(&options.initImage, "init-image", options.initImage, "Conduit init container image name")
 	cmd.PersistentFlags().StringVar(&options.proxyImage, "proxy-image", options.proxyImage, "Conduit proxy container image name")
+	cmd.PersistentFlags().StringVar(&options.dockerRegistry, "registry", options.dockerRegistry, "Docker registry to pull images from")
 	cmd.PersistentFlags().StringVar(&options.imagePullPolicy, "image-pull-policy", options.imagePullPolicy, "Docker image pull policy")
 	cmd.PersistentFlags().Int64Var(&options.proxyUID, "proxy-uid", options.proxyUID, "Run the proxy under this user ID")
 	cmd.PersistentFlags().StringVar(&options.proxyLogLevel, "proxy-log-level", options.proxyLogLevel, "Log level for the proxy")
