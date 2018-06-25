269714ab354a56a0145ebb6ce865a4b2ffbb3234
Risha Mars <mars@buoyant.io>
2018-06-25 14:37:16 -0700

    Re-add sidebar links to individual resource pages  (#1191)
    Add Sidebar links to Pods, Deployments, and Replication Controllers
    
    In #1016 we removed the sidebar links to individual resource pages in favour of a namespace
    page that lists all resources. These resource pages require no additional code so they're still 
    in our UI (accessible under /pods, /deployments etc), just not easily findable. I find them 
    useful to check when in development mode, or when debugging something, so I'd like to 
    re-add links.
    
    If we don't want them in permanently, we can gate them behind `NODE_ENV=development`

--- web/app/js/components/Sidebar.jsx
+++ web/app/js/components/Sidebar.jsx
@@ -180,6 +180,15 @@ class Sidebar extends React.Component {
                 : null
             }
 
+            <Menu.SubMenu
+              className="sidebar-menu-item"
+              key="byresource"
+              title={<span className="sidebar-title"><Icon type="bars" />{this.state.collapsed ? "" : "Resources"}</span>}>
+              <Menu.Item><ConduitLink to="/deployments">Deployments</ConduitLink></Menu.Item>
+              <Menu.Item><ConduitLink to="/replicationcontrollers">Replication Controllers</ConduitLink></Menu.Item>
+              <Menu.Item><ConduitLink to="/pods">Pods</ConduitLink></Menu.Item>
+            </Menu.SubMenu>
+
             <Menu.Item className="sidebar-menu-item" key="/docs">
               <Link to="https://conduit.io/docs/" target="_blank">
                 <Icon type="solution" />
