c8aeb27686ad1d77c87284654085035eecc4dfa2
Andrew Seigner <siggy@buoyant.io>
2018-06-28 15:36:33 -0700

    Set outbound capacity on proxy in Prometheus pod
    The `conduit-proxy` injected into the control-plane's Prometheus pod
    makes requests to every other mesh-enabled pod in the cluster. For a
    large number of pods (>300), this triggered
    `conduit_proxy router at capacity` errors from the proxy, as the default
    outbound capacity is set at 100.
    
    This change increases `CONDUIT_PROXY_OUTBOUND_ROUTER_CAPACITY` from 100
    to 10000 on the `conduit-proxy` running in the Prometheus pod.
    
    Relates to
    https://github.com/runconduit/conduit/issues/1179#issuecomment-400800995
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -76,18 +76,19 @@ func newPublicAPIClient() (pb.ApiClient, error) {
 }
 
 type proxyConfigOptions struct {
-	conduitVersion   string
-	proxyImage       string
-	initImage        string
-	dockerRegistry   string
-	imagePullPolicy  string
-	proxyUID         int64
-	proxyLogLevel    string
-	proxyBindTimeout string
-	proxyAPIPort     uint
-	proxyControlPort uint
-	proxyMetricsPort uint
-	tls              string
+	conduitVersion        string
+	proxyImage            string
+	initImage             string
+	dockerRegistry        string
+	imagePullPolicy       string
+	proxyUID              int64
+	proxyLogLevel         string
+	proxyBindTimeout      string
+	proxyAPIPort          uint
+	proxyControlPort      uint
+	proxyMetricsPort      uint
+	proxyOutboundCapacity map[string]uint
+	tls                   string
 }
 
 const (
@@ -97,18 +98,19 @@ const (
 
 func newProxyConfigOptions() *proxyConfigOptions {
 	return &proxyConfigOptions{
-		conduitVersion:   version.Version,
-		proxyImage:       defaultDockerRegistry + "/proxy",
-		initImage:        defaultDockerRegistry + "/proxy-init",
-		dockerRegistry:   defaultDockerRegistry,
-		imagePullPolicy:  "IfNotPresent",
-		proxyUID:         2102,
-		proxyLogLevel:    "warn,conduit_proxy=info",
-		proxyBindTimeout: "10s",
-		proxyAPIPort:     8086,
-		proxyControlPort: 4190,
-		proxyMetricsPort: 4191,
-		tls:              "",
+		conduitVersion:        version.Version,
+		proxyImage:            defaultDockerRegistry + "/proxy",
+		initImage:             defaultDockerRegistry + "/proxy-init",
+		dockerRegistry:        defaultDockerRegistry,
+		imagePullPolicy:       "IfNotPresent",
+		proxyUID:              2102,
+		proxyLogLevel:         "warn,conduit_proxy=info",
+		proxyBindTimeout:      "10s",
+		proxyAPIPort:          8086,
+		proxyControlPort:      4190,
+		proxyMetricsPort:      4191,
+		proxyOutboundCapacity: map[string]uint{},
+		tls: "",
 	}
 }
 
