a685dba8731f640ad429616878b7c3906c436d2d
Kevin Lingerfelt <kl@buoyant.io>
2018-06-29 14:28:13 -0700

    Use parent name instead of pod name in identity string (#1236)
    * Use parent name instead of pod name in identity string
    * Update protobuf comment
    
    Signed-off-by: Kevin Lingerfelt <kl@buoyant.io>

--- controller/destination/listener_test.go
+++ controller/destination/listener_test.go
@@ -164,7 +164,7 @@ func TestEndpointListener(t *testing.T) {
 		expectedConduitNamespace := "conduit-namespace"
 		expectedPodDeployment := "pod-deployment"
 		expectedTlsIdentity := &pb.TlsIdentity_K8SPodIdentity{
-			PodIdentity:  "pod1.deployment.this-namespace.conduit-managed.conduit-namespace.svc.cluster.local",
+			PodIdentity:  "pod-deployment.deployment.this-namespace.conduit-managed.conduit-namespace.svc.cluster.local",
 			ControllerNs: "conduit-namespace",
 		}
 
