0a51a36ad8459ea54c97516a23b24fb40a8d0b36
Oliver Gould <ver@buoyant.io>
2018-07-09 20:30:56 +0000

    protoc seems to work?

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -7,7 +7,7 @@ gen() {
     mkdir controller/gen
 
     for f in $@; do
-        sh -x bin/protoc -I proto --go_out="plugins=grpc:$GOPATH/src" "$f"
+        bin/protoc -I proto --go_out="plugins=grpc:$GOPATH/src" "$f"
     done
 
     git add controller/gen
