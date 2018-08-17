5487c535688ed886a04ade9a6cef6789a01e54cd
Risha Mars <mars@buoyant.io>
2018-08-16 17:04:50 -0700

    Add the svg code into a jsx module so we can import them without worryign about relative paths

--- web/app/js/components/GrafanaLink.jsx
+++ web/app/js/components/GrafanaLink.jsx
@@ -1,25 +1,16 @@
-import grafanaIcon from './../../img/grafana.svg';
+import { grafanaIcon } from './util/SvgWrappers.jsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const GrafanaLink = ({PrefixedLink, name, namespace, resource}) => {
-  const iconSize = 15;
+
   return (
     <PrefixedLink
       to={`/dashboard/db/linkerd-${resource}?var-namespace=${namespace}&var-${resource}=${name}`}
       deployment="grafana"
       targetBlank={true}>
       &nbsp;&nbsp;
-      <img
-        src={grafanaIcon}
-        onError={e => {
-          // awful hack to deal with the fact that we don't serve assets off absolute paths
-          e.target.src = e.target.src.replace(/(.*)(\/[a-zA-Z]*)(\/dist)(.*)/, "$1$3$4");
-        }}
-        width={iconSize}
-        height={iconSize}
-        title={`${resource} grafana dashboard`}
-        alt={`link to ${resource} grafana dashboard`} />
+      {grafanaIcon}
     </PrefixedLink>
   );
 };
