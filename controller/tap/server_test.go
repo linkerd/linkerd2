fff09c5d063938d1e14db6c402b1c27519d770c2
Risha Mars <mars@buoyant.io>
2018-08-28 09:59:52 -0700

    Only tap pods that are meshed (#1535)
    Previously, we would tap any resource's pods, regardless of whether the pods
    were meshed or not. We can't actually tap non-meshed pods, so I'm adding a check
    that will filter out non-meshed pods from the pods that tap watches.
    
    Previous behaviour:
    When attempting to hang a non meshed pod, it would establish
    a watch on the pods, but then never return any results. In the CLI you could
    just cancel it with Ctrl-C. In the web, clicking Stop would send a
    WebSocket.close(1000) but wouldn't actually close the connection... 
    
    Behaviour after change :
    If no pods under the specified resource are meshed, it'll
    return an error of no pods being found to tap

--- controller/tap/server_test.go
+++ controller/tap/server_test.go
@@ -35,6 +35,7 @@ metadata:
   namespace: emojivoto
   labels:
     app: emoji-svc
+    linkerd.io/control-plane-ns: controller-ns
   annotations:
     linkerd.io/proxy-version: testinjectversion
 status:
@@ -52,6 +53,30 @@ status:
 					Match: &public.TapByResourceRequest_Match{
 						Match: &public.TapByResourceRequest_Match_Any{
 							Any: &public.TapByResourceRequest_Match_Seq{},
+						},
+					},
+				},
+			},
+			tapExpected{
+				msg: "rpc error: code = NotFound desc = no pods found for ResourceSelection: {Resource:namespace:\"emojivoto\" type:\"pod\" name:\"emojivoto-not-meshed\"  LabelSelector:}",
+				k8sRes: []string{`
+apiVersion: v1
+kind: Pod
+metadata:
+  name: emojivoto-not-meshed
+  namespace: emojivoto
+  labels:
+    app: emoji-svc
+status:
+  phase: Running
+`,
+				},
+				req: public.TapByResourceRequest{
+					Target: &public.ResourceSelection{
+						Resource: &public.Resource{
+							Namespace: "emojivoto",
+							Type:      pkgK8s.Pod,
+							Name:      "emojivoto-not-meshed",
 						},
 					},
 				},
@@ -134,6 +159,7 @@ metadata:
   namespace: emojivoto
   labels:
     app: emoji-svc
+    linkerd.io/control-plane-ns: controller-ns
   annotations:
     linkerd.io/proxy-version: testinjectversion
 status:
@@ -163,7 +189,7 @@ status:
 				t.Fatalf("NewFakeAPI returned an error: %s", err)
 			}
 
-			server, listener, err := NewServer("localhost:0", 0, k8sAPI)
+			server, listener, err := NewServer("localhost:0", 0, "controller-ns", k8sAPI)
 			if err != nil {
 				t.Fatalf("NewServer error: %s", err)
 			}
