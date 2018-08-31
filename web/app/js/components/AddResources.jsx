855bda225c8860397be281269d781d5ab75a84fa
Risha Mars <mars@buoyant.io>
2018-08-31 12:43:31 -0700

    Use firendlyTitle insteald of startCase

--- web/app/js/components/AddResources.jsx
+++ web/app/js/components/AddResources.jsx
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { friendlyTitle } from './util/Utils.js';
 import { incompleteMeshMessage } from './util/CopyUtils.jsx';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -14,7 +15,7 @@ export default class AddResources extends React.Component {
 
     return (
       <div className="mesh-completion-message">
-        {_.startCase(resourceType)} {resourceName} is not in the mesh.
+        {friendlyTitle(resourceType).singular} {resourceName} is not in the mesh.
         {incompleteMeshMessage()}
       </div>
     );
