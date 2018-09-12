8d4271d396b8c064ee8f5bee6172072d2eca3bf2
Dennis Adjei-Baah <dennis@buoyant.io>
2018-09-12 14:21:46 -0700

    adjust height pixels and fix global font to Lato
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -52,6 +52,22 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              modifyVars: {
+                'font-family': '\'Lato\', helvetica, arial, sans-serif'
+              },
+              javascriptEnabled: true
+            }
+          }
+        ]
       }
     ]
   }
