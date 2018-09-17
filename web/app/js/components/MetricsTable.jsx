9a7bad1354ab8c1a41a38354a917bf7f6240bbac
Risha Mars <mars@buoyant.io>
2018-09-17 11:45:01 -0700

    remove metric tooltip on RPS column

--- web/app/js/components/MetricsTable.jsx
+++ web/app/js/components/MetricsTable.jsx
@@ -19,16 +19,6 @@ import { processedMetricsPropType, successRateWithMiniChart } from './util/Metri
 */
 const smMetricColWidth = "70px";
 
-const withTooltip = (d, metricName, hideUnit) => {
-  return (
-    <Tooltip
-      title={metricToFormatter["UNTRUNCATED"](d)}
-      overlayStyle={{ fontSize: "12px" }}>
-      <span>{ !hideUnit ? metricToFormatter[metricName](d) : metricToFormatter["NO_UNIT"](d)}</span>
-    </Tooltip>
-  );
-};
-
 const formatTitle = (title, tooltipText) => {
   if (!tooltipText) {
     return title;
@@ -130,7 +120,7 @@ const columnDefinitions = (resource, namespaces, onFilterClick, showNamespaceCol
       className: "numeric",
       width: smMetricColWidth,
       sorter: (a, b) => numericSort(a.requestRate, b.requestRate),
-      render: d => withTooltip(d, "REQUEST_RATE", true)
+      render: metricToFormatter["NO_UNIT"]
     },
     {
       title: formatTitle("P50", "P50 Latency"),
