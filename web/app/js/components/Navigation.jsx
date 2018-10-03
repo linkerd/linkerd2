e40a7076b81ea27c7f86ab69602612ba2bd3d59f
Risha Mars <mars@buoyant.io>
2018-10-03 16:35:38 -0700

    Nav and content touchups
    Fix icons in Nav
    
    Add fixed width to navbar and content so that we don't get odd hanging navbar at end

--- web/app/js/components/Navigation.jsx
+++ web/app/js/components/Navigation.jsx
@@ -5,38 +5,41 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import classNames from 'classnames';
 import Collapse from '@material-ui/core/Collapse';
-import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import GroupWorkIcon from  '@material-ui/icons/GroupWork';
-import HomeIcon from '@material-ui/icons/Home';
+import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import {Link} from 'react-router-dom';
-import { linkerdWordLogo } from './util/SvgWrappers.jsx';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import WavesIcon from '@material-ui/icons/Waves';
 import { withContext } from './util/AppContext.jsx';
 import { withStyles } from '@material-ui/core/styles';
+import {
+  Business as BusinessIcon,
+  Dashboard as DashboardIcon,
+  ExpandLess,
+  ExpandMore,
+  Home as HomeIcon,
+  Menu as MenuIcon,
+  Notifications as NotificationsIcon,
+  Pageview as PageviewIcon
+} from '@material-ui/icons';
+import { linkerdWordLogo, navIconTop } from './util/SvgWrappers.jsx';
 
 const drawerWidth = 250;
 const styles = theme => ({
   root: {
     flexGrow: 1,
     height: "100vh",
-    width: "100vw",
+    width: "100%",
     zIndex: 1,
     overflowX: 'scroll',
     position: 'relative',
@@ -93,6 +96,7 @@ const styles = theme => ({
   },
   content: {
     flexGrow: 1,
+    width: `calc(100% - ${drawerWidth}px)`,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3,
   },
@@ -126,22 +130,38 @@ class NavigationBase extends React.Component {
           position="absolute"
           className={classNames(classes.appBar, this.state.drawerOpen && classes.appBarShift)}>
           <Toolbar disableGutters={!this.state.drawerOpen}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, this.state.drawerOpen && classes.hide)}>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              <BreadcrumbHeader {...this.props} />
-            </Typography>
-            <div className={classes.grow} />
-            <IconButton color="inherit">
-              <Badge className={classes.margin} badgeContent="1" color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center">
+              <Grid item xs={1}>
+                <IconButton
+                  color="inherit"
+                  aria-label="Open drawer"
+                  onClick={this.handleDrawerOpen}
+                  className={classNames(classes.menuButton, this.state.drawerOpen && classes.hide)}>
+                  <MenuIcon />
+                </IconButton>
+              </Grid>
+              <Grid item xs={10}>
+                <Typography variant="title" color="inherit" noWrap>
+                  <BreadcrumbHeader {...this.props} />
+                </Typography>
+              </Grid>
+
+              <Grid item xs={1}>
+                <Typography variant="subheading" align="center">
+                  <IconButton color="inherit">
+                    <Badge className={classes.margin} badgeContent="1" color="secondary">
+                      <NotificationsIcon />
+                    </Badge>
+                  </IconButton>
+                </Typography>
+              </Grid>
+            </Grid>
+
+
           </Toolbar>
         </AppBar>
         <Drawer
@@ -163,42 +183,42 @@ class NavigationBase extends React.Component {
               <ListItemText primary="Overview" />
             </ListItem>
             <ListItem component={Link} to={prefixLink("/tap")}>
-              <ListItemIcon><CompareArrowsIcon /></ListItemIcon>
+              <ListItemIcon>{navIconTop}</ListItemIcon>
               <ListItemText primary="Tap" />
             </ListItem>
             <ListItem component={Link} to={prefixLink("/top")}>
-              <ListItemIcon><WavesIcon /></ListItemIcon>
+              <ListItemIcon><PageviewIcon /></ListItemIcon>
               <ListItemText primary="Top" />
             </ListItem>
             <ListItem component={Link} to={prefixLink("/servicemesh")}>
-              <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+              <ListItemIcon><BusinessIcon /></ListItemIcon>
               <ListItemText primary="ServiceMesh" />
             </ListItem>
             <ListItem button onClick={this.handleResourceMenuClick}>
-              <ListItemIcon><WavesIcon /></ListItemIcon>
+              <ListItemIcon><DashboardIcon /></ListItemIcon>
               <ListItemText inset primary="Resources" />
               {this.state.resourceMenuOpen ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
             <Collapse in={this.state.resourceMenuOpen} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
                 <ListItem component={Link} to={prefixLink("/authorities")} className={classes.nested}>
-                  <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+                  <ListItemIcon><DashboardIcon /></ListItemIcon>
                   <ListItemText primary="Authorities" />
                 </ListItem>
                 <ListItem component={Link} to={prefixLink("/deployments")} className={classes.nested}>
-                  <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+                  <ListItemIcon><DashboardIcon /></ListItemIcon>
                   <ListItemText primary="Deployments" />
                 </ListItem>
                 <ListItem component={Link} to={prefixLink("/namespaces")} className={classes.nested}>
-                  <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+                  <ListItemIcon><DashboardIcon /></ListItemIcon>
                   <ListItemText primary="Namespaces" />
                 </ListItem>
                 <ListItem component={Link} to={prefixLink("/pods")} className={classes.nested}>
-                  <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+                  <ListItemIcon><DashboardIcon /></ListItemIcon>
                   <ListItemText primary="Pods" />
                 </ListItem>
                 <ListItem component={Link} to={prefixLink("/replicationcontrollers")} className={classes.nested}>
-                  <ListItemIcon><GroupWorkIcon /></ListItemIcon>
+                  <ListItemIcon><DashboardIcon /></ListItemIcon>
                   <ListItemText primary="Replication Controllers" />
                 </ListItem>
               </List>
@@ -207,16 +227,16 @@ class NavigationBase extends React.Component {
           <Divider />
           <List
             component="nav"
-            subheader={<ListSubheader component="div">Nested List Items</ListSubheader>}>
+            subheader={!this.state.drawerOpen ? null : <ListSubheader component="div">Nested List Items</ListSubheader>}>
             <ListItem button>
               <ListItemIcon>
-                <WavesIcon />
+                <DashboardIcon />
               </ListItemIcon>
               <ListItemText inset primary="Sent mail" />
             </ListItem>
             <ListItem button>
               <ListItemIcon>
-                <WavesIcon />
+                <DashboardIcon />
               </ListItemIcon>
               <ListItemText inset primary="Drafts" />
             </ListItem>
