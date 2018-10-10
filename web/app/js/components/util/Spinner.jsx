4d31661a62393b7c8095a85c6eb37275d1ec296f
Risha Mars <mars@buoyant.io>
2018-10-10 11:51:02 -0700

    Touchups: Spinner

--- web/app/js/components/util/Spinner.jsx
+++ web/app/js/components/util/Spinner.jsx
@@ -1,14 +1,12 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
-import purple from '@material-ui/core/colors/purple';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = () => ({
   progress: {
-    "margin-left": "auto",
-    "margin-right": "auto"
+    "margin": "auto",
   },
 });
 
@@ -16,8 +14,8 @@ function CircularIndeterminate(props) {
   const { classes } = props;
   return (
     <Grid container justify="center">
-      <div style={{ marginTop: "100px" }}>
-        <CircularProgress className={classes.progress} style={{ color: purple[500] }} thickness={7} />
+      <div >
+        <CircularProgress className={classes.progress} style={{ color: "#26E99D" }} />
       </div>
     </Grid>
   );
