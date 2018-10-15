04153239b706ac20fa42c292363950f30491aa28
Risha Mars <mars@buoyant.io>
2018-10-15 15:20:16 -0700

    Move GrafanaLink.test.jsx

--- web/app/js/components/GrafanaLink.test.jsx
+++ web/app/js/components/GrafanaLink.test.jsx
@@ -1,14 +1,7 @@
-import Adapter from 'enzyme-adapter-react-16';
 import ApiHelpers from './util/ApiHelpers.jsx';
-import { expect } from 'chai';
 import GrafanaLink from './GrafanaLink.jsx';
 import { routerWrap } from '../../test/testHelpers.jsx';
-import sinon from 'sinon';
-import sinonStubPromise from 'sinon-stub-promise';
-import Enzyme, { mount } from 'enzyme';
-
-Enzyme.configure({ adapter: new Adapter() });
-sinonStubPromise(sinon);
+import { mount } from 'enzyme';
 
 describe('GrafanaLink', () => {
   it('makes a link', () => {
@@ -25,9 +18,12 @@ describe('GrafanaLink', () => {
     let expectedNsStr = "var-namespace=myns";
     let expectedVarNameStr = "var-replicationcontroller=aldksf-3409823049823";
 
-    expect(component.find("GrafanaLink")).to.have.length(1);
-    expect(component.html()).to.contain(expectedDashboardNameStr);
-    expect(component.html()).to.contain(expectedNsStr);
-    expect(component.html()).to.contain(expectedVarNameStr);
+    expect(component.find("GrafanaLink")).toHaveLength(1);
+
+    const href = component.find('a').props().href;
+
+    expect(href).toContain(expectedDashboardNameStr);
+    expect(href).toContain(expectedNsStr);
+    expect(href).toContain(expectedVarNameStr);
   });
 });
