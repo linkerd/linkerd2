7e8a0ce4267b75f6924b47b4c1c04db82e032343
Risha Mars <mars@buoyant.io>
2018-10-16 10:53:32 -0700

    Fixed tests: MetricUtils.test.js

--- web/app/js/components/util/MetricUtils.test.js
+++ web/app/js/components/util/MetricUtils.test.js
@@ -17,6 +17,7 @@ describe('MetricUtils', () => {
           name: 'voting',
           namespace: 'emojivoto',
           type: 'deployment',
+          key: "emojivoto-deployment-voting",
           requestRate: 2.5,
           successRate: 0.9,
           totalRequests: 150,
