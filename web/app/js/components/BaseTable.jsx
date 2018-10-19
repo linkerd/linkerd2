3b4808a363218ffefac2c96e3851594963ae32b5
Risha Mars <mars@buoyant.io>
2018-10-19 10:13:26 -0700

    Add marginBottom to BaseTable

--- web/app/js/components/BaseTable.jsx
+++ web/app/js/components/BaseTable.jsx
@@ -13,6 +13,7 @@ const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
   table: {},
