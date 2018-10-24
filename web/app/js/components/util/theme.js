c19848c3f65db500c31bf631899754d2d65464ce
Risha Mars <mars@buoyant.io>
2018-10-24 14:46:40 -0700

    Adjust the placing of custom variables in theme so tests can pass

--- web/app/js/components/util/theme.js
+++ web/app/js/components/util/theme.js
@@ -3,6 +3,24 @@ import grey from '@material-ui/core/colors/grey';
 import orange from '@material-ui/core/colors/orange';
 import red from '@material-ui/core/colors/red';
 
+const status = {
+  // custom variables for success rate indicators
+  dark: {
+    danger: red[500],
+    warning: orange[500],
+    good: green[500],
+    default: grey[500],
+  },
+  // custom variables for progress bars, which need both the normal colors
+  // as well as a lighter version of them for the bar background
+  light: {
+    danger: red[200],
+    warning: orange[200],
+    good: green[200],
+    default: grey[200],
+  }
+};
+
 export const dashboardTheme = {
   palette: {
     primary: green
@@ -11,36 +29,26 @@ export const dashboardTheme = {
     useNextVariants: true,
     suppressDeprecationWarnings: true // https://github.com/mui-org/material-ui/issues/13175
   },
-  status: {
-    // custom variables for success rate indicators
-    dark: {
-      danger: red[500],
-      warning: orange[500],
-      good: green[500],
-      default: grey[500],
-    },
-    // custom variables for progress bars, which need both the normal colors
-    // as well as a lighter version of them for the bar background
-    light: {
-      danger: red[200],
-      warning: orange[200],
-      good: green[200],
-      default: grey[200],
-    }
-  },
+  status
 };
 
-export const statusClassNames = theme => ({
-  poor: {
-    backgroundColor: theme.status.dark.danger,
-  },
-  warning: {
-    backgroundColor: theme.status.dark.warning,
-  },
-  good: {
-    backgroundColor: theme.status.dark.good,
-  },
-  default: {
-    backgroundColor: theme.status.dark.default,
-  },
-});
+
+
+export const statusClassNames = theme => {
+  theme.status = theme.status || status; // tests don't inject custom variables
+
+  return {
+    poor: {
+      backgroundColor: theme.status.dark.danger,
+    },
+    warning: {
+      backgroundColor: theme.status.dark.warning,
+    },
+    good: {
+      backgroundColor: theme.status.dark.good,
+    },
+    default: {
+      backgroundColor: theme.status.dark.default,
+    },
+  };
+};
