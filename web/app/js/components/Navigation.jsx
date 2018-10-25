f2efc1b5c7a26ad3d202e645798419d4c7c406c3
Risha Mars <mars@buoyant.io>
2018-10-24 17:56:01 -0700

    Small tweak to kick CI

--- web/app/js/components/Navigation.jsx
+++ web/app/js/components/Navigation.jsx
@@ -49,6 +49,7 @@ const styles = theme => ({
     display: 'flex',
   },
   appBar: {
+    color: 'white',
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
