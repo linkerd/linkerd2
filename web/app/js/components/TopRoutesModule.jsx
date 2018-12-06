2ca9184be06ed4bcf3265354c6079d2bc3d3050c
Risha Mars <mars@buoyant.io>
2018-12-05 16:06:21 -0800

    Show the call to action if all metric rows are UNKNOWN

--- web/app/js/components/TopRoutesModule.jsx
+++ web/app/js/components/TopRoutesModule.jsx
@@ -41,12 +41,14 @@ class TopRoutesBase extends React.Component {
   render() {
     const {data, loading} = this.props;
     let metrics = processTopRoutesResults(_.get(data, '[0].routes.rows', []));
+    let noServiceProfile = _.isEmpty(metrics) || _.every(metrics, m => m.route === "UNKNOWN");
+    let showCallToAction = !loading && noServiceProfile;
 
     return (
       <React.Fragment>
         {this.loading()}
         {this.banner()}
-        { !loading && _.isEmpty(metrics) ? <ConfigureProfilesMsg /> : null}
+        { showCallToAction ? <ConfigureProfilesMsg /> : null}
         <TopRoutesTable rows={metrics} />
       </React.Fragment>
     );
