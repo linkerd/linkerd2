9d15181269a1013d223268a6f256b1ce5696c631
Risha Mars <mars@buoyant.io>
2018-12-13 14:46:06 -0800

    Update QueryToCliCmd to include the linkerd namespace if it is non-default

--- web/app/js/components/QueryToCliCmd.jsx
+++ web/app/js/components/QueryToCliCmd.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
 import { displayOrder } from './util/CliQueryUtils.js';
+import { withContext } from './util/AppContext.jsx';
 
 const toCliParam = {
   "namespace": "--namespace",
@@ -25,9 +26,10 @@ const toCliParam = {
   prints a given linkerd api query in an equivalent CLI format, such that it
   could be pasted into a terminal
 */
-export default class QueryToCliCmd extends React.Component {
+class QueryToCliCmd extends React.Component {
   static propTypes = {
     cmdName: PropTypes.string.isRequired,
+    controllerNamespace: PropTypes.string.isRequired,
     query: PropTypes.shape({}).isRequired,
     resource: PropTypes.string.isRequired
   }
@@ -37,7 +39,7 @@ export default class QueryToCliCmd extends React.Component {
   }
 
   render = () => {
-    let { cmdName, query, resource } = this.props;
+    let { cmdName, query, resource, controllerNamespace } = this.props;
 
     return (
       _.isEmpty(resource) ? null :
@@ -51,8 +53,11 @@ export default class QueryToCliCmd extends React.Component {
           { _.map(displayOrder(cmdName, query), item => {
             return !toCliParam[item] ? null : this.renderCliItem(toCliParam[item], query[item]);
           })}
+          { controllerNamespace === "linkerd" ? null : ` --linkerd-namespace ${controllerNamespace}`}
         </code>
       </CardContent>
     );
   }
 }
+
+export default withContext(QueryToCliCmd);
