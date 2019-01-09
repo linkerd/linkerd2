ce61c079185fe9995594bd8b5a01d075a950696c
Dennis Adjei-Baah <dennis@buoyant.io>
2019-01-09 13:43:23 -0800

    add test for logFlags validation
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

--- cli/cmd/logs_test.go
+++ cli/cmd/logs_test.go
@@ -1,58 +1,73 @@
 package cmd
 
 import (
-	"fmt"
+	"bytes"
+	"errors"
 	"testing"
 
+	"github.com/fatih/color"
 	"github.com/wercker/stern/stern"
 )
 
-func assertConfig(actual, expected *stern.Config) bool{
+func assertConfig(actual, expected *stern.Config) bool {
 	return actual.Template != nil &&
-		actual.ContainerState == expected.ContainerState
+		actual.ContainerState == expected.ContainerState &&
+		actual.PodQuery != nil
 }
 func TestNewSternConfig(t *testing.T) {
-	var (
-		tests = []struct {
-			testMessage string
-			flags          *logFlags
-			expectedConfig *stern.Config
-			expectedErr    error
-		}{
-			{
-				"default flags",
-				&logFlags{
-					containerState: "running",
-					tail: -1,
-				},
-				&stern.Config{
-				},
-				nil,
-			},
-			{
-				"valid pod regex",
-				&logFlags{
-					containerState: "running",
-					tail: -1,
-				},
-				&stern.Config{
-				},
-				nil,
-			},
-		}
-	)
-	for _, tt := range tests {
-		t.Run(tt.testMessage, func(t *testing.T) {
-			c, err := tt.flags.NewSternConfig()
-			if assertConfig(c, tt.expectedConfig) {
-				fmt.Printf("Error: %v", err)
-				t.Fatalf("Unexpected config:\ngot: %+v\nexpected: %+v", c, tt.expectedConfig)
-			}
+	t.Run("Default log template", func(t *testing.T) {
+		flags := &logFlags{containerState:"running"}
+		testColor := color.New(color.FgHiRed)
+		expectedLogLine := "linkerd-controller-xyz web-container Starting server on port 8084"
+		buf := bytes.Buffer{}
+		c, err := flags.NewSternConfig()
 
-			if err != tt.expectedErr {
-				t.Fatalf("Unexpected err:\ngot: %+v\nexpected: %+v", err, tt.expectedErr)
-			}
+		if err != nil {
+			t.Fatalf("Error creating stern config: %s", err.Error())
+		}
+
+		err = c.Template.Execute(&buf, &stern.Log{
+			Message:        "Starting server on port 8084",
+			Namespace:      "linkerd",
+			PodName:        "linkerd-controller-xyz",
+			ContainerName:  "web-container",
+			PodColor:       testColor,
+			ContainerColor: testColor,
 		})
 
-	}
+		if err != nil{
+			t.Fatalf("Unexpected error: %s", err.Error())
+		}
+
+		if buf.String() != expectedLogLine {
+			t.Fatalf("Invalid log line format\n got: %s\n expected:%s\n", buf.String(), expectedLogLine)
+		}
+	})
+	
+	t.Run("label selector creation", func(t *testing.T) {
+		testCases := []struct{
+			labelStr string
+			expectedLabelSelector string
+			expectedErr error
+		}{
+			{labelStr:"", expectedLabelSelector:""},
+			{labelStr:"app=frontend", expectedLabelSelector:"app=frontend"},
+			{labelStr:"=app=invalid", expectedLabelSelector:"", expectedErr: errors.New("found '=', expected: !, identifier, or 'end of string'")},
+		}
+
+		for _, tt := range testCases{
+			flags := logFlags{
+				labelSelector: tt.labelStr,
+				containerState: "running",
+			}
+			c, err := flags.NewSternConfig()
+			if err != nil && err.Error() != tt.expectedErr.Error() {
+				t.Fatalf("Unexpected error: %s", err.Error())
+			}
+
+			if c != nil && c.LabelSelector.String() != tt.expectedLabelSelector{
+				t.Fatalf("Error creating label selector: %s", c.LabelSelector.String())
+			}
+		}
+	})
 }
