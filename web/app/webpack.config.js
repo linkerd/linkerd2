c22328d4f6da41010bd0a38a25ea47550d3cb356
Andrew Seigner <siggy@buoyant.io>
2019-01-14 10:15:27 -0800

    review feedback: moves and renames
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- web/app/webpack.config.js
+++ web/app/webpack.config.js
@@ -4,18 +4,12 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const WebpackMv = require('./webpack-mv.js');
-
-// analyze plugin speeds
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-// disable by default until this is fixed:
-// https://github.com/stephencookdev/speed-measure-webpack-plugin/issues/44
-const smp = new SpeedMeasurePlugin({disable: true});
+const WebpackMvPlugin = require('./webpack-mv-plugin.js');
 
 // uncomment here and in plugins to analyze webpack bundle size
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = smp.wrap({
+module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './js/index.js',
   devServer: {
@@ -76,13 +70,13 @@ module.exports = smp.wrap({
     new HtmlWebpackPlugin({
       inject: false,
       filename: "index_bundle.js.out",
-      template: 'lodash/index_bundle.js.lodash.tmpl',
+      template: 'index_bundle.js.lodash.tmpl',
     }),
     new LodashModuleReplacementPlugin({
       // 'chain': true,
       'collections': true,
       'paths': true
     }),
-    new WebpackMv()
+    new WebpackMvPlugin()
   ]
-});
+};
