4d58d2309d56bf16329f880599b7bea06c195d8b
David Capino <david.capino@gmail.com>
2019-02-07 16:17:04 -0800

    capino/cni-int-tests adding user input to make sure they are ok with killing their minikube, eventually this will be all in GKE using the travis CI
    Signed-off-by: David Capino <david.capino@gmail.com>

--- cni-plugin/integration_test/run_all_tests.sh
+++ cni-plugin/integration_test/run_all_tests.sh
@@ -22,6 +22,13 @@ doesCalicoExist() {
   if [ "$trimmedmatch" != "" ]; then echo "0"; else echo "1"; fi
 }
 
+echo "Running this CNI integration test will destroy and recreate your existing minikube installation"
+read -p "Are you sure? (y/Y)" kill_minikube
+if [ "$kill_minikube" != "y" -a "$kill_minikube" != "Y" ]
+then
+  exit 1
+fi
+
 header "Killing Minikube"
 minikube stop
 minikube delete
@@ -30,22 +37,20 @@ rm -rf ~/.minikube
 header "Creating Minikube"
 minikube start --kubernetes-version v1.10.8 --memory 8192 --cpus 4 --network-plugin=cni --extra-config=kubelet.network-plugin=cni
 
-#echo "-------------------Building deps and copying images to Minikube"
-#./../../bin/update-go-deps-shas
-#./../../bin/build-cli-bin
-#./../../bin/mkube ./../../bin/docker-build
-header "-------------------Docker saving"
-docker save gcr.io/linkerd-io/cni-plugin:dev-66070c26-x37y | (eval $(minikube docker-env) && docker load)
-docker save gcr.io/linkerd-io/proxy:dev-66070c26-x37y | (eval $(minikube docker-env) && docker load)
+header "Building deps and copying images to Minikube"
+./../../bin/update-go-deps-shas
+./../../bin/build-cli-bin
+./../../bin/mkube ./../../bin/docker-build
+header "Docker saving"
 
 header "Applying Calico"
 kubectl apply -f ./calico-etcd.yaml
 kubectl apply -f ./calico.yaml
 
 header "Waiting for Calico components to become ready"
-kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-etcd
-kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-kube-controllers
-kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-node
+kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-etcd --timeout=30s
+kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-kube-controllers --timeout=30s
+kubectl wait --for=condition=ready pod -n kube-system -l k8s-app=calico-node --timeout=30s
 
 header "Discover the calico conf file in /etc/cni/net.d"
 # adapted from https://superuser.com/questions/878640/unix-script-wait-until-a-file-exists
@@ -75,7 +80,7 @@ header "Applying linkerd-cni plugin"
 ./../../target/cli/darwin/linkerd install-cni --inbound-port 8080 --outbound-port 8080 --proxy-uid 2102 | kubectl apply -f -
 
 header "Waiting for linkerd-cni components to become ready"
-kubectl wait --for=condition=ready pod -n linkerd -l k8s-app=linkerd-cni
+kubectl wait --for=condition=ready pod -n linkerd -l k8s-app=linkerd-cni --timeout=30s
 
 header "Running tests"
 ./../../bin/mkube ./test_setup.sh
