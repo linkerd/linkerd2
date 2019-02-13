63952634504e7a58949061a55c17958472c1d4f2
lucperkins <lucperkins@gmail.com>
2019-02-13 14:30:40 -0800

    Run gofmt on new CLI command
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- cli/cmd/doc.go
+++ cli/cmd/doc.go
@@ -21,10 +21,10 @@ func newCmdDoc() *cobra.Command {
 	options := newDocOptions()
 
 	cmd := &cobra.Command{
-		Use: "doc",
+		Use:    "doc",
 		Hidden: true,
-		Short: "Generate YAML documentation for the linkerd executable (hidden command)",
-		Args: cobra.NoArgs,
+		Short:  "Generate YAML documentation for the linkerd executable (hidden command)",
+		Args:   cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return generateDocs(stdout, options)
 		},
@@ -37,7 +37,7 @@ func newCmdDoc() *cobra.Command {
 
 func generateDocs(w io.Writer, options *docOptions) error {
 	fmt.Println("Generating YAML docs...")
-	
+
 	err := doc.GenYamlTree(RootCmd, options.outputPath)
 
 	if err != nil {
