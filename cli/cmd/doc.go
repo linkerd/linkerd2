187984259fc1f4cde8abea7f07a74fe868bf184a
lucperkins <lucperkins@gmail.com>
2019-02-13 14:46:42 -0800

    Require output path for generated YAML
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- cli/cmd/doc.go
+++ cli/cmd/doc.go
@@ -1,44 +1,39 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
-	"io"
 )
 
-type docOptions struct {
-	outputPath string
-}
-
-func newDocOptions() *docOptions {
-	return &docOptions{
-		outputPath: "./",
-	}
-}
+var outputPath string
 
 func newCmdDoc() *cobra.Command {
-	options := newDocOptions()
-
 	cmd := &cobra.Command{
 		Use:    "doc",
 		Hidden: true,
 		Short:  "Generate YAML documentation for the linkerd executable (hidden command)",
 		Args:   cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return generateDocs(stdout, options)
+			return generateDocs(outputPath)
 		},
 	}
 
-	cmd.Flags().StringVarP(&options.outputPath, "output", "o", options.outputPath, "Output path for the resulting YAML file")
+	cmd.Flags().StringVarP(&outputPath, "output", "o", "", "Output path for the resulting YAML files")
 
 	return cmd
 }
 
-func generateDocs(w io.Writer, options *docOptions) error {
+func generateDocs(outputPath string) error {
+	if outputPath == "" {
+		return errors.New("you must specify an output path for the resulting YAML files using the -o or --output flag")
+	}
+
 	fmt.Println("Generating YAML docs...")
 
-	err := doc.GenYamlTree(RootCmd, options.outputPath)
+	err := doc.GenYamlTree(RootCmd, outputPath)
 
 	if err != nil {
 		return err
