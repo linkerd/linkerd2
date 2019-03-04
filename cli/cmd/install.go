28fb72590145d30f61629bc4b1b6481e5637ba5c
Ivan Sim <1330522+ihcsim@users.noreply.github.com>
2019-03-04 08:27:43 -0800

    Revert "Mark HA as non-experimental (#2430)" (#2436)
    This reverts commit ec51ccf31762faa6bed1d3fe3b1f6ffd2f487fea.
    
    Signed-off-by: Ivan Sim <ivan@buoyant.io>

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -146,7 +146,7 @@ func newCmdInstall() *cobra.Command {
 	cmd.PersistentFlags().StringVar(&options.controllerLogLevel, "controller-log-level", options.controllerLogLevel, "Log level for the controller and web components")
 	cmd.PersistentFlags().BoolVar(&options.proxyAutoInject, "proxy-auto-inject", options.proxyAutoInject, "Enable proxy sidecar auto-injection via a webhook (default false)")
 	cmd.PersistentFlags().BoolVar(&options.singleNamespace, "single-namespace", options.singleNamespace, "Experimental: Configure the control plane to only operate in the installed namespace (default false)")
-	cmd.PersistentFlags().BoolVar(&options.highAvailability, "ha", options.highAvailability, "Enable HA deployment config for the control plane (default false)")
+	cmd.PersistentFlags().BoolVar(&options.highAvailability, "ha", options.highAvailability, "Experimental: Enable HA deployment config for the control plane (default false)")
 	cmd.PersistentFlags().Int64Var(&options.controllerUID, "controller-uid", options.controllerUID, "Run the control plane components under this user ID")
 	cmd.PersistentFlags().BoolVar(&options.disableH2Upgrade, "disable-h2-upgrade", options.disableH2Upgrade, "Prevents the controller from instructing proxies to perform transparent HTTP/2 upgrading (default false)")
 	return cmd
