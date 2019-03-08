7ec53edcfd2b1ee49a229e992174af2b75d90213
Kevin Leimkuhler <kevinl@buoyant.io>
2019-03-07 16:42:59 -0800

    Change log Infof to Debugf
    Signed-off-by: Kevin Leimkuhler <kevinl@buoyant.io>

--- controller/k8s/api.go
+++ controller/k8s/api.go
@@ -787,7 +787,7 @@ func (api *API) GetServiceProfileFor(svc *corev1.Service, clientNs string) *spv1
 		}
 	}
 	// Not found; return default.
-	log.Infof("no Service Profile found for '%s' -- using default", dst)
+	log.Debugf("no Service Profile found for '%s' -- using default", dst)
 	return &spv1alpha1.ServiceProfile{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: dst,
