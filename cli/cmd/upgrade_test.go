9ba396d1a16934c1e6ad600427f058838b9a47a1
Andrew Seigner <siggy@buoyant.io>
2019-04-04 18:57:39 -0700

    upgrade: unit tests
    This change introduces some unit tests on individual methods in the
    upgrade code path, along with some minor cleanup.
    
    Part of #2637
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- cli/cmd/upgrade_test.go
+++ cli/cmd/upgrade_test.go
@@ -3,11 +3,13 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	pb "github.com/linkerd/linkerd2/controller/gen/config"
 	"github.com/linkerd/linkerd2/pkg/k8s"
-	"github.com/spf13/pflag"
 )
 
 const upgradeVersion = "TEST-VERSION"
@@ -19,7 +21,13 @@ func testUpgradeOptions() *upgradeOptions {
 }
 
 func TestRenderUpgrade(t *testing.T) {
-	k8sConfigs := []string{`
+	testCases := []struct {
+		k8sConfigs []string
+		outputfile string
+		err        error
+	}{
+		{
+			[]string{`
 kind: ConfigMap
 apiVersion: v1
 metadata:
@@ -36,7 +44,7 @@ data:
     {"proxyImage":{"imageName":"gcr.io/linkerd-io/proxy","pullPolicy":"IfNotPresent"},"proxyInitImage":{"imageName":"gcr.io/linkerd-io/proxy-init","pullPolicy":"IfNotPresent"},"controlPort":{"port":4190},"ignoreInboundPorts":[],"ignoreOutboundPorts":[],"inboundPort":{"port":4143},"adminPort":{"port":4191},"outboundPort":{"port":4140},"resource":{"requestCpu":"","requestMemory":"","limitCpu":"","limitMemory":""},"proxyUid":"2102","logLevel":{"level":"warn,linkerd2_proxy=info"},"disableExternalProfiles":true}
   install: |
     {"uuid":"57af298c-58b0-43fc-8d88-3c338789bfbc","cliVersion":"edge-19.4.1","flags":[]}`,
-		`
+				`
 kind: Secret
 apiVersion: v1
 metadata:
@@ -50,30 +58,46 @@ metadata:
 data:
   crt.pem: LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUJnekNDQVNtZ0F3SUJBZ0lCQVRBS0JnZ3Foa2pPUFFRREFqQXBNU2N3SlFZRFZRUURFeDVwWkdWdWRHbDAKZVM1c2FXNXJaWEprTG1Oc2RYTjBaWEl1Ykc5allXd3dIaGNOTVRrd05EQTBNak0xTXpNM1doY05NakF3TkRBegpNak0xTXpVM1dqQXBNU2N3SlFZRFZRUURFeDVwWkdWdWRHbDBlUzVzYVc1clpYSmtMbU5zZFhOMFpYSXViRzlqCllXd3dXVEFUQmdjcWhrak9QUUlCQmdncWhrak9QUU1CQndOQ0FBVCtTYjVYNHdpNFhQMFgzckp3TXAyM1ZCZGcKRU1NVThFVStLRzhVSTJMbUM1VmpnNVJXTE9XNkJKakJtalhWaUtNK2IrMS9vS0FlT2c2RnJKazhxeUZsbzBJdwpRREFPQmdOVkhROEJBZjhFQkFNQ0FRWXdIUVlEVlIwbEJCWXdGQVlJS3dZQkJRVUhBd0VHQ0NzR0FRVUZCd01DCk1BOEdBMVVkRXdFQi93UUZNQU1CQWY4d0NnWUlLb1pJemowRUF3SURTQUF3UlFJaEFLVUZHM3NZT1MrK2Jha1cKWW1KWlU0NWlDZFRMdGFlbE1EU0ZpSG9DOWVCS0FpQkRXenpvKy9DWUxMbW4zM2JBRW44cFFub2dQNEZ4MDZhagorVTlLNFdsYnpBPT0KLS0tLS1FTkQgQ0VSVElGSUNBVEUtLS0tLQo=
   key.pem: LS0tLS1CRUdJTiBFQyBQUklWQVRFIEtFWS0tLS0tCk1IY0NBUUVFSUhaaEFWTnNwSlRzMWZ4YmZ4VmptTTJvMTNTOFd4U2VVdTlrNFhZK0NPY3JvQW9HQ0NxR1NNNDkKQXdFSG9VUURRZ0FFL2ttK1YrTUl1Rno5Rjk2eWNES2R0MVFYWUJEREZQQkZQaWh2RkNOaTVndVZZNE9VVml6bAp1Z1NZd1pvMTFZaWpQbS90ZjZDZ0hqb09oYXlaUEtzaFpRPT0KLS0tLS1FTkQgRUMgUFJJVkFURSBLRVktLS0tLQo=`,
+			},
+			"upgrade_default.golden",
+			nil,
+		},
+		{
+			[]string{},
+			"",
+			errors.New("could not fetch configs from kubernetes: configmaps \"linkerd-config\" not found"),
+		},
 	}
 
-	options := testUpgradeOptions()
-	flags := options.recordableFlagSet(pflag.ExitOnError)
+	for i, tc := range testCases {
+		tc := tc // pin
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			options := testUpgradeOptions()
+			flags := options.recordableFlagSet()
 
-	clientset, _, err := k8s.NewFakeClientSets(k8sConfigs...)
-	if err != nil {
-		t.Fatalf("Error mocking k8s client: %s", err)
-	}
+			clientset, _, err := k8s.NewFakeClientSets(tc.k8sConfigs...)
+			if err != nil {
+				t.Fatalf("Error mocking k8s client: %s", err)
+			}
 
-	values, configs, err := options.validateAndBuild(clientset, flags)
-	if err != nil {
-		t.Fatalf("validateAndBuild failed with %s", err)
-	}
+			values, configs, err := options.validateAndBuild(clientset, flags)
+			if (err == nil && tc.err != nil) ||
+				(err != nil && tc.err == nil) ||
+				((err != nil && tc.err != nil) && (err.Error() != tc.err.Error())) {
+				t.Fatalf("Expected \"%s\", got \"%s\"", tc.err, err)
+			} else if err == nil {
+				if configs.GetGlobal().GetVersion() != upgradeVersion {
+					t.Errorf("version not upgraded in config")
+				}
 
-	if configs.GetGlobal().GetVersion() != upgradeVersion {
-		t.Errorf("version not upgraded in config")
+				var buf bytes.Buffer
+				if err = values.render(&buf, configs); err != nil {
+					t.Fatalf("could not render upgrade configuration: %s", err)
+				}
+				diffTestdata(t, tc.outputfile, buf.String())
+			}
+		})
 	}
-
-	var buf bytes.Buffer
-	if err = values.render(&buf, configs); err != nil {
-		t.Fatalf("could not render upgrade configuration: %s", err)
-	}
-	diffTestdata(t, "upgrade_default.golden", buf.String())
 }
 
 func TestUpgradeFromOldConfig(t *testing.T) {
@@ -97,9 +121,9 @@ data:
 `,
 	}
 
-	options := newUpgradeOptionsWithDefaults()
+	options := testUpgradeOptions()
 	options.proxyAutoInject = true
-	flags := options.recordableFlagSet(pflag.ExitOnError)
+	flags := options.recordableFlagSet()
 
 	clientset, _, err := k8s.NewFakeClientSets(k8sConfigs...)
 	if err != nil {
@@ -135,5 +159,96 @@ data:
 	}
 	if configs.GetGlobal().GetAutoInjectContext() == nil {
 		t.Errorf("autoinject config not serialized")
+	}
+}
+
+func TestFetchConfigs(t *testing.T) {
+	options := testInstallOptions()
+	values, _, err := options.validateAndBuild(nil)
+	if err != nil {
+		t.Fatalf("Unexpected error validating options: %v", err)
+	}
+	exp := options.configs(values.Identity.toIdentityContext())
+
+	testCases := []struct {
+		k8sConfigs []string
+		expected   *pb.All
+		err        error
+	}{
+		{
+			[]string{`
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: linkerd-config
+  namespace: linkerd
+data:
+  global: |
+    {"linkerdNamespace":"linkerd","cniEnabled":false,"version":"dev-undefined","identityContext":{"trustDomain":"cluster.local","trustAnchorsPem":"-----BEGIN CERTIFICATE-----\nMIIBYDCCAQegAwIBAgIBATAKBggqhkjOPQQDAjAYMRYwFAYDVQQDEw1jbHVzdGVy\nLmxvY2FsMB4XDTE5MDMwMzAxNTk1MloXDTI5MDIyODAyMDM1MlowGDEWMBQGA1UE\nAxMNY2x1c3Rlci5sb2NhbDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABAChpAt0\nxtgO9qbVtEtDK80N6iCL2Htyf2kIv2m5QkJ1y0TFQi5hTVe3wtspJ8YpZF0pl364\n6TiYeXB8tOOhIACjQjBAMA4GA1UdDwEB/wQEAwIBBjAdBgNVHSUEFjAUBggrBgEF\nBQcDAQYIKwYBBQUHAwIwDwYDVR0TAQH/BAUwAwEB/zAKBggqhkjOPQQDAgNHADBE\nAiBQ/AAwF8kG8VOmRSUTPakSSa/N4mqK2HsZuhQXCmiZHwIgZEzI5DCkpU7w3SIv\nOLO4Zsk1XrGZHGsmyiEyvYF9lpY=\n-----END CERTIFICATE-----\n","issuanceLifetime":"86400s","clockSkewAllowance":"20s"},"autoInjectContext":null}
+  proxy: |
+    {"proxyImage":{"imageName":"gcr.io/linkerd-io/proxy","pullPolicy":"IfNotPresent"},"proxyInitImage":{"imageName":"gcr.io/linkerd-io/proxy-init","pullPolicy":"IfNotPresent"},"controlPort":{"port":4190},"ignoreInboundPorts":[],"ignoreOutboundPorts":[],"inboundPort":{"port":4143},"adminPort":{"port":4191},"outboundPort":{"port":4140},"resource":{"requestCpu":"","requestMemory":"","limitCpu":"","limitMemory":""},"proxyUid":"2102","logLevel":{"level":"warn,linkerd2_proxy=info"},"disableExternalProfiles":true}
+  install: |
+    {"uuid":"deaab91a-f4ab-448a-b7d1-c832a2fa0a60","cliVersion":"dev-undefined","flags":[]}`,
+			},
+			exp,
+			nil,
+		},
+		{
+			[]string{`
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: linkerd-config
+  namespace: linkerd
+data:
+  global: |
+    {"linkerdNamespace":"ns","identityContext":null}
+  proxy: "{}"
+  install: "{}"`,
+			},
+			&pb.All{Global: &pb.Global{LinkerdNamespace: "ns", IdentityContext: nil}, Proxy: &pb.Proxy{}, Install: &pb.Install{}},
+			nil,
+		},
+		{
+			[]string{`
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: linkerd-config
+  namespace: linkerd
+data:
+  global: "{}"
+  proxy: "{}"
+  install: "{}"`,
+			},
+			&pb.All{Global: &pb.Global{}, Proxy: &pb.Proxy{}, Install: &pb.Install{}},
+			nil,
+		},
+		{
+			nil,
+			nil,
+			errors.New("configmaps \"linkerd-config\" not found"),
+		},
+	}
+
+	for i, tc := range testCases {
+		tc := tc // pin
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			clientset, _, err := k8s.NewFakeClientSets(tc.k8sConfigs...)
+			if err != nil {
+				t.Fatalf("Unexpected error: %s", err)
+			}
+
+			configs, err := fetchConfigs(clientset)
+			if (err == nil && tc.err != nil) ||
+				(err != nil && tc.err == nil) ||
+				((err != nil && tc.err != nil) && (err.Error() != tc.err.Error())) {
+				t.Fatalf("Expected \"%s\", got \"%s\"", tc.err, err)
+			}
+
+			if !proto.Equal(configs, tc.expected) {
+				t.Fatalf("Unexpected config:\nExpected:\n%+v\nGot:\n%+v", tc.expected, configs)
+			}
+		})
 	}
 }
