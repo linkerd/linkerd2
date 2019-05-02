cd8cfadda3a7089c98e5d145712f74bde157be93
David Capino <david.capino@gmail.com>
2019-05-02 16:23:18 -0700

    Updating the acmpca_retry to log.Error when we decide to not retry anymore
    Signed-off-by: David Capino <david.capino@gmail.com>

--- pkg/pcadelegate/acmpca_retry.go
+++ pkg/pcadelegate/acmpca_retry.go
@@ -48,8 +48,12 @@ func NewACMPCARetry(maxRetries int) (*ACMPCARetry, error) {
 
 // ShouldRetry is used to determine if we should retry based on the request contents.
 func (a ACMPCARetry) ShouldRetry(r *request.Request) bool {
-	log.Infof("There was an error with status code %v with status %v\n", r.HTTPResponse.StatusCode, r.HTTPResponse.Status)
 	// Error codes https://docs.aws.amazon.com/acm-pca/latest/APIReference/API_IssueCertificate.html
 	// TODO check the specific 400 error code and don't retry if the CA is in a bad state.
-	return 400 == r.HTTPResponse.StatusCode || a.DefaultRetryer.ShouldRetry(r)
+	result := 400 == r.HTTPResponse.StatusCode || a.DefaultRetryer.ShouldRetry(r)
+	if !result {
+		log.Errorf("There was an request error with status code %v with status %v\n and we will not retry", r.HTTPResponse.StatusCode, r.HTTPResponse.Status)
+	}
+
+	return result
 }
