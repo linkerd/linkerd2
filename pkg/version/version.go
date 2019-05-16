f58bdd0503baa121c68a331778b491716a3e47af
Alejandro Pedraza <alejandro.pedraza@gmail.com>
2019-05-16 09:18:31 -0500

    Pin proxy-init to v1.0.0 and added comments to Gopkg.toml and version.go
    Signed-off-by: Alejandro Pedraza <alejandro@buoyant.io>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -14,7 +14,9 @@ var Version = undefinedVersion
 
 // ProxyInitVersion is the pinned version of the proxy-init, from
 // https://github.com/linkerd/linkerd2-proxy-init
-var ProxyInitVersion = "stable-2.3.0"
+// This has to be kept in sync with the constraint version for
+// github.com/linkerd/linkerd2-proxy-init in /Gopkg.toml
+var ProxyInitVersion = "v1.0.0"
 
 const (
 	// undefinedVersion should take the form `channel-version` to conform to
