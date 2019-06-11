2914f77ea750a8fb08d1dbdb5abddabfcd125def
Risha Mars <mars@buoyant.io>
2019-06-11 11:16:35 -0700

    Fix BaseTable ignoring sorting

--- web/app/js/components/BaseTable.jsx
+++ web/app/js/components/BaseTable.jsx
@@ -16,8 +16,9 @@ import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import _find from 'lodash/find';
 import _get from 'lodash/get';
+import _isEmpty from 'lodash/isEmpty';
 import _isNil from 'lodash/isNil';
-import _sortBy from 'lodash/sortBy';
+import _orderBy from 'lodash/orderBy';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -89,8 +90,13 @@ class BaseTable extends React.Component {
   generateRows = (tableRows, tableColumns, order, orderBy, filterBy) => {
     let rows = tableRows;
     let col = _find(tableColumns, d => d.dataIndex === orderBy);
+    let {defaultOrderBy} = this.props;
+    if (_isEmpty(orderBy) && !_isEmpty(defaultOrderBy)) {
+      orderBy = defaultOrderBy;
+    }
+
     if (orderBy && col.sorter) {
-      rows = _sortBy(rows, row => col.sorter(row));
+      rows = _orderBy(rows, row => col.sorter(row), order);
     }
     if (filterBy) {
       let columnsToFilter = tableColumns.filter(col => col.filter);
@@ -102,7 +108,8 @@ class BaseTable extends React.Component {
       });
       rows = filteredRows;
     }
-    return order === 'desc' ? rows.reverse() : rows;
+
+    return rows;
   }
 
   renderHeaderCell = (col, order, orderBy) => {
