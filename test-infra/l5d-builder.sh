eb994eef3eefe39477073c29e8bcf6d0ab187468
Andrew Seigner <siggy@buoyant.io>
2019-06-27 00:47:40 +0200

    fix
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- test-infra/l5d-builder.sh
+++ test-infra/l5d-builder.sh
@@ -2,29 +2,27 @@
 
 set -eu
 
-export SHA=abc123
-export CLUSTER=testfoo
+# git clone https://github.com/$REPO_OWNER/$REPO_NAME.git $GOPATH/src/github.com/$REPO_OWNER/$REPO_NAME
+# cd $GOPATH/src/github.com/$REPO_OWNER/$REPO_NAME
 
-git clone https://github.com/siggy/linkerd2.git /go/src/github.com/linkerd/linkerd2
-cd /go/src/github.com/linkerd/linkerd2
-
-kind create cluster --name $CLUSTER
+kind create cluster &
 
 DOCKER_TRACE=1 bin/docker-build
 TAG=$(bin/linkerd version --client --short)
 
+# block until kind cluster creation completes
+wait
+
 for IMG in controller grafana proxy web ; do
-  kind load docker-image gcr.io/linkerd-io/$IMG:$TAG --name $CLUSTER
+  kind load docker-image gcr.io/linkerd-io/$IMG:$TAG
 done
 
 docker pull gcr.io/linkerd-io/proxy-init:v1.0.0
-kind load docker-image gcr.io/linkerd-io/proxy-init:v1.0.0 --name $CLUSTER
+kind load docker-image gcr.io/linkerd-io/proxy-init:v1.0.0
 
 docker pull prom/prometheus:v2.7.1
-kind load docker-image prom/prometheus:v2.7.1 --name $CLUSTER
+kind load docker-image prom/prometheus:v2.7.1
 
 bin/dep ensure
-export KUBECONFIG="$(kind get kubeconfig-path --name $CLUSTER)"
-bin/test-run `pwd`/bin/linkerd l5d-integration-$SHA
-
-sleep 86400
+export KUBECONFIG="$(kind get kubeconfig-path)"
+bin/test-run `pwd`/bin/linkerd
