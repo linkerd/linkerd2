9712e59dace92a8ebec247283fd4c20dbbfe802b
Andrew Seigner <siggy@buoyant.io>
2019-06-27 04:22:24 +0200

    kindconfig
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- test-infra/l5d-builder.sh
+++ test-infra/l5d-builder.sh
@@ -20,14 +20,13 @@ KINDCONFIG=$(kind get kubeconfig-path --name=$CLUSTER)
 POD=$(kubectl -n dind get po --selector=app=dind -o jsonpath='{.items[*].metadata.name}')
 KINDSERVER=$(kubectl --kubeconfig=$KINDCONFIG config view -o jsonpath='{.clusters[*].cluster.server}')
 KINDPORT=$(echo $KINDSERVER | cut -d':' -f3)
-
 kubectl -n dind port-forward $POD $KINDPORT &
-export KUBECONFIG=$KINDCONFIG
-kubectl cluster-info
 
 DOCKER_TRACE=1 bin/docker-build
 TAG=$(bin/linkerd version --client --short)
 
+export KUBECONFIG=$KINDCONFIG
+kubectl cluster-info
 for IMG in controller grafana proxy web ; do
   kind load docker-image gcr.io/linkerd-io/$IMG:$TAG --name=$CLUSTER
 done
