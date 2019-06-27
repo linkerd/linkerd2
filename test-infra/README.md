956b3094c8839e9d9705e5e1c32de48197918b80
Andrew Seigner <siggy@buoyant.io>
2019-06-27 03:04:11 +0200

    wip
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- test-infra/README.md
+++ test-infra/README.md
@@ -1,6 +1,12 @@
+## Build l5d-builder Docker image
+
 ```bash
 docker build -t gcr.io/linkerd-io/l5d-builder:latest .
 docker push gcr.io/linkerd-io/l5d-builder:latest
+```
 
-docker run --privileged gcr.io/linkerd-io/l5d-builder:latest
+## Deploy dind
+
+```bash
+cat dind.yaml | kubectl apply -f -
 ```
--- test-infra/l5d-builder.sh
+++ test-infra/l5d-builder.sh
@@ -5,24 +5,31 @@ set -eu
 # git clone https://github.com/$REPO_OWNER/$REPO_NAME.git $GOPATH/src/github.com/$REPO_OWNER/$REPO_NAME
 # cd $GOPATH/src/github.com/$REPO_OWNER/$REPO_NAME
 
-kind create cluster &
+CLUSTER=$PROW_JOB_ID
+kind create cluster --name=$CLUSTER
+KINDCONFIG=$(kind get kubeconfig-path --name=$CLUSTER)
+POD=$(kubectl -n dind get po --selector=app=dind -o jsonpath='{.items[*].metadata.name}')
+KINDSERVER=$(kubectl --kubeconfig=$KINDCONFIG config view -o jsonpath='{.clusters[*].cluster.server}')
+KINDPORT=$(echo $KINDSERVER | cut -d':' -f3)
+
+kubectl -n dind port-forward $POD $KINDPORT &
+export KUBECONFIG=$KINDCONFIG
+kubectl cluster-info
 
 DOCKER_TRACE=1 bin/docker-build
 TAG=$(bin/linkerd version --client --short)
 
-# block until kind cluster creation completes
-wait
-
 for IMG in controller grafana proxy web ; do
-  kind load docker-image gcr.io/linkerd-io/$IMG:$TAG
+  kind load docker-image gcr.io/linkerd-io/$IMG:$TAG --name=$CLUSTER
 done
 
 docker pull gcr.io/linkerd-io/proxy-init:v1.0.0
-kind load docker-image gcr.io/linkerd-io/proxy-init:v1.0.0
+kind load docker-image gcr.io/linkerd-io/proxy-init:v1.0.0 --name=$CLUSTER
 
 docker pull prom/prometheus:v2.7.1
-kind load docker-image prom/prometheus:v2.7.1
+kind load docker-image prom/prometheus:v2.7.1 --name=$CLUSTER
 
 bin/dep ensure
-export KUBECONFIG="$(kind get kubeconfig-path)"
 bin/test-run `pwd`/bin/linkerd
+
+kind delete cluster --name=$CLUSTER
