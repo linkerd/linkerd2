18b74aa8a89206ffef9ee0c89e7f490a97eb3d76
Andrew Seigner <siggy@buoyant.io>
2019-07-25 14:41:38 -0700

    Introduce Go modules support (#2481)
    The repo relied on `dep` for managing Go dependencies. Go 1.11 shipped
    with Go modules support. Go 1.13 will be released in August 2019 with
    module support enabled by default, deprecating GOPATH.
    
    This change replaces `dep` with Go modules for dependency management.
    All scripts, including Docker builds and ci, should work without any dev
    environment changes.
    
    To execute `go` commands directly during development, do one of the
    following:
    1. clone this repo outside of `GOPATH`; or
    2. run `export GO111MODULE=on`
    
    Summary of changes:
    - Docker build scripts and ci set `-mod=readonly`, to ensure
      dependencies defined in `go.mod` are exactly what is used for the
      builds.
    - Dependency updates to `go.mod` are accomplished by running
     `go build` and `go test` directly.
    - `bin/go-run`, `bin/build-cli-bin`, and `bin/test-run` set
      `GO111MODULE=on`, permitting usage inside and outside of GOPATH.
    - `gcr.io/linkerd-io/go-deps` tags hashed from `go.mod`.
    - `bin/update-codegen.sh` still requires running from GOPATH,
      instructions added to BUILD.md.
    
    Fixes #1488
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -3,21 +3,31 @@
 set -eu
 
 gen() {
-
-    rm -rf controller/gen/common controller/gen/controller controller/gen/public controller/gen/config
-    mkdir -p controller/gen
-
     for f in $@; do
-        bin/protoc -I proto --go_out="plugins=grpc:$GOPATH/src" "$f"
+        bin/protoc -I proto --go_out=plugins=grpc,paths=source_relative:controller/gen "$f"
     done
-
-    git add controller/gen
 }
 
-go install ./vendor/github.com/golang/protobuf/protoc-gen-go
+go install -mod=readonly github.com/golang/protobuf/protoc-gen-go
+
+rm -rf controller/gen/common controller/gen/config controller/gen/controller controller/gen/public
 
 gen proto/common/healthcheck.proto \
     proto/controller/discovery.proto \
     proto/controller/tap.proto \
     proto/public.proto \
     proto/config/config.proto
+
+# TODO: Re-organize the top-level /proto directory to mirror output packages.
+# As a work-around, manually move files after generation.
+mkdir -p controller/gen/common/healthcheck
+mkdir -p controller/gen/controller/discovery
+mkdir -p controller/gen/controller/tap
+mkdir -p controller/gen/public
+
+mv controller/gen/common/healthcheck.pb.go   controller/gen/common/healthcheck/
+mv controller/gen/controller/discovery.pb.go controller/gen/controller/discovery/
+mv controller/gen/controller/tap.pb.go       controller/gen/controller/tap/
+mv controller/gen/public.pb.go               controller/gen/public/
+
+git add controller/gen
--- bin/update-codegen.sh
+++ bin/update-codegen.sh
@@ -2,7 +2,12 @@
 
 set -eu
 
-# ROOT_PACKAGE :: the package (relative to $GOPATH/src) that is the target for code generation
+GO111MODULE=on go mod vendor
+git clone https://github.com/kubernetes/code-generator.git vendor/k8s.io/code-generator
+# check out code-generator at the revision Gopkg.lock had previously referenced
+git -C vendor/k8s.io/code-generator checkout aae79feb89bdded3679da91fd8c19b6dfcbdb79a
+
+# ROOT_PACKAGE :: the package that is the target for code generation
 ROOT_PACKAGE="github.com/linkerd/linkerd2"
 # CUSTOM_RESOURCE_NAME :: the name of the custom resource that we're generating client code for
 CUSTOM_RESOURCE_NAME="serviceprofile"
