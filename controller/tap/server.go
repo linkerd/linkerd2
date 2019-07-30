6cc52c3363f2031975046767082be3b6975ad131
Kevin Leimkuhler <kleimkuhler@icloud.com>
2019-07-30 11:17:52 -0700

    Add `l5d-require-id` header to Tap requests (#3154)
    ### Summary
    
    In order for Pods' tap servers to start authorizing tap clients, the tap
    controller must open TLS connections so that it can identity itself to the
    server.
    
    This change introduces the use of `l5d-require-id` header on outbound tap
    requests.
    
    ### Details
    
    When tap requests are made by the tap controller, the `Authority` header is an
    IP address. The proxy does not attempt to do service discovery on such requests
    and therefore the connection is over plaintext. By introducing the
    `l5d-require-id` header the proxy can require a server name on the connection.
    This allows the tap controller to identity itself as the client making tap
    requests. The name value for the header can be made from the Pod Spec and tap
    request, so the change is rather minimal.
    
    #### Proxy Changes
    
    * Update h2 to v0.1.26
    * Properly fall back in the dst_router (linkerd/linkerd2-proxy#291)
    
    ### Testing
    
    Unit tests for the header have not been added mainly because [no test
    infrastructure currently exists](https://github.com/linkerd/linkerd2/blob/065c2218588093831dc2f8f49516e85e4a71b896/controller/tap/server_test.go#L241) to mock proxy requests. After talking with
    @siggy a little about this, it makes to do in a separate change at some point
    when behavior like this cannot be reliably tested through integration tests
    either.
    
    Integration tests do test this well, and will continue to do once
    linkerd/linkerd2-proxy#290 lands.
    
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- controller/tap/server.go
+++ controller/tap/server.go
@@ -20,11 +20,13 @@ import (
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
+const requireIDHeader = "l5d-require-id"
 const podIPIndex = "ip"
 const defaultMaxRps = 100.0
 
@@ -105,8 +107,17 @@ func (s *server) TapByResource(req *public.TapByResourceRequest, stream pb.Tap_T
 	}
 
 	for _, pod := range pods {
+		// create the expected pod identity from the pod spec
+		ns := req.GetTarget().GetResource().GetNamespace()
+		name := fmt.Sprintf("%s.%s.serviceaccount.identity.%s.cluster.local", pod.Spec.ServiceAccountName, ns, s.controllerNamespace)
+		log.Debugf("initiating tap request to %s with required name %s", pod.Spec.ServiceAccountName, name)
+
+		// pass the header metadata into the request context
+		ctx := stream.Context()
+		ctx = metadata.AppendToOutgoingContext(ctx, requireIDHeader, name)
+
 		// initiate a tap on the pod
-		go s.tapProxy(stream.Context(), rpsPerPod, match, pod.Status.PodIP, events)
+		go s.tapProxy(ctx, rpsPerPod, match, pod.Status.PodIP, events)
 	}
 
 	// read events from the taps and send them back
