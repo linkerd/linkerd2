71b4fb23e6bb27fdf280ca7e21c2c0a6f53b2837
Ivan Sim <ivan@buoyant.io>
2019-07-30 20:32:42 -0700

    Perform a dry run installation if there is a local Tiller
    Signed-off-by: Ivan Sim <ivan@buoyant.io>

--- bin/helm.sh
+++ bin/helm.sh
@@ -9,3 +9,9 @@ helm lint $rootdir/charts/partials
 
 helm dep up $rootdir/charts/linkerd
 helm lint $rootdir/charts/linkerd
+
+# if tiller is deployed, perform a dry run installation to check for errors
+if tiller=`kubectl get po -l app=helm,name=tiller --all-namespaces`; then
+  echo "Performing dry run installation"
+  helm install --name=linkerd --dry-run charts/linkerd
+fi
