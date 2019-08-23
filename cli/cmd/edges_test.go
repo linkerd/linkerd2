089836842a8a0d50651f507d8d211e437ae8deb9
Carol A. Scott <carol@buoyant.io>
2019-08-23 09:28:02 -0700

    Add unit test for edges API endpoint (#3306)
    Fixes #3052.
    
    Adds a unit test for the edges API endpoint. To maintain a consistent order for
    testing, the returned rows in api/public/edges.go are now sorted.

--- cli/cmd/edges_test.go
+++ cli/cmd/edges_test.go
@@ -7,107 +7,38 @@ import (
 )
 
 type edgesParamsExp struct {
-	options         *edgesOptions
-	resSrc          []string
-	resSrcNamespace []string
-	resDst          []string
-	resDstNamespace []string
-	resClient       []string
-	resServer       []string
-	resMsg          []string
-	resourceType    string
-	file            string
+	options      *edgesOptions
+	resourceType string
+	file         string
 }
 
 func TestEdges(t *testing.T) {
-	// response content for SRC, DST, SRC_NS, DST_NS, CLIENT_ID, SERVER_ID and MSG
-	var (
-		resSrc = []string{
-			"web",
-			"vote-bot",
-			"web",
-			"linkerd-controller",
-		}
-		resDst = []string{
-			"voting",
-			"web",
-			"emoji",
-			"linkerd-prometheus",
-		}
-		resSrcNamespace = []string{
-			"emojivoto",
-			"emojivoto",
-			"emojivoto",
-			"linkerd",
-		}
-		resDstNamespace = []string{
-			"emojivoto",
-			"emojivoto",
-			"emojivoto",
-			"linkerd",
-		}
-		resClient = []string{
-			"web.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"default.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"web.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"linkerd-controller.linkerd.identity.linkerd.cluster.local",
-		}
-		resServer = []string{
-			"voting.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"web.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"emoji.emojivoto.serviceaccount.identity.linkerd.cluster.local",
-			"linkerd-prometheus.linkerd.identity.linkerd.cluster.local",
-		}
-		resMsg = []string{"", "", "", ""}
-	)
-
 	options := newEdgesOptions()
 	options.outputFormat = tableOutput
 	options.allNamespaces = true
 	t.Run("Returns edges", func(t *testing.T) {
 		testEdgesCall(edgesParamsExp{
-			options:         options,
-			resourceType:    "deployment",
-			resSrc:          resSrc,
-			resSrcNamespace: resSrcNamespace,
-			resDst:          resDst,
-			resDstNamespace: resDstNamespace,
-			resClient:       resClient,
-			resServer:       resServer,
-			resMsg:          resMsg,
-			file:            "edges_one_output.golden",
+			options:      options,
+			resourceType: "deployment",
+			file:         "edges_one_output.golden",
 		}, t)
 	})
 
 	options.outputFormat = jsonOutput
 	t.Run("Returns edges (json)", func(t *testing.T) {
 		testEdgesCall(edgesParamsExp{
-			options:         options,
-			resourceType:    "deployment",
-			resSrc:          resSrc,
-			resSrcNamespace: resSrcNamespace,
-			resDst:          resDst,
-			resDstNamespace: resDstNamespace,
-			resClient:       resClient,
-			resServer:       resServer,
-			resMsg:          resMsg,
-			file:            "edges_one_output_json.golden",
+			options:      options,
+			resourceType: "deployment",
+			file:         "edges_one_output_json.golden",
 		}, t)
 	})
 
 	t.Run("Returns edges (wide)", func(t *testing.T) {
 		options.outputFormat = wideOutput
 		testEdgesCall(edgesParamsExp{
-			options:         options,
-			resourceType:    "deployment",
-			resSrc:          resSrc,
-			resSrcNamespace: resSrcNamespace,
-			resDst:          resDst,
-			resDstNamespace: resDstNamespace,
-			resClient:       resClient,
-			resServer:       resServer,
-			resMsg:          resMsg,
-			file:            "edges_wide_output.golden",
+			options:      options,
+			resourceType: "deployment",
+			file:         "edges_wide_output.golden",
 		}, t)
 	})
 
@@ -169,7 +100,7 @@ func TestEdges(t *testing.T) {
 
 func testEdgesCall(exp edgesParamsExp, t *testing.T) {
 	mockClient := &public.MockAPIClient{}
-	response := public.GenEdgesResponse(exp.resourceType, exp.resSrc, exp.resDst, exp.resSrcNamespace, exp.resDstNamespace, exp.resClient, exp.resServer, exp.resMsg)
+	response := public.GenEdgesResponse(exp.resourceType, "all")
 
 	mockClient.EdgesResponseToReturn = &response
 
