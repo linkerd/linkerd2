f49a20b49f792a56da9aeae9989fa84e386e1852
Tarun Pothulapati <tarunpothulapati@outlook.com>
2019-09-09 01:49:49 +0530

    keep the structure, and add global ownerKind
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- controller/proxy-injector/webhook.go
+++ controller/proxy-injector/webhook.go
@@ -63,6 +63,7 @@ func Inject(api *k8s.API,
 	}
 
 	var parent *runtime.Object
+	ownerKind := ""
 	if ownerRef := resourceConfig.GetOwnerRef(); ownerRef != nil {
 		objs, err := api.GetObjects(request.Namespace, ownerRef.Kind, ownerRef.Name)
 		if err != nil {
@@ -72,40 +73,41 @@ func Inject(api *k8s.API,
 		} else {
 			parent = &objs[0]
 		}
-		proxyInjectionAdmissionRequests.With(admissionRequestLabels(strings.ToLower(ownerRef.Kind), request.Namespace)).Inc()
-
-		if injectable, reason := report.Injectable(); !injectable {
-			if parent != nil {
-				recorder.Eventf(*parent, v1.EventTypeNormal, eventTypeSkipped, "Linkerd sidecar proxy injection skipped: %s", reason)
-			}
-			log.Infof("skipped %s: %s", report.ResName(), reason)
-			proxyInjectionAdmissionResponses.With(admissionResponseLabels(strings.ToLower(ownerRef.Kind), request.Namespace, "true", reason)).Inc()
-			return admissionResponse, nil
-		}
-
-		resourceConfig.AppendPodAnnotations(map[string]string{
-			pkgK8s.CreatedByAnnotation: fmt.Sprintf("linkerd/proxy-injector %s", version.Version),
-		})
-		patchJSON, err := resourceConfig.GetPatch(true)
-		if err != nil {
-			return nil, err
-		}
-
-		if len(patchJSON) == 0 {
-			return admissionResponse, nil
-		}
-
-		if parent != nil {
-			recorder.Event(*parent, v1.EventTypeNormal, eventTypeInjected, "Linkerd sidecar proxy injected")
-		}
-		log.Infof("patch generated for: %s", report.ResName())
-		log.Debugf("patch: %s", patchJSON)
-		proxyInjectionAdmissionResponses.With(admissionResponseLabels(strings.ToLower(ownerRef.Kind), request.Namespace, "false", "")).Inc()
-
-		patchType := admissionv1beta1.PatchTypeJSONPatch
-		admissionResponse.Patch = patchJSON
-		admissionResponse.PatchType = &patchType
+		ownerKind = strings.ToLower(ownerRef.Kind)
+		proxyInjectionAdmissionRequests.With(admissionRequestLabels(ownerKind, request.Namespace)).Inc()
 	}
+
+	if injectable, reason := report.Injectable(); !injectable {
+		if parent != nil {
+			recorder.Eventf(*parent, v1.EventTypeNormal, eventTypeSkipped, "Linkerd sidecar proxy injection skipped: %s", reason)
+		}
+		log.Infof("skipped %s: %s", report.ResName(), reason)
+		proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "true", reason)).Inc()
+		return admissionResponse, nil
+	}
+
+	resourceConfig.AppendPodAnnotations(map[string]string{
+		pkgK8s.CreatedByAnnotation: fmt.Sprintf("linkerd/proxy-injector %s", version.Version),
+	})
+	patchJSON, err := resourceConfig.GetPatch(true)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(patchJSON) == 0 {
+		return admissionResponse, nil
+	}
+
+	if parent != nil {
+		recorder.Event(*parent, v1.EventTypeNormal, eventTypeInjected, "Linkerd sidecar proxy injected")
+	}
+	log.Infof("patch generated for: %s", report.ResName())
+	log.Debugf("patch: %s", patchJSON)
+	proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "false", "")).Inc()
+	patchType := admissionv1beta1.PatchTypeJSONPatch
+	admissionResponse.Patch = patchJSON
+	admissionResponse.PatchType = &patchType
+
 	return admissionResponse, nil
 }
 
