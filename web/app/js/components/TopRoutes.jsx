88de7192339f3c5ef7ae6b4ec01b110797c81c30
pierDipi <33736985+pierDipi@users.noreply.github.com>
2019-09-17 21:33:37 +0200

    Fix bad request in the top routes tab on empty fields (#3432)
    This PR disables the `Start` button in the dashboard's top routes view if there
    is no namespace or resource type selected. 
    
    Previously, clicking `Start` on the top routes tab with empty namespace and
    resource fields would result in a bad request error. 
    
    Signed-off-by: pierdipi <pierangelodipilato@gmail.com>

--- web/app/js/components/TopRoutes.jsx
+++ web/app/js/components/TopRoutes.jsx
@@ -208,7 +208,7 @@ class TopRoutes extends React.Component {
               <Button
                 color="primary"
                 variant="outlined"
-                disabled={this.state.requestInProgress}
+                disabled={this.state.requestInProgress || !this.state.query.namespace || !this.state.query.resource_type}
                 onClick={this.handleBtnClick(true)}>
               Start
               </Button>
