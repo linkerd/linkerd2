6685c11d75c391d9f5955eb82efee3c26ef2a6b7
Alejandro Pedraza Borrero <alejandro@buoyant.io>
2019-09-27 11:19:55 -0500

    Add integration tests for linkerd endpoints and edges
    Fixes #3477 and #3478

---
+++ test/edges/edges_test.go
@@ -0,0 +1,44 @@
+package edges
+
+import (
+	"bytes"
+	"html/template"
+	"os"
+	"regexp"
+	"testing"
+
+	"github.com/linkerd/linkerd2/testutil"
+)
+
+var TestHelper *testutil.TestHelper
+
+func TestMain(m *testing.M) {
+	TestHelper = testutil.NewTestHelper()
+	os.Exit(m.Run())
+}
+
+func TestEdges(t *testing.T) {
+	ns := TestHelper.GetLinkerdNamespace()
+	cmd := []string{
+		"edges",
+		"-n " + ns,
+		"deploy",
+		"-ojson",
+	}
+	out, _, err := TestHelper.LinkerdRun(cmd...)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+
+	tpl := template.Must(template.ParseFiles("testdata/linkerd_edges.golden"))
+	vars := struct{ Ns string }{ns}
+	var b bytes.Buffer
+	if err := tpl.Execute(&b, vars); err != nil {
+		t.Fatalf("failed to parse linkerd_edges.golden template: %s", err)
+	}
+
+	r := regexp.MustCompile(b.String())
+	if !r.MatchString(out) {
+		t.Errorf("Expected output:\n%s\nactual:\n%s", b.String(), out)
+	}
+}
