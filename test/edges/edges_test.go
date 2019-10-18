4d815b955ab0e16760c6b22dde41aec688704e5d
Alex Leong <alex@buoyant.io>
2019-10-18 14:40:50 -0700

    Add direct edges integration test
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- test/edges/edges_test.go
+++ test/edges/edges_test.go
@@ -3,8 +3,10 @@ package edges
 import (
 	"bytes"
 	"html/template"
+	"io/ioutil"
 	"os"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/linkerd/linkerd2/testutil"
@@ -43,5 +45,92 @@ func TestEdges(t *testing.T) {
 	r := regexp.MustCompile(b.String())
 	if !r.MatchString(out) {
 		t.Errorf("Expected output:\n%s\nactual:\n%s", b.String(), out)
+	}
+}
+
+// TestDirectEdges deploys a terminus and then generates a load generator which
+// sends traffic directly to the pod ip of the terminus pod. Traffic which is
+// addressed this way (as opposed to using the service name) does not show up
+// in the `linkerd edges` command. This test should be updated once
+// `linkerd edges` is updated to support this kind of traffic.
+func TestDirectEdges(t *testing.T) {
+
+	// setup
+
+	testNamespace := TestHelper.GetTestNamespace("direct-edges-test")
+	err := TestHelper.CreateNamespaceIfNotExists(testNamespace, nil)
+	if err != nil {
+		t.Fatalf("failed to create %s namespace: %s", testNamespace, err)
+	}
+
+	// inject terminus
+
+	out, stderr, err := TestHelper.LinkerdRun("inject", "--manual", "testdata/terminus.yaml")
+	if err != nil {
+		t.Fatalf("'linkerd %s' command failed with %s: %s\n", "inject", err.Error(), stderr)
+	}
+
+	// deploy terminus
+
+	out, err = TestHelper.KubectlApply(out, testNamespace)
+	if err != nil {
+		t.Fatalf("kubectl apply command failed\n%s", out)
+	}
+
+	if err := TestHelper.CheckPods(testNamespace, "terminus", 1); err != nil {
+		t.Error(err)
+	}
+
+	if err := TestHelper.CheckDeployment(testNamespace, "terminus", 1); err != nil {
+		t.Errorf("Error validating deployment [%s]:\n%s", "terminus", err)
+	}
+
+	// get terminus pod ip
+
+	ip, err := TestHelper.Kubectl("", "-n", testNamespace, "get", "pod", "-ojsonpath=\"{.items[*].status.podIP}\"")
+	if err != nil {
+		t.Error(err)
+	}
+	ip = strings.Trim(ip, "\"") // strip quotes
+
+	bytes, err := ioutil.ReadFile("testdata/slow-cooker.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	slowcooker := string(bytes)
+	slowcooker = strings.ReplaceAll(slowcooker, "___TERMINUS_POD_IP___", ip)
+
+	// inject slow cooker
+
+	out, stderr, err = TestHelper.PipeToLinkerdRun(slowcooker, "inject", "--manual", "-")
+	if err != nil {
+		t.Fatalf("'linkerd %s' command failed with %s: %s\n", "inject", err.Error(), stderr)
+	}
+
+	// deploy slow cooker
+
+	out, err = TestHelper.KubectlApply(out, testNamespace)
+	if err != nil {
+		t.Fatalf("kubectl apply command failed\n%s", out)
+	}
+
+	if err := TestHelper.CheckPods(testNamespace, "slow-cooker", 1); err != nil {
+		t.Error(err)
+	}
+
+	if err := TestHelper.CheckDeployment(testNamespace, "slow-cooker", 1); err != nil {
+		t.Errorf("Error validating deployment [%s]:\n%s", "terminus", err)
+	}
+
+	// check edges
+
+	_, stderr, err = TestHelper.LinkerdRun("-n", testNamespace, "edges", "pods")
+	if err != nil {
+		t.Fatalf("'linkerd %s' command failed with %s: %s\n", "edges", err.Error(), stderr)
+	}
+	stderr = strings.TrimSpace(stderr)
+	if stderr != "No edges found." {
+		t.Fatalf("Expected: \"%s\" Got: \"%s\"", "No edges found.", stderr)
 	}
 }
