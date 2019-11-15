4c522dc811c689421ced4dedd74872dfa937cfb1
Alejandro Pedraza <alejandro@buoyant.io>
2019-11-14 20:10:30 -0500

    Added cleanup to external_issuer_integration_tests, added call to custom_domain_integration_tests in test-run (and also updated comments), and trigger docker_deploy temporarily for PRs

--- bin/_test-run.sh
+++ bin/_test-run.sh
@@ -77,6 +77,7 @@ function external_issuer_integration_tests() {
 
     run_test "$test_directory/externalissuer/external_issuer_test.go" --linkerd-namespace=$linkerd_namespace-external-issuer --external-issuer=true
     exit_on_err "error during external issuer tests"
+    cleanup
 }
 
 #
