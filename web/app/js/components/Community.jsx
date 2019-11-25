65d5778b9393cd41c001b5023cf02a0d7626d03c
Cynthia S. Garcia <cynthiasg@icloud.com>
2019-11-25 20:29:05 +0100

    Stop using react-iframe library in dashboard (#3695)
    This PR removes a JS dependency for the Community iFrame page.
    
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/app/js/components/Community.jsx
+++ web/app/js/components/Community.jsx
@@ -1,15 +1,28 @@
-import Iframe from 'react-iframe';
+import PropTypes from 'prop-types';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-const Community = () => {
+const styles = () => ({
+  iframe: {
+    border: "0px",
+    height: "100vh",
+    width: "100%",
+    overflow: "hidden",
+  },
+});
+
+const Community = ({classes}) => {
   return (
-    <Iframe
-      url="https://linkerd.io/dashboard/"
-      position="inherit"
-      display="block"
-      height="100vh"
-      border="none" />
+    <iframe
+      title="Community"
+      src="https://linkerd.io/dashboard/"
+      scrolling="no"
+      className={classes.iframe} />
   );
 };
 
-export default Community;
+Community.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(Community);
