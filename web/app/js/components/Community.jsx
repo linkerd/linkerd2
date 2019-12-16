f48076289e046de35709c28579e6d24219aba77a
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2019-12-16 19:19:25 +0100

    Fix issue with Community iframe height
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/app/js/components/Community.jsx
+++ web/app/js/components/Community.jsx
@@ -1,22 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
+import _has from 'lodash/has';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = () => ({
   iframe: {
     border: "0px",
-    height: "100vh",
     width: "100%",
     overflow: "hidden",
   },
 });
 
 const Community = ({classes}) => {
+  const [iframeHeight, setIframeHeight] = useState(0);
+  useEffect(() => {
+    // We add 5px to avoid cutting box shadow
+    const setFromIframeEvent = e => {
+      if (!_has(e.data, "dashboardHeight")) {
+        return;
+      }
+      setIframeHeight(e.data.dashboardHeight + 5);
+    };
+    window.addEventListener("message", setFromIframeEvent);
+    return () => {
+      window.removeEventListener("message", setFromIframeEvent);
+    };
+  }, []);
+
   return (
     <iframe
       title="Community"
       src="https://linkerd.io/dashboard/"
       scrolling="no"
+      style={{ height: iframeHeight > 0 ? iframeHeight : "100vh" }}
       className={classes.iframe} />
   );
 };
