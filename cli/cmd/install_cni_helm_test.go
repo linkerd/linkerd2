efb1101bdb7462b46b8bb68ef142a201a23592f5
Tarun Pothulapati <tarunpothulapati@outlook.com>
2019-12-17 04:39:57 +0530

    Switch to smaller-case values in linkerd2-cni (#3827)
    * update linkerd2-cni templates and cli
    * update readme and docs
    * update helm unit tests
    * update helm build script
    * use smaller case linkerd version
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- cli/cmd/install_cni_helm_test.go
+++ cli/cmd/install_cni_helm_test.go
@@ -17,7 +17,7 @@ func TestRenderCniHelm(t *testing.T) {
 	// override most defaults with pinned values.
 	// use the Helm lib to render the templates.
 	// the golden file is generated using the following `helm template` command:
-	// bin/helm template --set Namespace="linkerd-test" --set ControllerNamespaceLabel="linkerd.io/control-plane-ns-test" --set CNIResourceAnnotation="linkerd.io/cni-resource-test" --set InboundProxyPort=1234 --set OutboundProxyPort=5678 --set CreatedByAnnotation="linkerd.io/created-by-test" --set CNIPluginImage="gcr.io/linkerd-io/cni-plugin-test" --set CNIPluginVersion="test-version" --set LogLevel="debug" --set ProxyUID=1111 --set DestCNINetDir="/etc/cni/net.d-test" --set DestCNIBinDir="/opt/cni/bin-test" --set UseWaitFlag=true --set CliVersion=test-version charts/linkerd2-cni
+	// bin/helm template --set namespace="linkerd-test" --set controllerNamespaceLabel="linkerd.io/control-plane-ns-test" --set cniResourceAnnotation="linkerd.io/cni-resource-test" --set inboundProxyPort=1234 --set outboundProxyPort=5678 --set createdByAnnotation="linkerd.io/created-by-test" --set cniPluginImage="gcr.io/linkerd-io/cni-plugin-test" --set cniPluginVersion="test-version" --set logLevel="debug" --set proxyUID=1111 --set destCNINetDir="/etc/cni/net.d-test" --set destCNIBinDir="/opt/cni/bin-test" --set useWaitFlag=true --set cliVersion=test-version charts/linkerd2-cni
 
 	t.Run("Cni Install", func(t *testing.T) {
 		chartCni := chartCniPlugin(t)
@@ -33,20 +33,20 @@ func testRenderCniHelm(t *testing.T, chart *pb.Chart, goldenFileName string) {
 	)
 	overrideJSON :=
 		`{
-			"Namespace": "linkerd-test",
-  			"ControllerNamespaceLabel": "linkerd.io/control-plane-ns-test",
-  			"CNIResourceAnnotation": "linkerd.io/cni-resource-test",
-  			"InboundProxyPort": 1234,
-  			"OutboundProxyPort": 5678,
-			"CreatedByAnnotation": "linkerd.io/created-by-test",
-  			"CNIPluginImage": "gcr.io/linkerd-io/cni-plugin-test",
-  			"CNIPluginVersion": "test-version",
-  			"LogLevel": "debug",
-  			"ProxyUID": 1111,
-  			"DestCNINetDir": "/etc/cni/net.d-test",
-  			"DestCNIBinDir": "/opt/cni/bin-test",
-  			"UseWaitFlag": true,
-			"CliVersion": "test-version"
+			"namespace": "linkerd-test",
+  			"controllerNamespaceLabel": "linkerd.io/control-plane-ns-test",
+  			"cniResourceAnnotation": "linkerd.io/cni-resource-test",
+  			"inboundProxyPort": 1234,
+  			"outboundProxyPort": 5678,
+			"createdByAnnotation": "linkerd.io/created-by-test",
+  			"cniPluginImage": "gcr.io/linkerd-io/cni-plugin-test",
+  			"cniPluginVersion": "test-version",
+  			"logLevel": "debug",
+  			"proxyUID": 1111,
+  			"destCNINetDir": "/etc/cni/net.d-test",
+  			"destCNIBinDir": "/opt/cni/bin-test",
+  			"useWaitFlag": true,
+			"cliVersion": "test-version"
 		}`
 
 	overrideConfig := &pb.Config{Raw: overrideJSON}
