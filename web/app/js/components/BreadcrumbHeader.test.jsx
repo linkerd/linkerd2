6f3f7842c414e76bcaaeef9f51a33ff1f80824af
Carol Scott <carol@buoyant.io>
2019-12-15 17:51:34 -0800

    Updating unit test

--- web/app/js/components/BreadcrumbHeader.test.jsx
+++ web/app/js/components/BreadcrumbHeader.test.jsx
@@ -26,4 +26,45 @@ describe('Tests for <BreadcrumbHeader>', () => {
     const crumbs = component.find("span");
     expect(crumbs).toHaveLength(3);
   });
+
+  it("renders correct breadcrumb text for resource list page", () => {
+
+    loc.pathname = "/namespaces/emojivoto/deployments";
+    const component = mount(
+      <BrowserRouter>
+        <BreadcrumbHeader
+          location={loc}
+          pathPrefix="" />
+      </BrowserRouter>
+    );
+
+    const crumbs = component.find("span");
+    expect(crumbs).toHaveLength(3);
+    const crumbText = crumbs.reduce((acc, crumb) => {
+      acc+= crumb.text();
+      return acc;
+    }, "")
+    expect(crumbText).toEqual("Namespace > emojivoto > Deployments")
+  });
+
+  it("renders correct breadcrumb text for resource detail page", () => {
+
+    loc.pathname = "/namespaces/emojivoto/deployments/web";
+    const component = mount(
+      <BrowserRouter>
+        <BreadcrumbHeader
+          location={loc}
+          pathPrefix="" />
+      </BrowserRouter>
+    );
+
+    const crumbs = component.find("span");
+    expect(crumbs).toHaveLength(3);
+    const crumbText = crumbs.reduce((acc, crumb) => {
+      acc+= crumb.text();
+      return acc;
+    }, "")
+    expect(crumbText).toEqual("Namespace > emojivoto > deployment/web")
+
+  });
 });
