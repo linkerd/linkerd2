e13a74cb300e24cd63fc9d3e18c123d3b5231110
Animesh Narayan Dangwal <animesh.leo@gmail.com>
2020-01-14 10:44:24 +0530

    Linkerd CLI Chocolatey Package This draft PR aims to fix #3063 by building a chocolatey package for linkerd2's Windows CLI The package currently exists in the folder `windowspkg`
    To build (on windows system):
    - Install chocolatey
    - cd into `windowspkg/linkerd2-cli`
    - Run `choco pack`
    - Run `choco install linkerd-cli -s . `, make sure cmd is running as administrator
    - Run `refreshenv` to add linkerd2-cli path to system
    - To unistall run `choco uninstall linkerd2-cli`
    
    The following is further work to be done:
    - Fill `linkerd2-cli.nuspec` appropriately
    - Fill `LICENSE.txt` and `VERIFICATION.txt` appropriately
    
    Future work:
    - After approval future commits will add auto update feature for the package
    
    Signed-off-by: Animesh Narayan Dangwal <animesh.leo@gmail.com>

---
+++ windowspkg/linkerd2-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+
+  packageName    = 'linkerd'
+  fileFullPath   = "$toolsPath\linkerd.exe"
+  url            = 'https://github.com/linkerd/linkerd2/releases/download/stable-2.6.1/linkerd2-cli-stable-2.6.1-windows.exe'
+  checksum       = '4ed915b10c2a76070d40bf3223f09e742777105914acaeadfc83d99ef6cf1798'
+  checksumType   = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
+Install-ChocolateyPath $packageArgs.fileFullPath 'Machine'
