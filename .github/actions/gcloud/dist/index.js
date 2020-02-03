a97c44489ce30f62d39b183a34edb9eef80ed914
Alejandro Pedraza <alejandro@buoyant.io>
2020-02-03 13:01:58 -0500

    Got rid of cloud_cleanup by using a post hook in the gcloud action

--- .github/actions/gcloud/dist/index.js
+++ .github/actions/gcloud/dist/index.js
@@ -1530,18 +1530,9 @@ const core = __webpack_require__(907);
 const exec = __webpack_require__(649);
 const fs = __webpack_require__(747);
 
-function validate() {
-  switch (core.getInput('action')) {
-    case 'create':
-      break;
-    case 'destroy':
-      break;
-    case '':
-      break;
-    default:
-      throw 'Invalid value for "action"';
-  }
+const isPost = 'STATE_isPost';
 
+function validate() {
   if (core.getInput('release_channel') && core.getInput('cluster_version')) {
     throw 'At most one of --release-channel | --cluster-version may be specified';
   }
@@ -1589,55 +1580,79 @@ async function configure() {
     await exec.exec('gcloud config set core/project', [core.getInput('gcp_project')]);
     await exec.exec('gcloud config set compute/zone', [core.getInput('gcp_zone')]);
     await exec.exec('gcloud auth configure-docker --quiet');
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
 
-    if (core.getInput('create') || core.getInput('destroy')) {
+async function create(name) {
+  try {
+    const args = [
+      name,
+      '--machine-type', core.getInput('machine_type'),
+      '--num-nodes', core.getInput('num_nodes')
+    ];
+    if (core.getInput('release_channel')) {
+      args.push('--release-channel', core.getInput('release_channel'));
+    }
+    if (core.getInput('cluster_version')) {
+      args.push('--cluster-version', core.getInput('cluster_version'));
+    }
+    if (core.getInput('preemptible')) {
+      args.push('--preemptible');
+    }
+    if (core.getInput('enable_network_policy')) {
+      args.push('--enable-network-policy')
+    }
+    if (!core.getInput('enable_stackdriver')) {
+      args.push('--no-enable-stackdriver-kubernetes')
+    }
+    if (!core.getInput('enable_basic_auth')) {
+      args.push('--no-enable-basic-auth')
+    }
+    if (!core.getInput('enable_legacy_auth')) {
+      args.push('--no-enable-legacy-authorization')
+    }
+
+    // Needs beta in order to use --release-channel
+    await exec.exec('gcloud beta container clusters create', args);
+
+    await exec.exec('gcloud config set container/cluster',  [name]);
+    await exec.exec('gcloud container clusters get-credentials', [name]);
+
+    let sa;
+    await exec.exec('gcloud config get-value account', [], {
+      listeners: {
+        stdout: (data) => {
+          sa = data.toString()
+        }
+      }
+    });
+    await exec.exec('kubectl create clusterrolebinding ci-cluster-admin --clusterrole=cluster-admin',
+      ['--user', sa]);
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
+async function destroy(name) {
+  try {
+    await exec.exec('gcloud container clusters delete --quiet', [name]);
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
+async function run() {
+  try {
+    await configure();
+    if (core.getInput('create')) {
       const name = await getClusterName();
-      if (core.getInput('create')) {
-        const args = [
-          name,
-          '--machine-type', core.getInput('machine_type'),
-          '--num-nodes', core.getInput('num_nodes')
-        ];
-        if (core.getInput('release_channel')) {
-          args.push('--release-channel', core.getInput('release_channel'));
-        }
-        if (core.getInput('cluster_version')) {
-          args.push('--cluster-version', core.getInput('cluster_version'));
-        }
-        if (core.getInput('preemptible')) {
-          args.push('--preemptible');
-        }
-        if (core.getInput('enable_network_policy')) {
-          args.push('--enable-network-policy')
-        }
-        if (!core.getInput('enable_stackdriver')) {
-          args.push('--no-enable-stackdriver-kubernetes')
-        }
-        if (!core.getInput('enable_basic_auth')) {
-          args.push('--no-enable-basic-auth')
-        }
-        if (!core.getInput('enable_legacy_auth')) {
-          args.push('--no-enable-legacy-authorization')
-        }
-
-        // Needs beta in order to use --release-channel
-        await exec.exec('gcloud beta container clusters create', args);
-
-        await exec.exec('gcloud config set container/cluster',  [name]);
-        await exec.exec('gcloud container clusters get-credentials', [name]);
-
-        let sa;
-        await exec.exec('gcloud config get-value account', [], {
-          listeners: {
-            stdout: (data) => {
-              sa = data.toString()
-            }
-          }
-        });
-        await exec.exec('kubectl create clusterrolebinding ci-cluster-admin --clusterrole=cluster-admin',
-          ['--user', sa]);
+      if (!core.getState(isPost)) {
+        core.saveState(isPost, 'true');
+        await create(name);
       } else {
-        await exec.exec('gcloud container clusters delete --quiet', [name]);
+        await destroy(name);
       }
     }
   } catch (e) {
@@ -1648,7 +1663,7 @@ async function configure() {
 try {
     fs.writeFileSync(process.env.HOME + '/.gcp.json', core.getInput('cloud_sdk_service_account_key'));
     validate();
-    configure();
+    run();
 } catch (e) {
     core.setFailed(e.message);
 }
