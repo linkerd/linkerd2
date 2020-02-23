262287d8c88619fadce9997a712b37fc3ef51bcb
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-02-24 01:31:35 +0530

    Add relevant unit tests for mergeRaw
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- cli/cmd/install_test.go
+++ cli/cmd/install_test.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"path/filepath"
+	"reflect"
 	"testing"
+
+	"github.com/ghodss/yaml"
 
 	"github.com/linkerd/linkerd2/controller/gen/config"
 	pb "github.com/linkerd/linkerd2/controller/gen/config"
@@ -483,6 +486,63 @@ func TestValidate(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestMergeRaw(t *testing.T) {
+	t.Run("Test Ovewriting of Values struct", func(*testing.T) {
+
+		initialValues := charts.Values{
+			PrometheusImage:        "initial-prometheus",
+			EnableH2Upgrade:        true,
+			ControllerReplicas:     1,
+			OmitWebhookSideEffects: false,
+			InstallNamespace:       true,
+		}
+
+		// Overwrite values should not be unmarshalled from values struct as the zero values are added
+		// causing overwriting of fields not present in the inital struct to zero values. This can be mitigated
+		// partially by using omitempty, but then we don't have relevant checks in helm templates as they would
+		// be nil when omitempty is present.
+		rawOverwriteValues := `
+prometheusImage: override-prometheus
+enableH2Upgrade: false
+controllerReplicas: 2
+omitWebhookSideEffects: true
+enablePodAntiAffinity: true`
+
+		expectedValues := charts.Values{
+			PrometheusImage:        "override-prometheus",
+			EnableH2Upgrade:        false,
+			ControllerReplicas:     2,
+			OmitWebhookSideEffects: true,
+			EnablePodAntiAffinity:  true,
+			InstallNamespace:       true,
+		}
+
+		rawInitialValues, err := yaml.Marshal(initialValues)
+		if err != nil {
+			t.Fatalf("Error while Marshaling: %s", err)
+
+		}
+
+		actualRawValues, err := mergeRaw(rawInitialValues, []byte(rawOverwriteValues))
+		if err != nil {
+			t.Fatalf("Error while Merging: %s", err)
+
+		}
+
+		var actualValues charts.Values
+		err = yaml.Unmarshal(actualRawValues, &actualValues)
+		if err != nil {
+			t.Fatalf("Error while unmarshalling: %s", err)
+
+		}
+		if !reflect.DeepEqual(expectedValues, actualValues) {
+			t.Fatal("Expected and Actual not equal.")
+
+		}
+	})
+
 }
 
 func fakeHeartbeatSchedule() string {
