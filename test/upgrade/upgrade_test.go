c6383a61be5ae518ef852a7853462d53ed5978ff
Supratik Das <rick.das08@gmail.com>
2020-02-27 01:47:39 +0530

    Add integration test to validate kubectl applies updated config
    Kubectl's dropping of config objects was tackled in [this](https://github.com/linkerd/linkerd2/pull/4061). An integration test was required to ensure its effect.
    
    An integration test which compares a known kubectl apply config with generated one on updating config with custom value is (here it is `foo: bar`) is implemented
    
    go test -v ./test/upgrade -integration-tests -linkerd `pwd`/bin/linkerd gives all tests passed. It validates its success
    
    Signed-off-by: Supratik Das <rick.das08@gmail.com>

---
+++ test/upgrade/upgrade_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/linkerd/linkerd2/testutil"
+)
+
+//////////////////////
+///   TEST SETUP   ///
+//////////////////////
+
+var TestHelper *testutil.TestHelper
+
+func TestMain(m *testing.M) {
+	TestHelper = testutil.NewTestHelper()
+	os.Exit(m.Run())
+}
+
+//////////////////////
+/// TEST EXECUTION ///
+//////////////////////
+
+func TestUpgradeWithUpdatedConfig(t *testing.T) {
+	upgradeResources, err := testutil.ReadFile("testdata/label_update.yaml")
+	if err != nil {
+		t.Fatalf("failed to read label update file: %s", err)
+	}
+
+	kubectlOut, err := TestHelper.Kubectl(upgradeResources, "apply", "--prune", "-l", "linkerd.io/control-plane-ns=linkerd", "-oyaml", "-f", "-")
+	if err != nil {
+		t.Fatalf("kubectl get command failed with %s\n%s", err, kubectlOut)
+	}
+
+	var golden string = "kubectl.apply.golden"
+	err = TestHelper.ValidateOutput(kubectlOut, golden)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
