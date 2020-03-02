2d17d6253dd58728cf4acafc6fd59760462e6c51
Andrew Seigner <andrew@sig.gy>
2020-03-02 13:20:40 -0800

    Fix namespace-scoped Grafana links (#4119)
    The `/namespaces` page in the web dashboard was rendering broken Grafana
    links, containing an extra `var-namespace=` param, for example:
    ```
    /grafana/dashboard/db/linkerd-namespace?var-namespace=&var-namespace=emojivoto
    ```
    
    Root cause was the `GrafanaLink` component taking both `resource` and
    `namespace` properties, but not special-casing when
    `resource === 'namespace' && namespace === ''`.
    
    Modify the `GrafanaLink` component to omit the `var-namespace` param
    when a `namespace` property is not provided.
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- web/app/js/components/GrafanaLink.test.jsx
+++ web/app/js/components/GrafanaLink.test.jsx
@@ -26,4 +26,25 @@ describe('GrafanaLink', () => {
     expect(href).toContain(expectedNsStr);
     expect(href).toContain(expectedVarNameStr);
   });
+
+  it('makes a link without a namespace', () => {
+    let api = ApiHelpers('');
+    let linkProps = {
+      resource: "replicationcontroller",
+      name: "aldksf-3409823049823",
+      PrefixedLink: api.PrefixedLink
+    };
+    let component = mount(routerWrap(GrafanaLink, linkProps));
+
+    let expectedDashboardNameStr = "/linkerd-replicationcontroller";
+    let expectedVarNameStr = "var-replicationcontroller=aldksf-3409823049823";
+
+    expect(component.find("GrafanaLink")).toHaveLength(1);
+
+    const href = component.find('a').props().href;
+
+    expect(href).toContain(expectedDashboardNameStr);
+    expect(href).not.toContain("namespace");
+    expect(href).toContain(expectedVarNameStr);
+  });
 });
