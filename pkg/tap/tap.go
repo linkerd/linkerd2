5da5334aff58f0b9b8acb95bf2656a7edbfa44e8
Charles Pretzer <charles@buoyant.io>
2020-03-06 20:40:51 -0800

    fix formatting

--- pkg/tap/tap.go
+++ pkg/tap/tap.go
@@ -38,7 +38,7 @@ func Reader(k8sAPI *k8s.KubernetesAPI, req *pb.TapByResourceRequest, timeout tim
 	if err != nil {
 		return nil, nil, err
 	}
-	url.Path = fmt.Sprintf("%s%s",url.Path, protohttp.TapReqToURL(req))
+	url.Path = fmt.Sprintf("%s%s", url.Path, protohttp.TapReqToURL(req))
 
 	httpReq, err := http.NewRequest(
 		http.MethodPost,
