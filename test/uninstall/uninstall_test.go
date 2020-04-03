e5c8e97197fb3b95305f627b0b1730d95cb9a88f
Alejandro Pedraza <alejandro@buoyant.io>
2020-04-03 15:20:38 -0500

    Forgot to check for --uninstall

--- test/uninstall/uninstall_test.go
+++ test/uninstall/uninstall_test.go
@@ -12,6 +12,10 @@ var TestHelper *testutil.TestHelper
 
 func TestMain(m *testing.M) {
 	TestHelper = testutil.NewTestHelper()
+	if !TestHelper.Uninstall() {
+		fmt.Fprintln(os.Stderr, "Uninstall test disabled")
+		os.Exit(0)
+	}
 	os.Exit(m.Run())
 }
 
