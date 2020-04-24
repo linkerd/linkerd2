17de49aed6b7e0e36c738183ec25c35af182ed33
Alejandro Pedraza <alejandro@buoyant.io>
2020-04-24 15:20:09 -0500

    Appease linter

--- testutil/ext_err.go
+++ testutil/ext_err.go
@@ -10,23 +10,23 @@ import (
 
 const envFlag = "GH_ANNOTATION"
 
-// extError is a wrapper to the error type, extending it with the file and line
+// ExtError is a wrapper to the error type, extending it with the file and line
 // that produced the error, and a generic error description that is sent to stdout
 // as a Github annotation when the envFlag environment variable is set.
-type extError struct {
+type ExtError struct {
 	err  error
 	file string
 	line int
 	desc string
 }
 
-func newExtError(err error, desc string) extError {
+func newExtError(err error, desc string) ExtError {
 	_, fn, ln, ok := runtime.Caller(2)
 	if !ok {
 		panic("Couldn't recover runtime info")
 	}
 	fileName := fn[strings.LastIndex(fn, "/linkerd2/")+10:]
-	return extError{
+	return ExtError{
 		err:  err,
 		file: fileName,
 		line: ln,
@@ -36,7 +36,7 @@ func newExtError(err error, desc string) extError {
 
 // Error is a wrapper for test error messages; msg can either be
 // a string or an error type.
-func Error(msg interface{}) extError {
+func Error(msg interface{}) ExtError {
 	switch v := msg.(type) {
 	case error:
 		return newExtError(v, v.Error())
@@ -50,23 +50,23 @@ func Error(msg interface{}) extError {
 
 // Errorf is a wrapper for test error messages, following
 // the Printf() signature (using a format specifier)
-func Errorf(format string, args ...interface{}) extError {
+func Errorf(format string, args ...interface{}) ExtError {
 	err := fmt.Errorf(format, args...)
 	return newExtError(err, err.Error())
 }
 
-// WithAnn is to be called on a extError type to provide a generic
+// WithAnn is to be called on a ExtError type to provide a generic
 // error description
-func (extErr extError) WithAnn(desc string) extError {
+func (extErr ExtError) WithAnn(desc string) ExtError {
 	extErr.desc = desc
 	return extErr
 }
 
-// Error ensures that the struct extError implements the `error` interface.
-// When extError.Error() is called, the underlying Error() is returned, and
+// Error ensures that the struct ExtError implements the `error` interface.
+// When ExtError.Error() is called, the underlying Error() is returned, and
 // as a side-effect (only if the envFlag environment variable is set) a Github
 // annotation string is sent to stdout
-func (extErr extError) Error() string {
+func (extErr ExtError) Error() string {
 	if _, ok := os.LookupEnv(envFlag); ok {
 		fmt.Printf("::error file=%s,line=%d::%s\n", extErr.file, extErr.line, extErr.desc)
 	}
