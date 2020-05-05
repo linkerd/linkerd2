fc7456ce2a88d31279b6c459b6152b25f6ab11cf
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-05-05 14:11:21 +0530

    Refactor linkerd-config-addons configmap (#4318)
    * rename linkerd-values to linkerd-config-addons
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * refactor linkerd-config-addons to be more saner
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * add comments for the linkerd-config-addons
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * move _addon.tpl to partials
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * remove unnecessary checks in _addons.tpl
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * add _addon.tpl partials to TestRenderHelm
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>
    
    * remove on a copy
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -153,7 +153,7 @@ var (
 		"templates/sp-validator.yaml",
 		"templates/tap.yaml",
 		"templates/smi-metrics.yaml",
-		"templates/linkerd-values.yaml",
+		"templates/linkerd-config-addons.yaml",
 	}
 )
 
