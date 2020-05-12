bfd5b24dec2ae6c9b5520dce11f7d2097cc11e2b
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-05-12 19:29:42 +0530

    rename to prometheus test
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- cli/cmd/install_addon_test.go
+++ cli/cmd/install_addon_test.go
@@ -21,20 +21,20 @@ func TestAddOnRender(t *testing.T) {
 	withTracingAddonValues.Tracing["enabled"] = true
 	addFakeTLSSecrets(withTracingAddonValues)
 
-	withGrafanaAddOnOverwrite, err := testInstallOptions()
+	withPrometheusAddOnOverwrite, err := testInstallOptions()
 	if err != nil {
 		t.Fatalf("Unexpected error: %v\n", err)
 	}
-	withGrafanaAddOnOverwrite.addOnConfig = filepath.Join("testdata", "prom-config.yaml")
-	withGrafanaAddOnOverwriteValues, _, _ := withGrafanaAddOnOverwrite.validateAndBuild("", nil)
-	addFakeTLSSecrets(withGrafanaAddOnOverwriteValues)
+	withPrometheusAddOnOverwrite.addOnConfig = filepath.Join("testdata", "prom-config.yaml")
+	withPrometheusAddOnOverwriteValues, _, _ := withPrometheusAddOnOverwrite.validateAndBuild("", nil)
+	addFakeTLSSecrets(withPrometheusAddOnOverwriteValues)
 
 	testCases := []struct {
 		values         *charts.Values
 		goldenFileName string
 	}{
 		{withTracingAddonValues, "install_tracing.golden"},
-		{withGrafanaAddOnOverwriteValues, "install_prometheus_overwrite.golden"},
+		{withPrometheusAddOnOverwriteValues, "install_prometheus_overwrite.golden"},
 	}
 
 	for i, tc := range testCases {
