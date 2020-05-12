1bf1cecae0370c02a9341a04f2184248fe6ea56e
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-05-12 22:27:37 +0530

    linting fix
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- pkg/charts/linkerd2/values.go
+++ pkg/charts/linkerd2/values.go
@@ -84,7 +84,7 @@ type (
 		ControlPlaneTracing      bool   `json:"controlPlaneTracing"`
 		IdentityTrustAnchorsPEM  string `json:"identityTrustAnchorsPEM"`
 		IdentityTrustDomain      string `json:"identityTrustDomain"`
-		GrafanaUrl               string `json:"grafanaUrl"`
+		GrafanaURL               string `json:"grafanaUrl"`
 
 		Proxy     *Proxy     `json:"proxy"`
 		ProxyInit *ProxyInit `json:"proxyInit"`
