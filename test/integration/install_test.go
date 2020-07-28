c65058de35cd183cdf9d843b06e36bd0721951f7
Andrew Seigner <siggy@buoyant.io>
2020-07-27 20:28:32 -0700

    KubeCon EU demo
    Does this break CI?
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- test/integration/install_test.go
+++ test/integration/install_test.go
@@ -218,7 +218,7 @@ func TestInstallOrUpgradeCli(t *testing.T) {
 
 	if TestHelper.UpgradeFromVersion() != "" {
 
-		cmd = "upgrade"
+		cmd = "upgrade-broken"
 		// test 2-stage install during upgrade
 		out, stderr, err := TestHelper.LinkerdRun(cmd, "config")
 		if err != nil {
