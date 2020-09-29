de098cd52d892c94dbd37b46a165999f94af6460
Lutz Behnke <lutz.behnke@finleap.com>
2020-09-29 16:17:09 +0200

    make api service secrets compatible to cert manager (#4737)
    Currently the secrets for the proxy-injector, sp-validator webhooks and tap API service are using the Opaque secret type and linkerd-specific field names. This makes it impossible to use cert-manager (https://github.com/jetstack/cert-manager) to provisions and rotate the secrets for these services. This change converts the secrets defined in the linkerd2 helm charts and the controller use the kubernetes.io/tls format instead. This format is used for secrets containing the generated secrets by cert-manager.
    
    Signed-off-by: Lutz Behnke <lutz.behnke@finleap.com>

--- cli/cmd/upgrade_test.go
+++ cli/cmd/upgrade_test.go
@@ -331,6 +331,62 @@ func TestUpgradeOverwriteTracingAddon(t *testing.T) {
 	}
 }
 
+// this test constructs a set of secrets resources
+func TestUpgradeWebhookCrtsNameChange(t *testing.T) {
+	installOpts, installFlags, upgradeOpts, upgradeFlags := testOptionsAndFlags(t)
+
+	values := installValues(t, installOpts, installFlags)
+	injectorCerts := generateCerts(t, "linkerd-proxy-injector.linkerd.svc", false)
+	defer injectorCerts.cleanup()
+	values.ProxyInjector.TLS = &linkerd2.TLS{
+		CaBundle: injectorCerts.ca,
+		CrtPEM:   injectorCerts.crt,
+		KeyPEM:   injectorCerts.key,
+	}
+	tapCerts := generateCerts(t, "linkerd-tap.linkerd.svc", false)
+	defer tapCerts.cleanup()
+	values.Tap.TLS = &linkerd2.TLS{
+		CaBundle: tapCerts.ca,
+		CrtPEM:   tapCerts.crt,
+		KeyPEM:   tapCerts.key,
+	}
+	validatorCerts := generateCerts(t, "linkerd-sp-validator.linkerd.svc", false)
+	defer validatorCerts.cleanup()
+	values.ProfileValidator.TLS = &linkerd2.TLS{
+		CaBundle: validatorCerts.ca,
+		CrtPEM:   validatorCerts.crt,
+		KeyPEM:   validatorCerts.key,
+	}
+
+	rendered := renderInstall(t, values)
+	expected := replaceVersions(rendered.String())
+
+	// switch back to old tls secret names.
+	install := replaceK8sSecrets(expected)
+
+	upgrade, err := renderUpgrade(t, install, upgradeOpts, upgradeFlags)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedManifests := parseManifestList(expected)
+	upgradeManifests := parseManifestList(upgrade.String())
+	for id, diffs := range diffManifestLists(expectedManifests, upgradeManifests) {
+		for _, diff := range diffs {
+			t.Errorf("Unexpected diff in %s:\n%s", id, diff.String())
+		}
+	}
+}
+
+func replaceK8sSecrets(input string) string {
+	manifest := strings.ReplaceAll(input, "kubernetes.io/tls", "Opaque")
+	manifest = strings.ReplaceAll(manifest, "tls.key", "key.pem")
+	manifest = strings.ReplaceAll(manifest, "tls.crt", "crt.pem")
+	manifest = strings.ReplaceAll(manifest, "linkerd-proxy-injector-k8s-tls", "linkerd-proxy-injector-tls")
+	manifest = strings.ReplaceAll(manifest, "linkerd-tap-k8s-tls", "linkerd-tap-tls")
+	manifest = strings.ReplaceAll(manifest, "linkerd-sp-validator-k8s-tls", "linkerd-sp-validator-tls")
+	return manifest
+}
+
 func TestUpgradeTwoLevelWebhookCrts(t *testing.T) {
 	installOpts, installFlags, upgradeOpts, upgradeFlags := testOptionsAndFlags(t)
 
