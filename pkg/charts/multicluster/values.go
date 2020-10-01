b11e9f29ae909018adbd8e7b8339cb0b59f3c1d5
Dariusch Ochlast <dariusch.ochlast@gmail.com>
2020-10-01 13:35:19 +0200

    adjust muticluster structs
    Signed-off-by: Dariusch Ochlast <dariusch.ochlast@gmail.com>

--- pkg/charts/multicluster/values.go
+++ pkg/charts/multicluster/values.go
@@ -14,37 +14,46 @@ const (
 	helmDefaultLinkChartDir = "linkerd2-multicluster-link"
 )
 
-// Values contains the top-level elements in the Helm charts
-type Values struct {
-	CliVersion                     string `json:"cliVersion"`
-	ControllerComponentLabel       string `json:"controllerComponentLabel"`
-	ControllerImage                string `json:"controllerImage"`
-	ControllerImageVersion         string `json:"controllerImageVersion"`
-	CreatedByAnnotation            string `json:"createdByAnnotation"`
-	Gateway                        bool   `json:"gateway"`
-	GatewayLocalProbePath          string `json:"gatewayLocalProbePath"`
-	GatewayLocalProbePort          uint32 `json:"gatewayLocalProbePort"`
-	GatewayName                    string `json:"gatewayName"`
-	GatewayNginxImage              string `json:"gatewayNginxImage"`
-	GatewayNginxImageVersion       string `json:"gatewayNginxImageVersion"`
-	GatewayPort                    uint32 `json:"gatewayPort"`
-	GatewayProbePath               string `json:"gatewayProbePath"`
-	GatewayProbePort               uint32 `json:"gatewayProbePort"`
-	GatewayProbeSeconds            uint32 `json:"gatewayProbeSeconds"`
-	IdentityTrustDomain            string `json:"identityTrustDomain"`
-	InstallNamespace               bool   `json:"installNamespace"`
-	LinkerdNamespace               string `json:"linkerdNamespace"`
-	LinkerdVersion                 string `json:"linkerdVersion"`
-	Namespace                      string `json:"namespace"`
-	ProxyOutboundPort              uint32 `json:"proxyOutboundPort"`
-	ServiceMirror                  bool   `json:"serviceMirror"`
-	LogLevel                       string `json:"logLevel"`
-	ServiceMirrorRetryLimit        uint32 `json:"serviceMirrorRetryLimit"`
-	ServiceMirrorUID               int64  `json:"serviceMirrorUID"`
-	RemoteMirrorServiceAccount     bool   `json:"remoteMirrorServiceAccount"`
-	RemoteMirrorServiceAccountName string `json:"remoteMirrorServiceAccountName"`
-	TargetClusterName              string `json:"targetClusterName"`
-}
+type (
+	// Values contains the top-level elements in the Helm charts
+	Values struct {
+		CliVersion                     string   `json:"cliVersion"`
+		ControllerComponentLabel       string   `json:"controllerComponentLabel"`
+		ControllerImage                string   `json:"controllerImage"`
+		ControllerImageVersion         string   `json:"controllerImageVersion"`
+		CreatedByAnnotation            string   `json:"createdByAnnotation"`
+		Gateway                        bool     `json:"gateway"`
+		GatewayLocalProbePath          string   `json:"gatewayLocalProbePath"`
+		GatewayLocalProbePort          uint32   `json:"gatewayLocalProbePort"`
+		GatewayName                    string   `json:"gatewayName"`
+		GatewayNginxImage              string   `json:"gatewayNginxImage"`
+		GatewayNginxImageVersion       string   `json:"gatewayNginxImageVersion"`
+		GatewayPort                    uint32   `json:"gatewayPort"`
+		GatewayProbePath               string   `json:"gatewayProbePath"`
+		GatewayProbePort               uint32   `json:"gatewayProbePort"`
+		GatewayProbeSeconds            uint32   `json:"gatewayProbeSeconds"`
+		IdentityTrustDomain            string   `json:"identityTrustDomain"`
+		InstallNamespace               bool     `json:"installNamespace"`
+		LinkerdNamespace               string   `json:"linkerdNamespace"`
+		LinkerdVersion                 string   `json:"linkerdVersion"`
+		Namespace                      string   `json:"namespace"`
+		ProxyOutboundPort              uint32   `json:"proxyOutboundPort"`
+		ServiceMirror                  bool     `json:"serviceMirror"`
+		LogLevel                       string   `json:"logLevel"`
+		ServiceMirrorRetryLimit        uint32   `json:"serviceMirrorRetryLimit"`
+		ServiceMirrorUID               int64    `json:"serviceMirrorUID"`
+		RemoteMirrorServiceAccount     bool     `json:"remoteMirrorServiceAccount"`
+		RemoteMirrorServiceAccountName string   `json:"remoteMirrorServiceAccountName"`
+		TargetClusterName              string   `json:"targetClusterName"`
+		Service                        *Service `json:"service"`
+	}
+
+	// Service contains the detauls to be applied to the service
+	Service struct {
+		Annotations []map[string]string `json:"annotations"`
+		Labels      []map[string]string `json:"labels"`
+	}
+)
 
 // NewInstallValues returns a new instance of the Values type.
 func NewInstallValues() (*Values, error) {
