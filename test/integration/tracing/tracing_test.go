17cd4f661e3a3674d920a4b4e8027aa97e570406
Alejandro Pedraza <alejandro@buoyant.io>
2020-11-13 16:00:16 -0500

    Remove logs comparisons in integration tests (#5223)
    The rare cases where these tests were useful don't make up for the burden of
    maintaing them, having different k8s version change the messages and
    having unexpected warnings come up that didn't affect the final
    convergence of the system.
    
    With this we also revert the indirection added back in #4538 that
    fetched unmatched warnings after a test had failed.

--- test/integration/tracing/tracing_test.go
+++ test/integration/tracing/tracing_test.go
@@ -34,7 +34,7 @@ var TestHelper *testutil.TestHelper
 
 func TestMain(m *testing.M) {
 	TestHelper = testutil.NewTestHelper()
-	os.Exit(testutil.Run(m, TestHelper))
+	os.Exit(m.Run())
 }
 
 //////////////////////
