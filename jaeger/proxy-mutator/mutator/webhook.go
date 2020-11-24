dc0b59e9772add2b3180264c71f85fa10d3dd772
Alejandro Pedraza <alejandro@buoyant.io>
2020-11-24 20:00:29 +0000

    Jaeger injector mutating webhook
    Closes #5231. This is based off of the `alex/sep-tracing` branch.
    
    This webhook injects the `LINKERD2_PROXY_TRACE_COLLECTOR_SVC_ADDR`,
    `LINKERD2_PROXY_TRACE_COLLECTOR_SVC_NAME` and
    `LINKERD2_PROXY_TRACE_ATTRIBUTES_PATH` environment vars into the proxy
    spec when a pod is created, as well as the podinfo volume and its mount.
    If any of these are found to be present already in the pod spec, it
    exits without applying a patch.
    
    The `values.yaml` file has been expanded to include config for this
    webhook. In particular, one can define a `namespaceSelector` and/or a
    `objectSelector` to filter which pods will this webhook act on.
    
    The config entries in `values.yam` for `collectorSvcAddr` and
    `collectorSvcAccount` can be overriden with the
    `config.linkerd.io/trace-collector` and
    `config.alpha.linkerd.io/trace-collector-service-account` annotation at
    the namespace or pod spec level.
    
    ## How to test:
    ```bash
    docker build . -t ghcr.io/linkerd/jaeger-webhook:0.0.1 -f
    jaeger/proxy-mutator/Dockerfile
    k3d image import ghcr.io/linkerd/jaeger-webhook:0.0.1
    bin/helm-build
    linkerd install
    helm install jaeger jaeger/charts/jaeger
    linkerd inject https://run.linkerd.io/emojivoto.yml | kubectl apply -f -
    kubectl -n emojivoto get po -l app=emoji-svc -oyaml | grep -A1 TRACE
    ```
    
    ## Reinvocation policy
    The webhookconfig resource is configured with `reinvocationPolicy:
    IfNeeded` so that if the tracing injector gets triggered before the
    proxy injector, it will get triggered a second time after the proxy
    injector runs so it can act on the injected proxy. By default this won't
    be necessary because the webhooks run in alphabetical order (this is not
    documented in k8s docs though) so
    `linkerd-proxy-injector-webhook-config` will run before
    `linkerd-proxy-mutator-webhook-config`. In order to test the
    reinvocation mechanism, you can change the name of the former so it gets
    called first.
    
    I versioned the webhook image as `0.0.1`, but we can decide to align
    that with linkerd's main version tag.

---
+++ jaeger/proxy-mutator/mutator/webhook.go
@@ -0,0 +1,128 @@
+package mutator
+
+import (
+	"bytes"
+	"context"
+	"html/template"
+
+	"github.com/linkerd/linkerd2/controller/k8s"
+	"github.com/linkerd/linkerd2/controller/webhook"
+	labels "github.com/linkerd/linkerd2/pkg/k8s"
+	"github.com/prometheus/common/log"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	collectorSvcAddrAnnotation    = labels.ProxyConfigAnnotationsPrefix + "/trace-collector"
+	collectorSvcAccountAnnotation = labels.ProxyConfigAnnotationsPrefixAlpha +
+		"/trace-collector-service-account"
+)
+
+type Params struct {
+	ProxyIndex          int
+	CollectorSvcAddr    string
+	CollectorSvcAccount string
+}
+
+// Mutate returns an AdmissionResponse containing the patch, if any, to apply
+// to the proxy
+func Mutate(collectorSvcAddr, collectorSvcAccount string) webhook.Handler {
+	return func(
+		ctx context.Context,
+		api *k8s.API,
+		request *admissionv1beta1.AdmissionRequest,
+		recorder record.EventRecorder,
+	) (*admissionv1beta1.AdmissionResponse, error) {
+		log.Debugf("request object bytes: %s", request.Object.Raw)
+
+		admissionResponse := &admissionv1beta1.AdmissionResponse{
+			UID:     request.UID,
+			Allowed: true,
+		}
+
+		if collectorSvcAddr == "" {
+			return admissionResponse, nil
+		}
+
+		var pod *corev1.Pod
+		if err := yaml.Unmarshal(request.Object.Raw, &pod); err != nil {
+			return nil, err
+		}
+
+		params := Params{
+			ProxyIndex:          getProxyContainerIndex(pod.Spec.Containers),
+			CollectorSvcAddr:    collectorSvcAddr,
+			CollectorSvcAccount: collectorSvcAccount,
+		}
+		if params.ProxyIndex < 0 || alreadyMutated(pod, params.ProxyIndex) {
+			return admissionResponse, nil
+		}
+
+		namespace, err := api.NS().Lister().Get(request.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		applyOverrides(namespace, pod, &params)
+
+		t, err := template.New("tpl").Parse(tpl)
+		if err != nil {
+			return nil, err
+		}
+		var patchJSON bytes.Buffer
+		if err = t.Execute(&patchJSON, params); err != nil {
+			return nil, err
+		}
+
+		patchType := admissionv1beta1.PatchTypeJSONPatch
+		admissionResponse.Patch = patchJSON.Bytes()
+		admissionResponse.PatchType = &patchType
+
+		return admissionResponse, nil
+	}
+}
+
+func getProxyContainerIndex(containers []corev1.Container) int {
+	for i, c := range containers {
+		if c.Name == labels.ProxyContainerName {
+			return i
+		}
+	}
+	return -1
+}
+
+func alreadyMutated(pod *corev1.Pod, proxyIndex int) bool {
+	for _, v := range pod.Spec.Volumes {
+		if v.DownwardAPI != nil && v.Name == "podinfo" {
+			return true
+		}
+	}
+	for _, mount := range pod.Spec.Containers[proxyIndex].VolumeMounts {
+		if mount.Name == "podinfo" && mount.MountPath == "var/run/linkerd/podinfo" {
+			return true
+		}
+	}
+	for _, env := range pod.Spec.Containers[proxyIndex].Env {
+		if env.Name == "LINKERD2_PROXY_TRACE_ATTRIBUTES_PATH" ||
+			env.Name == "LINKERD2_PROXY_TRACE_COLLECTOR_SVC_ADDR" ||
+			env.Name == "LINKERD2_PROXY_TRACE_COLLECTOR_SVC_NAME" {
+			return true
+		}
+	}
+	return false
+}
+
+func applyOverrides(ns *corev1.Namespace, pod *corev1.Pod, params *Params) {
+	ann := ns.GetAnnotations()
+	for k, v := range pod.Annotations {
+		ann[k] = v
+	}
+	if override, ok := ann[collectorSvcAddrAnnotation]; ok {
+		params.CollectorSvcAddr = override
+	}
+	if override, ok := ann[collectorSvcAccountAnnotation]; ok {
+		params.CollectorSvcAccount = override
+	}
+}
