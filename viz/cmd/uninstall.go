facf8470fd408de525e8a0ede27d0dbeafd9b677
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-12-22 11:41:56 +0000

    viz: add a new uninstall command
    Depends on #3340
    
    This adds a new `linkerd viz uninstall` command emitting the resources
    with the `linkerd.io/extension=linkerd-viz` label set.
    
    This is similar to that of the other linkerd uninstall commands.
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

---
+++ viz/cmd/uninstall.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/linkerd/linkerd2/pkg/k8s"
+	"github.com/linkerd/linkerd2/pkg/k8s/resource"
+	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func newCmdUninstall() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "uninstall",
+		Args:  cobra.NoArgs,
+		Short: "Output Kubernetes resources to uninstall the Linkerd viz extension",
+		Long: `Output Kubernetes resources to uninstall the Linkerd viz extension.
+
+This command provides all Kubernetes namespace-scoped and cluster-scoped resources (e.g services, deployments, RBACs, etc.) necessary to uninstall the Linkerd-viz extension.`,
+		Example: `linkerd uninstall | kubectl delete -f -`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return uninstallRunE(cmd.Context())
+		},
+	}
+
+	return cmd
+}
+
+func uninstallRunE(ctx context.Context) error {
+	k8sAPI, err := k8s.NewAPI(kubeconfigPath, kubeContext, impersonate, impersonateGroup, 0)
+	if err != nil {
+		return err
+	}
+
+	resources, err := resource.FetchKubernetesResources(ctx, k8sAPI,
+		metav1.ListOptions{LabelSelector: "linkerd.io/extension=linkerd-viz"},
+	)
+	if err != nil {
+		return err
+	}
+
+	for _, r := range resources {
+		if err := r.RenderResource(os.Stdout); err != nil {
+			return fmt.Errorf("error rendering Kubernetes resource: %v", err)
+		}
+	}
+	return nil
+}
