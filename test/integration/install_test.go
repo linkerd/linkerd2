b21c0a009a2f0fd6f1e44bd455b0d6464075025b
Tarun Pothulapati <tarunpothulapati@outlook.com>
2021-01-19 09:09:51 +0000

    wait for injector before install
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- test/integration/install_test.go
+++ test/integration/install_test.go
@@ -404,6 +404,15 @@ func TestInstallOrUpgradeCli(t *testing.T) {
 			"'kubectl apply' command failed\n%s", out)
 	}
 
+	// Wait for the proxy injector to be up
+	name := "linkerd-proxy-injector"
+	ns := "linkerd"
+	o, err := TestHelper.Kubectl("", "--namespace="+ns, "wait", "--for=condition=available", "--timeout=120s", "deploy/"+name)
+	if err != nil {
+		testutil.AnnotatedFatalf(t, fmt.Sprintf("failed to wait for condition=available for deploy/%s in namespace %s", name, ns),
+			"failed to wait for condition=available for deploy/%s in namespace %s: %s: %s", name, ns, err, o)
+	}
+
 	// Install Linkerd Viz Extension
 	exec = append(vizCmd, vizArgs...)
 	out, err = TestHelper.LinkerdRun(exec...)
