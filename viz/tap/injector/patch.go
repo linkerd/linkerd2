f10fa668451a260437237dd306f9be482d1b069a
Kevin Leimkuhler <kevin@kleimkuhler.com>
2021-02-02 18:23:11 +0000

    Fix patch
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- viz/tap/injector/patch.go
+++ viz/tap/injector/patch.go
@@ -10,7 +10,7 @@ var tpl = `[
     "op": "add",
     "path": "/spec/containers/{{.ProxyIndex}}/env/-",
     "value": {
-      "name": LINKERD2_PROXY_TAP_SVC_NAME,
+      "name": "LINKERD2_PROXY_TAP_SVC_NAME",
       "value": "{{.ProxyTapSvcName}}"
     }
   }
