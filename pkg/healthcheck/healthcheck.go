8e0aa6ae509344de9e693df245f776aeede544fb
Alex Leong <alex@buoyant.io>
2021-02-02 15:31:13 -0800

    Add better error message when upgrade cannot load config
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -680,9 +680,10 @@ func (hc *HealthChecker) allCategories() []Category {
 					fatal:       true,
 					check: func(ctx context.Context) (err error) {
 						hc.uuid, hc.linkerdConfig, err = hc.checkLinkerdConfigConfigMap(ctx)
-						if hc.linkerdConfig != nil {
-							hc.CNIEnabled = hc.linkerdConfig.GetGlobal().CNIEnabled
+						if hc.linkerdConfig == nil {
+							return errors.New("failed to load linkerd-config")
 						}
+						hc.CNIEnabled = hc.linkerdConfig.GetGlobal().CNIEnabled
 						return
 					},
 				},
@@ -1694,6 +1695,9 @@ func FetchCurrentConfiguration(ctx context.Context, k kubernetes.Interface, cont
 		return configMap, &fullValues, nil
 	}
 
+	if configPb == nil {
+		return configMap, nil, nil
+	}
 	// fall back to the older configMap
 	// TODO: remove this once the newer config override secret becomes the default i.e 2.10
 	return configMap, config.ToValues(configPb), nil
