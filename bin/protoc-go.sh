f039104ba73a90e6dc3fb609944912243093013c
Kevin Leimkuhler <kevin@kleimkuhler.com>
2021-02-04 15:31:42 +0000

    Rename tap.proto to viz_tap.proto
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -14,10 +14,10 @@ mkdir -p controller/gen/common/healthcheck controller/gen/common/net controller/
 "$bindir"/protoc -I proto --go_out=plugins=grpc,paths=source_relative:controller/gen proto/config/config.proto
 "$bindir"/protoc -I proto --go_out=plugins=grpc,paths=source_relative:controller/gen proto/common/healthcheck.proto
 "$bindir"/protoc -I proto -I viz/metrics-api/proto --go_out=plugins=grpc,paths=source_relative:viz/metrics-api/gen viz/metrics-api/proto/viz.proto
-"$bindir"/protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go_out=plugins=grpc,paths=source_relative:viz/tap/gen viz/tap/proto/tap.proto
+"$bindir"/protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go_out=plugins=grpc,paths=source_relative:viz/tap/gen viz/tap/proto/viz_tap.proto
 
 mv controller/gen/common/net.pb.go   controller/gen/common/net/
 mv controller/gen/public.pb.go controller/gen/public/
 mv controller/gen/common/healthcheck.pb.go   controller/gen/common/healthcheck/
 mv viz/metrics-api/gen/viz.pb.go viz/metrics-api/gen/viz/viz.pb.go
-mv viz/tap/gen/tap.pb.go viz/tap/gen/tap/tap.pb.go
+mv viz/tap/gen/viz_tap.pb.go viz/tap/gen/tap/viz_tap.pb.go
