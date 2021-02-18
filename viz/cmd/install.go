ddc27e03bf57b1caf0d0114a5aaf4f2147435073
Tarun Pothulapati <tarunpothulapati@outlook.com>
2021-02-18 12:15:53 +0000

    cli: make jaeger and multicluster installs wait for core cp
    This PR updates the jaeger and multicluster installs to wait
    for the core control-plane to be up before moving to the rendering
    logic. This prevents these components from being installed before
    the injector is up and running correctly.
    
    `--skip-checks` has been added to jaeger to skip these checks. The
    same has not been added to `multicluster` as the install fails when
    there is no core cp is present.
    
    This PR also cleans up extra core cp check that we have for `viz install`
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- viz/cmd/install.go
+++ viz/cmd/install.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"path"
@@ -12,7 +11,6 @@ import (
 	partials "github.com/linkerd/linkerd2/pkg/charts/static"
 	"github.com/linkerd/linkerd2/pkg/flags"
 	"github.com/linkerd/linkerd2/pkg/healthcheck"
-	"github.com/linkerd/linkerd2/pkg/k8s"
 	api "github.com/linkerd/linkerd2/pkg/public"
 	"github.com/linkerd/linkerd2/viz/static"
 	"github.com/spf13/cobra"
@@ -60,22 +58,7 @@ A full list of configurable values can be found at https://www.github.com/linker
   `,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if !skipChecks {
-				// Ensure there is a Linkerd installation.
-				kubeAPI, err := k8s.NewAPI(kubeconfigPath, kubeContext, impersonate, impersonateGroup, 0)
-				if err != nil {
-					return err
-				}
-
-				exists, err := healthcheck.CheckIfLinkerdExists(cmd.Context(), kubeAPI, controlPlaneNamespace)
-				if err != nil {
-					return fmt.Errorf("could not check for Linkerd existence: %s", err)
-				}
-
-				if !exists {
-					return fmt.Errorf("could not find a Linkerd installation")
-				}
-
-				// Wait for the core extension to be up and running
+				// Wait for the core control-plane to be up and running
 				api.CheckPublicAPIClientOrRetryOrExit(healthcheck.Options{
 					ControlPlaneNamespace: controlPlaneNamespace,
 					KubeConfig:            kubeconfigPath,
@@ -93,7 +76,7 @@ A full list of configurable values can be found at https://www.github.com/linker
 
 	cmd.Flags().BoolVar(
 		&skipChecks, "skip-checks", false,
-		`Skip checks for namespace existence`,
+		`Skip checks for linkerd core control-plane existence`,
 	)
 
 	cmd.Flags().BoolVar(
