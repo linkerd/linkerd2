9a3704380fec1ec7aa3a13104fc5cc8a145afca2
Kevin Leimkuhler <kevin@kleimkuhler.com>
2021-02-18 20:08:29 +0000

    Remove unused ParseOpaquePorts
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- pkg/util/parsing.go
+++ pkg/util/parsing.go
@@ -35,6 +35,16 @@ func ParseContainerOpaquePorts(override string, containers []corev1.Container) [
 	return values
 }
 
+// GetPortRanges gets port ranges from an override annotation
+func GetPortRanges(override string) []*config.PortRange {
+	split := strings.Split(strings.TrimSuffix(override, ","), ",")
+	ports := make([]*config.PortRange, len(split))
+	for i, p := range split {
+		ports[i] = &config.PortRange{PortRange: p}
+	}
+	return ports
+}
+
 // isNamed checks if a port range is actually a container named port (e.g.
 // `123-456` is a valid name, but also is a valid range); all port names must
 // be checked before making it a list.
@@ -47,31 +57,4 @@ func isNamed(pr string, containers []corev1.Container) (int32, bool) {
 		}
 	}
 	return 0, false
-}
-
-// ParseOpaquePorts parses the opaque ports annotation into a list of ports
-func ParseOpaquePorts(override string) []string {
-	portRanges := GetPortRanges(override)
-	var values []string
-	for _, portRange := range portRanges {
-		pr, err := ports.ParsePortRange(portRange.GetPortRange())
-		if err != nil {
-			log.Warnf("Invalid port range [%v]: %s", pr, err)
-			continue
-		}
-		for i := pr.LowerBound; i <= pr.UpperBound; i++ {
-			values = append(values, strconv.Itoa(i))
-		}
-	}
-	return values
-}
-
-// GetPortRanges gets port ranges from an override annotation
-func GetPortRanges(override string) []*config.PortRange {
-	split := strings.Split(strings.TrimSuffix(override, ","), ",")
-	ports := make([]*config.PortRange, len(split))
-	for i, p := range split {
-		ports[i] = &config.PortRange{PortRange: p}
-	}
-	return ports
 }
