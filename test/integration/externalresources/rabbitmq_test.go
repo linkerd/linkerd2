cdd356944df0a23b07efe1376ee83386e7c8640e
Kris Barkar <kristin.barkardottir@gmail.com>
2021-03-15 10:10:28 +0000

    Rabbitmq integration tests. (#5757)
    Add integration tests for external resources
    
    linkerd changes sometimes causes regressions on external components that users have installed in their stacks such as rabbitmq.
    
    This PR adds a new integration test, adds functionality to install additional components and run basic test on them to make sure linkerd changes do not have an adverse effect w.r.t RabbitMq.
    
    This change will include the following steps
    - Deploy a rabbitmq server
    - Deploy a rabbitmq client, the code for that client is hosted at https://github.com/barkardk/integration
    - Use a golden file for output verification
    - Uses the test suite to inject linkerd into the deployments
    - Client then creates a queue, a message and consumes said message and sends a log output.
    
    Signed-off-by: Kris Barkar <kristin.barkardottir@gmail.com>

---
+++ test/integration/externalresources/rabbitmq_test.go
@@ -0,0 +1,87 @@
+package externalresources
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/linkerd/linkerd2/testutil"
+)
+
+var TestHelper *testutil.TestHelper
+
+func TestMain(m *testing.M) {
+	TestHelper = testutil.NewTestHelper()
+	os.Exit(m.Run())
+}
+
+//////////////////////
+/// TEST EXECUTION ///
+//////////////////////
+
+func TestRabbitMQDeploy(t *testing.T) {
+	ctx := context.Background()
+	TestHelper.WithDataPlaneNamespace(ctx, "rabbitmq-test", map[string]string{}, t, func(t *testing.T, testNamespace string) {
+		out, err := TestHelper.LinkerdRun("inject", "--manual", "testdata/rabbitmq-server.yaml")
+		// inject rabbitmq server
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "'linkerd inject' command failed", "'linkerd inject' command failed: %s", err)
+		}
+		// deploy rabbitmq server
+		_, err = TestHelper.KubectlApply(out, testNamespace)
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "kubectl apply command failed", "'kubectl apply' command failed: %s", err)
+		}
+		if err := TestHelper.CheckPods(ctx, testNamespace, "rabbitmq", 1); err != nil {
+			if rce, ok := err.(*testutil.RestartCountError); ok {
+				testutil.AnnotatedWarn(t, "CheckPods timed-out %s", rce)
+			} else {
+				testutil.AnnotatedError(t, "CheckPods timed-out %s", err)
+			}
+		}
+		if err := TestHelper.CheckDeployment(ctx, testNamespace, "rabbitmq", 1); err != nil {
+			testutil.AnnotatedErrorf(t, "CheckDeployment  timed-out", "Error validating deployment [%s]: \n%s", "rabbitmq", err)
+		}
+		// inject rabbitmq-client
+		stdout, err := TestHelper.LinkerdRun("inject", "--manual", "testdata/rabbitmq-client.yaml")
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "'linkerd inject' command failed", "'linkerd inject' command failed: %s", err)
+		}
+		// deploy rabbitmq client
+		_, err = TestHelper.KubectlApply(stdout, testNamespace)
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "kubectl apply command failed", "'kubectl apply' command failed: %s", err)
+		}
+		if err := TestHelper.CheckPods(ctx, testNamespace, "rabbitmq-client", 1); err != nil {
+			if rce, ok := err.(*testutil.RestartCountError); ok {
+				testutil.AnnotatedWarn(t, "CheckPods timed-out %s", rce)
+			} else {
+				testutil.AnnotatedError(t, "CheckPods timed-out %s", err)
+			}
+		}
+		if err := TestHelper.CheckDeployment(ctx, testNamespace, "rabbitmq-client", 1); err != nil {
+			testutil.AnnotatedErrorf(t, "CheckDeployment  timed-out", "Error validating deployment [%s]: \n%s", "rabbitmq", err)
+		}
+		// Verify client output
+		golden := "check.rabbitmq.golden"
+		timeout := 50 * time.Second
+		err = TestHelper.RetryFor(timeout, func() error {
+			out, err := TestHelper.Kubectl("", "-n", testNamespace, "logs", "-lapp=rabbitmq-client", "-crabbitmq-client")
+			if err != nil {
+				return fmt.Errorf("'kubectl logs -l app=rabbitmq-client -c rabbitmq-client' command failed\n%s", err)
+			}
+			err = TestHelper.ValidateOutput(out, golden)
+			if err != nil {
+				return fmt.Errorf("received unexpected output\n%s", err.Error())
+			}
+			return nil
+		})
+		if err != nil {
+			testutil.AnnotatedFatal(t, fmt.Sprintf("'kubectl logs' command timed-out (%s)", timeout), err)
+		}
+
+	})
+
+}
