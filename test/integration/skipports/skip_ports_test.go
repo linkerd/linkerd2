78363ca89400eb7563491608156c554c2b60927b
Dennis Adjei-Baah <dennis@buoyant.io>
2021-04-16 11:44:17 -0500

    Disable protocol and TLS hints on skipped ports (#6022)
    When a pod is configured with `skip-inbound-ports` annotation, a client
    proxy trying to connect to that pod tries to connect to it via H2 and
    also tries to initiate a TLS connection. This issue is caused by the
    destination controller when it sends protocol and TLS hints to the
    client proxy for that skipped port.
    
    This change fixes the destination controller so that it no longer
    sends protocol and TLS identity hints to outbound proxies resolving a
    `podIP:port` that is on a skipped inbound port.
    
    I've included a test that exhibits this error prior to this fix but you
    can also test the prior behavior by:
    
    ```bash
    curl https://run.linkerd.io/booksapp.yml > booksapp.yaml
    
    # edit either the books or authors service to:
    1: Configure a failure rate of 0.0
    2: add the `skip-inbound-ports` config annotation
    
    bin/linkerd viz stat pods webapp
    
    There should be no successful requests on the webapp deployment
    ```
    Fixes #5995
    
    Signed-off-by: Dennis Adjei-Baah <dennis@buoyant.io>

---
+++ test/integration/skipports/skip_ports_test.go
@@ -0,0 +1,80 @@
+package skipports
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/linkerd/linkerd2/testutil"
+)
+
+var TestHelper *testutil.TestHelper
+
+var (
+	skipPortsNs               = "skip-ports-test"
+	booksappDeployments       = []string{"books", "traffic", "authors", "webapp"}
+	httpResponseTotalMetricRE = regexp.MustCompile(
+		`route_response_total\{direction="outbound",dst="books\.skip-ports-test\.svc\.cluster\.local:7002",classification="failure".*`,
+	)
+)
+
+func TestMain(m *testing.M) {
+	TestHelper = testutil.NewTestHelper()
+	os.Exit(m.Run())
+}
+
+//////////////////////
+/// TEST EXECUTION ///
+//////////////////////
+
+func TestSkipInboundPorts(t *testing.T) {
+	ctx := context.Background()
+	TestHelper.WithDataPlaneNamespace(ctx, skipPortsNs, nil, t, func(t *testing.T, ns string) {
+		out, err := TestHelper.LinkerdRun("inject", "--manual", "testdata/skip_ports_application.yaml")
+		if err != nil {
+			testutil.AnnotatedFatal(t, "'linkerd inject' command failed", err)
+		}
+		out, err = TestHelper.KubectlApply(out, ns)
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "'kubectl apply' command failed",
+				"'kubectl apply' command failed\n%s", out)
+		}
+
+		// Check all booksapp deployments are up and running
+		for _, deploy := range booksappDeployments {
+			if err := TestHelper.CheckPods(ctx, ns, deploy, 1); err != nil {
+				if rce, ok := err.(*testutil.RestartCountError); ok {
+					testutil.AnnotatedWarn(t, "CheckPods timed-out", rce)
+				} else {
+					testutil.AnnotatedError(t, "CheckPods timed-out", err)
+				}
+			}
+		}
+
+		// Wait for slow-cookers to start sending requests
+		time.Sleep(30 * time.Second)
+
+		t.Run("expect webapp to not have any 5xx response errors", func(t *testing.T) {
+			pods, err := TestHelper.GetPods(ctx, ns, map[string]string{"app": "webapp"})
+			if err != nil {
+				testutil.AnnotatedFatalf(t, "error getting pods", "error getting pods\n%s", err)
+			}
+
+			podName := fmt.Sprintf("pod/%s", pods[0].Name)
+			cmd := []string{"diagnostics", "proxy-metrics", "--namespace", ns, podName}
+
+			metrics, err := TestHelper.LinkerdRun(cmd...)
+			if err != nil {
+				testutil.AnnotatedFatalf(t, "error getting metrics for pod", "error getting metrics for pod\n%s", err)
+			}
+
+			if httpResponseTotalMetricRE.MatchString(metrics) {
+				testutil.AnnotatedFatalf(t, "expected not to find HTTP outbound response failures to dst=books.skip-ports-test.svc.cluster.local:7002",
+					"expected not to find HTTP outbound requests when pod is skipping inbound port\n%s", metrics)
+			}
+		})
+	})
+}
