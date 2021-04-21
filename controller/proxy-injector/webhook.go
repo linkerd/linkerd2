linkerd/linkerd2

commit a11012819c366574e07fadbbdf4e4a341d0cae54

Add opaque ports namespace inheritance to pods (#5941)
### What

When a namespace has the opaque ports annotation, pods and services should
inherit it if they do not have one themselves. Currently, services do this but
pods do not. This can lead to surprising behavior where services are correctly
marked as opaque, but pods are not.

This changes the proxy-injector so that it now passes down the opaque ports
annotation to pods from their namespace if they do not have their own annotation
set. Closes #5736.

### How

The proxy-injector webhook receives admission requests for pods and services.
Regardless of the resource kind, it now checks if the resource should inherit
the opaque ports annotation from its namespace. It should inherit it if the
namespace has the annotation but the resource does not.

If the resource should inherit the annotation, the webhook creates an annotation
patch which is only responsible for adding the opaque ports annotation.

After generating the annotation patch, it checks if the resource is injectable.
From here there are a few scenarios:

1. If no annotation patch was created and the resource is not injectable, then
   admit the request with no changes. Examples of this are services with no OP
   annotation and inject-disabled pods with no OP annotation.
2. If the resource is a pod and it is injectable, create a patch that includes
   the proxy and proxy-init containers—as well as any other annotations and
   labels.
3. The above two scenarios lead to a patch being generated at this point, so no
   matter the resource the patch is returned.

### UI changes

Resources are now reported to either be "injected", "skipped", or "annotated".

The first pass at this PR worked around the fact that injection reports consider
services and namespaces injectable. This is not accurate because they don't have
pod templates that could be injected; they can however be annotated.

controller/proxy-injector/webhook.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"strings"
 
-	"k8s.io/apimachinery/pkg/labels"
-
 	"github.com/linkerd/linkerd2/controller/k8s"
 	"github.com/linkerd/linkerd2/pkg/config"
 	"github.com/linkerd/linkerd2/pkg/inject"
@@ -15,6 +13,7 @@ import (
 	log "github.com/sirupsen/logrus"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 )
@@ -34,35 +33,34 @@ func Inject(
 ) (*admissionv1beta1.AdmissionResponse, error) {
 	log.Debugf("request object bytes: %s", request.Object.Raw)
 
+	// Build the resource config based off the request metadata and kind of
+	// object. This is later used to build the injection report and generated
+	// patch.
 	valuesConfig, err := config.Values(pkgK8s.MountPathValuesConfig)
 	if err != nil {
 		return nil, err
 	}
-
 	namespace, err := api.NS().Lister().Get(request.Namespace)
 	if err != nil {
 		return nil, err
 	}
 	nsAnnotations := namespace.GetAnnotations()
-
 	resourceConfig := inject.NewResourceConfig(valuesConfig, inject.OriginWebhook).
 		WithOwnerRetriever(ownerRetriever(ctx, api, request.Namespace)).
 		WithNsAnnotations(nsAnnotations).
 		WithKind(request.Kind.Kind)
+
+	// Build the injection report.
 	report, err := resourceConfig.ParseMetaAndYAML(request.Object.Raw)
 	if err != nil {
 		return nil, err
 	}
 	log.Infof("received %s", report.ResName())
 
-	admissionResponse := &admissionv1beta1.AdmissionResponse{
-		UID:     request.UID,
-		Allowed: true,
-	}
-
-	configLabels := configToPrometheusLabels(resourceConfig)
+	// If the resource has an owner, then it should be retrieved for recording
+	// events.
 	var parent *runtime.Object
-	ownerKind := ""
+	var ownerKind string
 	if ownerRef := resourceConfig.GetOwnerRef(); ownerRef != nil {
 		objs, err := api.GetObjects(request.Namespace, ownerRef.Kind, ownerRef.Name, labels.Everything())
 		if err != nil {
@@ -74,9 +72,64 @@ func Inject(
 		}
 		ownerKind = strings.ToLower(ownerRef.Kind)
 	}
+
+	configLabels := configToPrometheusLabels(resourceConfig)
 	proxyInjectionAdmissionRequests.With(admissionRequestLabels(ownerKind, request.Namespace, report.InjectAnnotationAt, configLabels)).Inc()
 
-	if injectable, reasons := report.Injectable(); !injectable {
+	// If the pod's namespace has the opaque ports annotation but the pod does
+	// not, then it should be added to the pod template metadata.
+	opaquePorts, opaquePortsOk := resourceConfig.GetOpaquePorts()
+	if resourceConfig.IsPod() && opaquePortsOk {
+		resourceConfig.AppendPodAnnotation(pkgK8s.ProxyOpaquePortsAnnotation, opaquePorts)
+	}
+
+	// If the resource is injectable then admit it after creating a patch that
+	// adds the proxy-init and proxy containers.
+	injectable, reasons := report.Injectable()
+	if injectable {
+		resourceConfig.AppendPodAnnotation(pkgK8s.CreatedByAnnotation, fmt.Sprintf("linkerd/proxy-injector %s", version.Version))
+		patchJSON, err := resourceConfig.GetPodPatch(true)
+		if err != nil {
+			return nil, err
+		}
+		if parent != nil {
+			recorder.Event(*parent, v1.EventTypeNormal, eventTypeInjected, "Linkerd sidecar proxy injected")
+		}
+		log.Infof("injection patch generated for: %s", report.ResName())
+		log.Debugf("injection patch: %s", patchJSON)
+		proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "false", "", report.InjectAnnotationAt, configLabels)).Inc()
+		patchType := admissionv1beta1.PatchTypeJSONPatch
+		return &admissionv1beta1.AdmissionResponse{
+			UID:       request.UID,
+			Allowed:   true,
+			PatchType: &patchType,
+			Patch:     patchJSON,
+		}, nil
+	}
+
+	// If the resource is not injectable but does need the opaque ports
+	// annotation added, then admit it after creating a patch that adds the
+	// annotation.
+	if opaquePortsOk {
+		patchJSON, err := resourceConfig.CreateAnnotationPatch(opaquePorts)
+		if err != nil {
+			return nil, err
+		}
+		log.Infof("annotation patch generated for: %s", report.ResName())
+		log.Debugf("annotation patch: %s", patchJSON)
+		proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "false", "", report.InjectAnnotationAt, configLabels)).Inc()
+		patchType := admissionv1beta1.PatchTypeJSONPatch
+		return &admissionv1beta1.AdmissionResponse{
+			UID:       request.UID,
+			Allowed:   true,
+			PatchType: &patchType,
+			Patch:     patchJSON,
+		}, nil
+	}
+
+	// The resource should be admitted without a patch. If it is a pod, create
+	// an event to record that injection was skipped.
+	if resourceConfig.IsPod() {
 		var readableReasons, metricReasons string
 		metricReasons = strings.Join(reasons, ",")
 		for _, reason := range reasons {
@@ -89,37 +142,15 @@ func Inject(
 		}
 		log.Infof("skipped %s: %s", report.ResName(), readableReasons)
 		proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "true", metricReasons, report.InjectAnnotationAt, configLabels)).Inc()
-		return admissionResponse, nil
+		return &admissionv1beta1.AdmissionResponse{
+			UID:     request.UID,
+			Allowed: true,
+		}, nil
 	}
-
-	resourceConfig.AppendPodAnnotations(map[string]string{
-		pkgK8s.CreatedByAnnotation: fmt.Sprintf("linkerd/proxy-injector %s", version.Version),
-	})
-	var patchJSON []byte
-	if resourceConfig.IsService() {
-		patchJSON, err = resourceConfig.GetServicePatch()
-	} else {
-		patchJSON, err = resourceConfig.GetPodPatch(true)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	if len(patchJSON) == 0 {
-		return admissionResponse, nil
-	}
-
-	if parent != nil {
-		recorder.Event(*parent, v1.EventTypeNormal, eventTypeInjected, "Linkerd sidecar proxy injected")
-	}
-	log.Infof("patch generated for: %s", report.ResName())
-	log.Debugf("patch: %s", patchJSON)
-	proxyInjectionAdmissionResponses.With(admissionResponseLabels(ownerKind, request.Namespace, "false", "", report.InjectAnnotationAt, configLabels)).Inc()
-	patchType := admissionv1beta1.PatchTypeJSONPatch
-	admissionResponse.Patch = patchJSON
-	admissionResponse.PatchType = &patchType
-
-	return admissionResponse, nil
+	return &admissionv1beta1.AdmissionResponse{
+		UID:     request.UID,
+		Allowed: true,
+	}, nil
 }
 
 func ownerRetriever(ctx context.Context, api *k8s.API, ns string) inject.OwnerRetrieverFunc {

pkg/inject/inject.go
@@ -90,12 +90,13 @@ type OwnerRetrieverFunc func(*corev1.Pod) (string, string)
 
 // ResourceConfig contains the parsed information for a given workload
 type ResourceConfig struct {
-	// These values used for the rendering of the patch may be further overridden
-	// by the annotations on the resource or the resource's namespace.
+	// These values used for the rendering of the patch may be further
+	// overridden by the annotations on the resource or the resource's
+	// namespace.
 	values *l5dcharts.Values
 	// These annotations from the resources's namespace are used as a base.
-	// The resources's annotations will be applied on top of these, which allows
-	// the nsAnnotations to act as a default.
+	// The resources's annotations will be applied on top of these, which
+	// allows the nsAnnotations to act as a default.
 	nsAnnotations  map[string]string
 	ownerRetriever OwnerRetrieverFunc
 	origin         Origin
@@ -103,11 +104,9 @@ type ResourceConfig struct {
 	workload struct {
 		obj      runtime.Object
 		metaType metav1.TypeMeta
-
 		// Meta is the workload's metadata. It's exported so that metadata of
 		// non-workload resources can be unmarshalled by the YAML parser
-		Meta *metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
-
+		Meta     *metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 		ownerRef *metav1.OwnerReference
 	}
 
@@ -136,8 +135,9 @@ type podPatch struct {
 	DebugContainer        *l5dcharts.DebugContainer `json:"debugContainer"`
 }
 
-type servicePatch struct {
-	OpaquePorts string
+type annotationPatch struct {
+	AddRootAnnotations bool
+	OpaquePorts        string
 }
 
 // NewResourceConfig creates and initializes a ResourceConfig
@@ -148,6 +148,7 @@ func NewResourceConfig(values *l5dcharts.Values, origin Origin) *ResourceConfig 
 		origin:        origin,
 	}
 
+	config.workload.Meta = &metav1.ObjectMeta{}
 	config.pod.meta = &metav1.ObjectMeta{}
 
 	// Values can be nil for commands like Uninject
@@ -305,24 +306,36 @@ func (conf *ResourceConfig) GetPodPatch(injectProxy bool) ([]byte, error) {
 	return res, nil
 }
 
-// GetServicePatch returns the JSON patch containing the service opaque ports
-// annotation if the annotation is present on the namespace, but absent on the
-// service.
-func (conf *ResourceConfig) GetServicePatch() ([]byte, error) {
-	_, ok := conf.workload.Meta.Annotations[k8s.ProxyOpaquePortsAnnotation]
-	// There does not need to be a patch if the service already has the
-	// annotation.
+// GetOpaquePorts returns two values. The first value is the the opaque ports
+// annotation value. The second is used to decide whether or not the caller
+// should add the annotation. The caller should not add the annotation if the
+// resource already has its own.
+func (conf *ResourceConfig) GetOpaquePorts() (string, bool) {
+	_, ok := conf.pod.meta.Annotations[k8s.ProxyOpaquePortsAnnotation]
 	if ok {
-		return nil, nil
+		log.Debugf("using pod %s %s annotation value", conf.pod.meta.Name, k8s.ProxyOpaquePortsAnnotation)
+		return "", false
 	}
-	opaquePorts, ok := conf.nsAnnotations[k8s.ProxyOpaquePortsAnnotation]
-	// There does not need to be a patch if the namespace does not have the
-	// annotation.
-	if !ok {
-		return nil, nil
+	_, ok = conf.workload.Meta.Annotations[k8s.ProxyOpaquePortsAnnotation]
+	if ok {
+		log.Debugf("using service %s %s annotation value", conf.workload.Meta.Name, k8s.ProxyOpaquePortsAnnotation)
+		return "", false
 	}
-	patch := &servicePatch{
-		OpaquePorts: opaquePorts,
+	annotation, ok := conf.nsAnnotations[k8s.ProxyOpaquePortsAnnotation]
+	if ok {
+		log.Debugf("using namespace %s %s annotation value", conf.workload.Meta.Namespace, k8s.ProxyOpaquePortsAnnotation)
+		return annotation, true
+	}
+	return "", false
+}
+
+// CreateAnnotationPatch returns a json patch which adds the opaque ports
+// annotation with the `opaquePorts` value.
+func (conf *ResourceConfig) CreateAnnotationPatch(opaquePorts string) ([]byte, error) {
+	addRootAnnotations := len(conf.pod.meta.Annotations) == 0
+	patch := &annotationPatch{
+		AddRootAnnotations: addRootAnnotations,
+		OpaquePorts:        opaquePorts,
 	}
 	t, err := template.New("tpl").Parse(tpl)
 	if err != nil {
@@ -484,7 +497,6 @@ func (conf *ResourceConfig) parse(bytes []byte) error {
 		}
 		conf.workload.obj = v
 		conf.workload.Meta = &v.ObjectMeta
-		// If annotations not present previously
 		if conf.workload.Meta.Annotations == nil {
 			conf.workload.Meta.Annotations = map[string]string{}
 		}
@@ -528,6 +540,9 @@ func (conf *ResourceConfig) parse(bytes []byte) error {
 			}
 		}
 		conf.pod.labels[k8s.WorkloadNamespaceLabel] = v.Namespace
+		if conf.pod.meta.Annotations == nil {
+			conf.pod.meta.Annotations = map[string]string{}
+		}
 
 	case *corev1.Service:
 		if err := yaml.Unmarshal(bytes, v); err != nil {
@@ -547,16 +562,15 @@ func (conf *ResourceConfig) parse(bytes []byte) error {
 		}
 	}
 
-	if conf.pod.meta.Annotations == nil {
-		conf.pod.meta.Annotations = map[string]string{}
-	}
-
 	return nil
 }
 
 func (conf *ResourceConfig) complete(template *corev1.PodTemplateSpec) {
 	conf.pod.spec = &template.Spec
 	conf.pod.meta = &template.ObjectMeta
+	if conf.pod.meta.Annotations == nil {
+		conf.pod.meta.Annotations = map[string]string{}
+	}
 }
 
 // injectPodSpec adds linkerd sidecars to the provided PodSpec.
@@ -921,20 +935,28 @@ func (conf *ResourceConfig) IsService() bool {
 	return strings.ToLower(conf.workload.metaType.Kind) == k8s.Service
 }
 
-//InjectNamespace annotates any given Namespace config
-func (conf *ResourceConfig) InjectNamespace(annotations map[string]string) ([]byte, error) {
+// IsPod checks if a given config is a workload of Kind pod.
+func (conf *ResourceConfig) IsPod() bool {
+	return strings.ToLower(conf.workload.metaType.Kind) == k8s.Pod
+}
+
+// HasPodTemplate checks if a given config has a pod template spec.
+func (conf *ResourceConfig) HasPodTemplate() bool {
+	return conf.pod.meta != nil && conf.pod.spec != nil
+}
+
+// AnnotateNamespace annotates a namespace resource config with `annotations`.
+func (conf *ResourceConfig) AnnotateNamespace(annotations map[string]string) ([]byte, error) {
 	ns, ok := conf.workload.obj.(*corev1.Namespace)
 	if !ok {
 		return nil, errors.New("can't inject namespace. Type assertion failed")
 	}
 	ns.Annotations[k8s.ProxyInjectAnnotation] = k8s.ProxyInjectEnabled
-	//For overriding annotations
 	if len(annotations) > 0 {
 		for annotation, value := range annotations {
 			ns.Annotations[annotation] = value
 		}
 	}
-
 	j, err := getFilteredJSON(ns)
 	if err != nil {
 		return nil, err
@@ -942,23 +964,18 @@ func (conf *ResourceConfig) InjectNamespace(annotations map[string]string) ([]by
 	return yaml.JSONToYAML(j)
 }
 
-// AnnotateService returns a Service with the appropriate annotations
-// Currently, a `Service` may only need the `config.linkerd.io/opaque-ports` annotation via `inject`
-// See - https://github.com/linkerd/linkerd2/pull/5721
+// AnnotateService annotates a service resource config with `annotations`.
 func (conf *ResourceConfig) AnnotateService(annotations map[string]string) ([]byte, error) {
-	ns, ok := conf.workload.obj.(*corev1.Service)
+	service, ok := conf.workload.obj.(*corev1.Service)
 	if !ok {
 		return nil, errors.New("can't inject service. Type assertion failed")
 	}
-
-	//For overriding annotations
 	if len(annotations) > 0 {
 		for annotation, value := range annotations {
-			ns.Annotations[annotation] = value
+			service.Annotations[annotation] = value
 		}
 	}
-
-	j, err := getFilteredJSON(ns)
+	j, err := getFilteredJSON(service)
 	if err != nil {
 		return nil, err
 	}

commit 99d15f887715277a0d0ec10e319db9e809994914

Add ns annotation inheritance to pods (#6002) (#6002)
Closes #5977  

## What

This changes adds support for namespace configuration annotation inheritance for pods. Any annotations (e.g `config.linkerd.io/skip-outbound-ports` or `config.linkerd.io/proxy-await`) that are applied against a namespace will now also be applied to pods running in that namespace by the _proxy-injector_. 

* Pods do not inherit annotations from their namespaces; the exception to this is `opaque-ports` introduced in #5941. This expands on the work by allowing all config annotations to be inherited.
* Main advantage here is that instead of applying annotations on a workload-by-workload basis we can just apply them against the namespace and it will be mirrored on all pods within the namespace.
* Through this change the controller can also check the proxy's configuration directly from the pod's meta rather than from env variables.

## How

Change is pretty straightforward. We want to make sure that before we apply a JSON patch we first copy all of the namespace annotations to the pod. The logic that was in place takes care of applying the patch.

* One obvious constraint is that we want only want valid configuration annotations to be applied. To be a "valid" configuration it has to exist and it has to be prefixed with `config.linkerd.io` -- the easiest way to do this is to go through all of the available proxy configuration options and check whether any of the options are included in the namespace's annotations (done in `GetNsConfigKeys()` where we fetch all annotation keys from the namespace).
* A consideration I had with this change is whether to add `opaque-ports` as part of all of the config keys; opaque ports is a bit different though since it can be applied on a pod as well as a service -- through this change we only want to apply config annotations to pods. I chose to keep the two separate.

controller/proxy-injector/webhook.go
@@ -76,18 +76,15 @@ func Inject(
 	configLabels := configToPrometheusLabels(resourceConfig)
 	proxyInjectionAdmissionRequests.With(admissionRequestLabels(ownerKind, request.Namespace, report.InjectAnnotationAt, configLabels)).Inc()
 
-	// If the pod's namespace has the opaque ports annotation but the pod does
-	// not, then it should be added to the pod template metadata.
-	opaquePorts, opaquePortsOk := resourceConfig.GetOpaquePorts()
-	if resourceConfig.IsPod() && opaquePortsOk {
-		resourceConfig.AppendPodAnnotation(pkgK8s.ProxyOpaquePortsAnnotation, opaquePorts)
-	}
-
 	// If the resource is injectable then admit it after creating a patch that
 	// adds the proxy-init and proxy containers.
 	injectable, reasons := report.Injectable()
 	if injectable {
 		resourceConfig.AppendPodAnnotation(pkgK8s.CreatedByAnnotation, fmt.Sprintf("linkerd/proxy-injector %s", version.Version))
+
+		// If namespace has annotations that do not exist on pod then copy them
+		// over to pod's template.
+		resourceConfig.AppendNamespaceAnnotations()
 		patchJSON, err := resourceConfig.GetPodPatch(true)
 		if err != nil {
 			return nil, err
@@ -110,7 +107,7 @@ func Inject(
 	// If the resource is not injectable but does need the opaque ports
 	// annotation added, then admit it after creating a patch that adds the
 	// annotation.
-	if opaquePortsOk {
+	if opaquePorts, opaquePortsOk := resourceConfig.GetConfigAnnotation(pkgK8s.ProxyOpaquePortsAnnotation); opaquePortsOk {
 		patchJSON, err := resourceConfig.CreateAnnotationPatch(opaquePorts)
 		if err != nil {
 			return nil, err

pkg/inject/inject.go
@@ -52,6 +52,7 @@ var (
 		k8s.ProxyCPULimitAnnotation,
 		k8s.ProxyCPURequestAnnotation,
 		k8s.ProxyImageAnnotation,
+		k8s.ProxyLogFormatAnnotation,
 		k8s.ProxyLogLevelAnnotation,
 		k8s.ProxyMemoryLimitAnnotation,
 		k8s.ProxyMemoryRequestAnnotation,
@@ -63,6 +64,11 @@ var (
 		k8s.ProxyIgnoreOutboundPortsAnnotation,
 		k8s.ProxyOutboundConnectTimeout,
 		k8s.ProxyInboundConnectTimeout,
+	}
+	// ProxyAlphaConfigAnnotations is the list of all alpha configuration
+	// (config.alpha prefix) that can be applied to a pod or namespace.
+	ProxyAlphaConfigAnnotations = []string{
+		k8s.ProxyWaitBeforeExitSecondsAnnotation,
 	}
 )
 
@@ -186,6 +192,29 @@ func (conf *ResourceConfig) GetOwnerRef() *metav1.OwnerReference {
 	return conf.workload.ownerRef
 }
 
+// AppendNamespaceAnnotations allows pods to inherit config specific annotations
+// from the namespace they belong to. If the namespace has a valid config key
+// that the pod does not, then it is appended to the pod's template
+func (conf *ResourceConfig) AppendNamespaceAnnotations() {
+	for _, key := range ProxyAnnotations {
+		if _, found := conf.nsAnnotations[key]; !found {
+			continue
+		}
+		if val, ok := conf.GetConfigAnnotation(key); ok {
+			conf.AppendPodAnnotation(key, val)
+		}
+	}
+
+	for _, key := range ProxyAlphaConfigAnnotations {
+		if _, found := conf.nsAnnotations[key]; !found {
+			continue
+		}
+		if val, ok := conf.GetConfigAnnotation(key); ok {
+			conf.AppendPodAnnotation(key, val)
+		}
+	}
+}
+
 // AppendPodAnnotations appends the given annotations to the pod spec in conf
 func (conf *ResourceConfig) AppendPodAnnotations(annotations map[string]string) {
 	for annotation, value := range annotations {
@@ -220,7 +249,6 @@ func (conf *ResourceConfig) ParseMetaAndYAML(bytes []byte) (*Report, error) {
 // GetOverriddenValues returns the final Values struct which is created
 // by overiding annoatated configuration on top of default Values
 func (conf *ResourceConfig) GetOverriddenValues() (*linkerd2.Values, error) {
-
 	// Make a copy of Values and mutate that
 	copyValues, err := conf.values.DeepCopy()
 	if err != nil {
@@ -306,24 +334,24 @@ func (conf *ResourceConfig) GetPodPatch(injectProxy bool) ([]byte, error) {
 	return res, nil
 }
 
-// GetOpaquePorts returns two values. The first value is the the opaque ports
-// annotation value. The second is used to decide whether or not the caller
+// GetConfigAnnotation returns two values. The first value is the the annotation
+// value for a given key. The second is used to decide whether or not the caller
 // should add the annotation. The caller should not add the annotation if the
 // resource already has its own.
-func (conf *ResourceConfig) GetOpaquePorts() (string, bool) {
-	_, ok := conf.pod.meta.Annotations[k8s.ProxyOpaquePortsAnnotation]
+func (conf *ResourceConfig) GetConfigAnnotation(annotationKey string) (string, bool) {
+	_, ok := conf.pod.meta.Annotations[annotationKey]
 	if ok {
-		log.Debugf("using pod %s %s annotation value", conf.pod.meta.Name, k8s.ProxyOpaquePortsAnnotation)
+		log.Debugf("using pod %s %s annotation value", conf.pod.meta.Name, annotationKey)
 		return "", false
 	}
-	_, ok = conf.workload.Meta.Annotations[k8s.ProxyOpaquePortsAnnotation]
+	_, ok = conf.workload.Meta.Annotations[annotationKey]
 	if ok {
-		log.Debugf("using service %s %s annotation value", conf.workload.Meta.Name, k8s.ProxyOpaquePortsAnnotation)
+		log.Debugf("using service %s %s annotation value", conf.workload.Meta.Name, annotationKey)
 		return "", false
 	}
-	annotation, ok := conf.nsAnnotations[k8s.ProxyOpaquePortsAnnotation]
+	annotation, ok := conf.nsAnnotations[annotationKey]
 	if ok {
-		log.Debugf("using namespace %s %s annotation value", conf.workload.Meta.Namespace, k8s.ProxyOpaquePortsAnnotation)
+		log.Debugf("using namespace %s %s annotation value", conf.workload.Meta.Namespace, annotationKey)
 		return annotation, true
 	}
 	return "", false
@@ -697,11 +725,18 @@ func (conf *ResourceConfig) injectPodAnnotations(values *podPatch) {
 
 func (conf *ResourceConfig) applyAnnotationOverrides(values *l5dcharts.Values) {
 	annotations := make(map[string]string)
-	for k, v := range conf.nsAnnotations {
-		annotations[k] = v
-	}
 	for k, v := range conf.pod.meta.Annotations {
 		annotations[k] = v
+	}
+
+	// If injecting from CLI, skip applying overrides from new annotations;
+	// overrides in this case should already be applied through flags.
+	if conf.origin != OriginCLI {
+		// Override base values inferred from current pod annotations with
+		// values from annotations that will be applied to pod after the patch.
+		for k, v := range conf.pod.annotations {
+			annotations[k] = v
+		}
 	}
 
 	if override, ok := annotations[k8s.ProxyInjectAnnotation]; ok {

controller/proxy-injector/webhook_test.go
@@ -49,6 +49,16 @@ func confNsWithoutOpaquePorts() *inject.ResourceConfig {
 		WithNsAnnotations(map[string]string{pkgK8s.ProxyInjectAnnotation: pkgK8s.ProxyInjectEnabled})
 }
 
+func confNsWithConfigAnnotations() *inject.ResourceConfig {
+	return inject.
+		NewResourceConfig(values, inject.OriginWebhook).
+		WithNsAnnotations(map[string]string{
+			pkgK8s.ProxyInjectAnnotation:                pkgK8s.ProxyInjectEnabled,
+			pkgK8s.ProxyIgnoreOutboundPortsAnnotation:   "34567",
+			pkgK8s.ProxyWaitBeforeExitSecondsAnnotation: "300",
+			"config.linkerd.io/invalid-key":             "invalid-value",
+		})
+}
 func TestGetPodPatch(t *testing.T) {
 
 	values.Proxy.DisableIdentity = true
@@ -140,6 +150,7 @@ func TestGetPodPatch(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Unexpected error: %s", err)
 		}
+
 		expectedPatch, err := unmarshalPatch(expectedPatchBytes)
 		if err != nil {
 			t.Fatalf("Unexpected error: %s", err)
@@ -169,6 +180,45 @@ func TestGetPodPatch(t *testing.T) {
 				expectedPatchBytes, patchJSON)
 		}
 
+	})
+
+	t.Run("by checking pod inherits config annotations from namespace", func(t *testing.T) {
+		expectedPatchBytes, err := factory.FileContents("pod-with-ns-annotations.patch.json")
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+		expectedPatch, err := unmarshalPatch(expectedPatchBytes)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		pod, err := factory.FileContents("pod-inject-enabled.yaml")
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+		fakeReq := getFakePodReq(pod)
+		conf := confNsWithConfigAnnotations().
+			WithKind(fakeReq.Kind.Kind).
+			WithOwnerRetriever(ownerRetrieverFake)
+		_, err = conf.ParseMetaAndYAML(fakeReq.Object.Raw)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// The namespace has two config annotations: one valid and one invalid
+		// the pod patch should only contain the valid annotation.
+		conf.AppendNamespaceAnnotations()
+		patchJSON, err := conf.GetPodPatch(true)
+		if err != nil {
+			t.Fatalf("Unexpected PatchForAdmissionRequest error: %s", err)
+		}
+		actualPatch, err := unmarshalPatch(patchJSON)
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+		if !reflect.DeepEqual(expectedPatch, actualPatch) {
+			t.Fatalf("The actual patch didn't match what was expected.\nExpected: %s\nActual: %s", expectedPatchBytes, patchJSON)
+		}
 	})
 
 	t.Run("by checking container spec", func(t *testing.T) {
@@ -288,7 +338,7 @@ func TestGetAnnotationPatch(t *testing.T) {
 					t.Fatal(err)
 				}
 				var patchJSON []byte
-				opaquePorts, ok := fullConf.GetOpaquePorts()
+				opaquePorts, ok := fullConf.GetConfigAnnotation(pkgK8s.ProxyOpaquePortsAnnotation)
 				if ok {
 					fullConf.AppendPodAnnotation(pkgK8s.ProxyOpaquePortsAnnotation, opaquePorts)
 					patchJSON, err = fullConf.CreateAnnotationPatch(opaquePorts)

pkg/inject/inject_test.go
@@ -267,6 +267,7 @@ func TestGetOverriddenValues(t *testing.T) {
 				t.Fatal(err)
 			}
 
+			resourceConfig.AppendNamespaceAnnotations()
 			actual, err := resourceConfig.GetOverriddenValues()
 			if err != nil {
 				t.Fatal(err)