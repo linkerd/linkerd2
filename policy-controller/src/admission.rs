5960a485cf91af329bcc60b5b3a73823838d4bdf
Matei David <matei@buoyant.io>
2021-09-15 15:04:12 +0000

    * Change admission deserializing
    * Add RequestData struct
    
    Signed-off-by: Matei David <matei@buoyant.io>

--- policy-controller/src/admission.rs
+++ policy-controller/src/admission.rs
@@ -1,5 +1,6 @@
 use crate::api;
 use anyhow::{anyhow, bail, Result};
+use api::policy::ServerSpec;
 use kube::{api::Api, core::DynamicObject, ResourceExt};
 use std::convert::{Infallible, TryInto};
 use tracing::{debug, info, warn};
@@ -26,7 +27,7 @@ pub fn routes(client: kube::Client) -> BoxedFilter<(impl warp::Reply,)> {
 
             // Parse the server instance under review before doing anything with the API--i.e., if
             // this fails we don't have to waste the API calls.
-            let (ns, review_server) = match parse_server(req) {
+            let (ns, name, review_spec) = match parse_server(req) {
                 Ok(s) => s,
                 Err(error) => {
                     warn!(%error, "Failed to deserialize server from admission request");
@@ -48,10 +49,10 @@ pub fn routes(client: kube::Client) -> BoxedFilter<(impl warp::Reply,)> {
             };
 
             // If validation fails, deny admission.
-            let rsp = match validate(&review_server, &*servers.items) {
+            let rsp = match validate(&name, &review_spec, &*servers.items) {
                 Ok(()) => rsp,
                 Err(error) => {
-                    info!(%error, %ns, name = %review_server.name(), "Denying server");
+                    info!(%error, %ns, %name, "Denying server");
                     rsp.deny(error)
                 }
             };
@@ -72,31 +73,45 @@ fn ok(reply: impl warp::Reply + 'static) -> Result<Box<dyn warp::Reply>, Infalli
 }
 
 /// Parses a `Server` instance and it's namespace from the admission request.
-fn parse_server(req: Request) -> Result<(String, api::policy::Server)> {
+fn parse_server(req: Request) -> Result<(String, String, api::policy::ServerSpec)> {
     let obj = req.object.ok_or_else(|| anyhow!("missing server"))?;
-    let server = serde_json::from_value::<api::policy::Server>(obj.data)?;
-    let ns = server
+    let ns = obj
         .namespace()
         .ok_or_else(|| anyhow!("no 'namespace' field set on server"))?;
-    Ok((ns, server))
+    let name = obj.name();
+    let data = serde_json::from_value::<RequestData>(obj.data)?;
+    Ok((ns, name, data.spec))
 }
 
 /// Validates a new server (`review`) against existing `servers`.
-fn validate(review: &api::policy::Server, servers: &[api::policy::Server]) -> Result<()> {
+fn validate(
+    review_name: &str,
+    review_spec: &api::policy::ServerSpec,
+    servers: &[api::policy::Server],
+) -> Result<()> {
     for s in servers {
         // If the port and pod selectors select the same resources, fail the admission of the
         // server. Ignore existing instances of this Server (e.g., if the server's metadata is
         // changing).
-        if s.name() != review.name()
+        if s.name() != review_name
             // TODO(ver) this isn't rigorous about detecting servers that select the same port if one port
             // specifies a numeric port and the other specifies the port's name.
-            && s.spec.port == review.spec.port
+            && s.spec.port == review_spec.port
             // TODO(ver) We can probably detect overlapping selectors more effectively.
-            && s.spec.pod_selector == review.spec.pod_selector
+            && s.spec.pod_selector == review_spec.pod_selector
         {
             bail!("identical server spec already exists");
         }
     }
 
     Ok(())
+}
+
+/// RequestData is a wrapper around the `data` field of the AdmissionRequest.
+/// The data field contains a `serde_json::value::Value::Object` type that wraps
+/// the spec of the resource, deserializing directly to a ServiceSpec won't work
+/// in this case.
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct RequestData {
+    spec: ServerSpec,
 }
