7949d0ed681d8ebd27a7dae7664a6d7c400a6e1e
Kevin Leimkuhler <kevin@kleimkuhler.com>
2021-10-18 18:32:16 +0000

    Add script review feedback
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>

--- bin/update-codegen.sh
+++ bin/update-codegen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -eu
 
@@ -10,25 +10,17 @@ codegen_pkg=${GOPATH}/pkg/mod/k8s.io/code-generator@${gen_ver}
 # ROOT_PACKAGE :: the package that is the target for code generation
 ROOT_PACKAGE=github.com/linkerd/linkerd2
 
-# The name and version of the service profile CRD that we generate client code
-# for.
-service_profile_name=serviceprofile
-service_profile_versions=v1alpha2
+crds=(serviceprofile:v1alpha2 server:v1beta1 serverauthorization:v1beta1)
 
-# The name and version of the server CRD that we generate client code for.
-server_name=server
-server_version=v1beta1
-
-# The name and version of the server authorization CRD that we generate client
-# code for
-server_authorization_name=serverauthorization
-server_authorization_version=v1beta1
-
-rm -f "${rootdir}/controller/gen/apis/$service_profile_name/$service_profile_versions/zz_generated.deepcopy.go"
-rm -f "${rootdir}/controller/gen/apis/$server_name/$server_version/zz_generated.deepcopy.go"
-rm -f "${rootdir}/controller/gen/apis/$server_authorization_name/$server_authorization_version/zz_generated.deepcopy.go"
+# remove previously generated code
 rm -rf "${rootdir}/controller/gen/client"
 rm -rf "${GOPATH}/src/${ROOT_PACKAGE}/controller/gen"
+for crd in ${crds[@]}
+do
+  crd_path=$(tr : / <<< "$crd")
+  rm -f "${rootdir}/controller/gen/apis/${crd_path}/zz_generated.deepcopy.go"
+done
+
 
 chmod +x "${codegen_pkg}/generate-groups.sh"
 
@@ -37,7 +29,7 @@ GO111MODULE='on' "${codegen_pkg}/generate-groups.sh" \
   'deepcopy,client,informer,lister' \
   "${ROOT_PACKAGE}/controller/gen/client" \
   "${ROOT_PACKAGE}/controller/gen/apis" \
-  "$service_profile_name:$service_profile_versions $server_name:$server_version $server_authorization_name:$server_authorization_version" \
+  "${crds[*]}" \
   --go-header-file "${codegen_pkg}"/hack/boilerplate.go.txt
 
 # copy generated code out of GOPATH
