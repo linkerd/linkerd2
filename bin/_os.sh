839b969e1aa66cd356e218f204f3c4e852b6c998
Kevin Leimkuhler <kleimkuhler@icloud.com>
2021-11-23 11:09:32 -0700

    Handle unsupported architectures
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- bin/_os.sh
+++ bin/_os.sh
@@ -4,6 +4,32 @@ set -eu
 
 export OS_ARCH_ALL="linux-amd64 linux-arm64 linux-arm darwin darwin-arm64 windows"
 
+architecture() {
+  arch=$(uname -m)
+  case $arch in
+    x86_64)
+      arch=amd64
+      ;;
+    armv8*)
+      arch=arm64
+      ;;
+    aarch64*)
+      arch=arm64
+      ;;
+    armv*)
+      arch=arm
+      ;;
+    amd64|arm64)
+      arch=$arch
+      ;;
+    *)
+      echo "unsupported architecture: $arch" >&2
+      exit 1
+      ;;
+  esac
+  echo "$arch"
+}
+
 os() {
   os=$(uname -s)
   arch=""
@@ -16,28 +42,7 @@ os() {
       ;;
     Linux)
       os=linux
-      arch=$(uname -m)
-      case $arch in
-        x86_64)
-          arch=amd64
-          ;;
-        armv8*)
-          arch=arm64
-          ;;
-        aarch64*)
-          arch=arm64
-          ;;
-        armv*)
-          arch=arm
-          ;;
-        amd64|arm64)
-          arch=$arch
-          ;;
-        *)
-          echo "unsupported architecture: $arch" >&2
-          exit 1
-          ;;
-      esac
+      arch=architecture
       ;;
     *)
       echo "unsupported os: $os" >&2
