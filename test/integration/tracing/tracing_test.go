f2ad5a9b2ae7df8d581293a45aded2e5ea6319c7
Tarun Pothulapati <tarunpothulapati@outlook.com>
2022-01-17 22:25:33 +0530

    test: remove nginx ingress from tracing e2e (#7601)
    Part of #7538
    
    This PR removes the `nginx-ingress` from the tracing integration
    test, and also replaces the emojivoto manifest with the official
    manifest from the getting started guide.
    
    This should increase the speed of this test, as initializing `nginx-ingress`
    [seems to take a while](https://github.com/linkerd/linkerd2/issues/7538#issuecomment-1011092916).
    
    ```bash
    # Before
    Test script: [tracing] Params: []
    ok      github.com/linkerd/linkerd2/test/integration/tracing    222.845s
    # After
    Test script: [tracing] Params: []
    ok      github.com/linkerd/linkerd2/test/integration/tracing    170.030s
    ```
    
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- test/integration/tracing/tracing_test.go
+++ test/integration/tracing/tracing_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"html/template"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
@@ -49,11 +48,6 @@ func TestTracing(t *testing.T) {
 	ctx := context.Background()
 	if os.Getenv("RUN_ARM_TEST") != "" {
 		t.Skip("Skipped. Jaeger & Open Census images does not support ARM yet")
-	}
-
-	// Require an environment variable to be set for this test to be run.
-	if os.Getenv("RUN_FLAKEY_TEST") == "" {
-		t.Skip("Skipping due to flakiness. See linkerd/linkerd2#7538")
 	}
 
 	// linkerd-jaeger extension
@@ -113,130 +107,78 @@ func TestTracing(t *testing.T) {
 		testutil.AnnotatedFatal(t, fmt.Sprintf("'linkerd jaeger check' command timed-out (%s)", timeout), err)
 	}
 
-	// Emojivoto components
-	emojivotoNs := TestHelper.GetTestNamespace("emojivoto")
-	err = TestHelper.CreateDataPlaneNamespaceIfNotExists(ctx, emojivotoNs, nil)
-	if err != nil {
-		testutil.AnnotatedFatalf(t, fmt.Sprintf("failed to create %s namespace", emojivotoNs),
-			"failed to create %s namespace: %s", emojivotoNs, err)
-	}
-
-	emojivotoYaml, err := testutil.ReadFile("testdata/emojivoto.yaml")
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "failed to read emojivoto yaml",
-			"failed to read emojivoto yaml\n%s\n", err)
-	}
-	emojivotoYaml = strings.ReplaceAll(emojivotoYaml, "___TRACING_NS___", tracingNs)
-	out, stderr, err := TestHelper.PipeToLinkerdRun(emojivotoYaml, "inject", "-")
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "'linkerd inject' command failed",
-			"'linkerd inject' command failed\n%s\n%s", out, stderr)
-	}
-
-	out, err = TestHelper.KubectlApply(out, emojivotoNs)
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "'kubectl apply' command failed",
-			"'kubectl apply' command failed\n%s", out)
-	}
-
-	// Ingress components
-	// Ingress must run in the same namespace as the service it routes to (web)
-	ingressNs := emojivotoNs
-	err = TestHelper.CreateDataPlaneNamespaceIfNotExists(ctx, ingressNs, nil)
-	if err != nil {
-		testutil.AnnotatedFatalf(t, fmt.Sprintf("failed to create %s namespace", ingressNs),
-			"failed to create %s namespace: %s", ingressNs, err)
-	}
-
-	ingressYaml, err := testutil.ReadFile("testdata/ingress.yaml")
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "failed to read ingress yaml",
-			"failed to read ingress yaml\n%s\n", err)
-	}
-	ingressYaml = strings.ReplaceAll(ingressYaml, "___INGRESS_NAMESPACE___", ingressNs)
-	ingressYaml = strings.ReplaceAll(ingressYaml, "___TRACING_NS___", tracingNs)
-	out, stderr, err = TestHelper.PipeToLinkerdRun(ingressYaml, "inject", "-")
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "'linkerd inject' command failed",
-			"'linkerd inject' command failed\n%s\n%s", out, stderr)
-	}
-
-	out, err = TestHelper.KubectlApply(out, ingressNs)
-	if err != nil {
-		testutil.AnnotatedFatalf(t, "'kubectl apply' command failed",
-			"'kubectl apply' command failed\n%s", out)
-	}
-
-	// wait for deployments to start
-	for _, deploy := range []struct {
-		ns   string
-		name string
-	}{
-		{ns: emojivotoNs, name: "vote-bot"},
-		{ns: emojivotoNs, name: "web"},
-		{ns: emojivotoNs, name: "emoji"},
-		{ns: emojivotoNs, name: "voting"},
-		{ns: ingressNs, name: "nginx-ingress"},
-		{ns: tracingNs, name: "collector"},
-		{ns: tracingNs, name: "jaeger"},
-	} {
-		if err := TestHelper.CheckPods(ctx, deploy.ns, deploy.name, 1); err != nil {
-			if rce, ok := err.(*testutil.RestartCountError); ok {
-				testutil.AnnotatedWarn(t, "CheckPods timed-out", rce)
-			} else {
-				testutil.AnnotatedError(t, "CheckPods timed-out", err)
-			}
-		}
-	}
-
-	t.Run("expect full trace", func(t *testing.T) {
-
-		timeout := 120 * time.Second
-		err = TestHelper.RetryFor(timeout, func() error {
-			url, err := TestHelper.URLFor(ctx, tracingNs, "jaeger", 16686)
-			if err != nil {
-				return err
-			}
-
-			tracesJSON, err := TestHelper.HTTPGetURL(url + "/jaeger/api/traces?lookback=1h&service=nginx")
-			if err != nil {
-				return err
-			}
-			traces := traces{}
-
-			err = json.Unmarshal([]byte(tracesJSON), &traces)
-			if err != nil {
-				return err
-			}
-
-			processes := []string{"nginx", "web", "emoji", "voting", "linkerd-proxy"}
-			if !hasTraceWithProcesses(&traces, processes) {
-				return fmt.Errorf("No trace found with processes: %s", processes)
-			}
-			return nil
-		})
+	TestHelper.WithDataPlaneNamespace(ctx, "tracing-test", map[string]string{}, t, func(t *testing.T, namespace string) {
+		emojivotoYaml, err := testutil.ReadFile("testdata/emojivoto.yaml")
 		if err != nil {
-			testutil.AnnotatedFatal(t, fmt.Sprintf("timed-out checking trace (%s)", timeout), err)
+			testutil.AnnotatedFatalf(t, "failed to read emojivoto yaml",
+				"failed to read emojivoto yaml\n%s\n", err)
 		}
+
+		out, err = TestHelper.KubectlApply(emojivotoYaml, namespace)
+		if err != nil {
+			testutil.AnnotatedFatalf(t, "'kubectl apply' command failed",
+				"'kubectl apply' command failed\n%s", out)
+		}
+
+		// wait for deployments to start
+		for _, deploy := range []struct {
+			ns   string
+			name string
+		}{
+			{ns: namespace, name: "vote-bot"},
+			{ns: namespace, name: "web"},
+			{ns: namespace, name: "emoji"},
+			{ns: namespace, name: "voting"},
+			{ns: tracingNs, name: "collector"},
+			{ns: tracingNs, name: "jaeger"},
+		} {
+			if err := TestHelper.CheckPods(ctx, deploy.ns, deploy.name, 1); err != nil {
+				if rce, ok := err.(*testutil.RestartCountError); ok {
+					testutil.AnnotatedWarn(t, "CheckPods timed-out", rce)
+				} else {
+					testutil.AnnotatedError(t, "CheckPods timed-out", err)
+				}
+			}
+		}
+
+		t.Run("expect full trace", func(t *testing.T) {
+			timeout := 3 * time.Minute
+			err = TestHelper.RetryFor(timeout, func() error {
+				url, err := TestHelper.URLFor(ctx, tracingNs, "jaeger", 16686)
+				if err != nil {
+					return err
+				}
+
+				tracesJSON, err := TestHelper.HTTPGetURL(url + "/jaeger/api/traces?lookback=1h&service=linkerd-proxy")
+				if err != nil {
+					return err
+				}
+				traces := traces{}
+
+				err = json.Unmarshal([]byte(tracesJSON), &traces)
+				if err != nil {
+					return err
+				}
+
+				if !hasTraceWithProcess(&traces, "linkerd-proxy") {
+					return fmt.Errorf("No trace found with processes: linkerd-proxy")
+				}
+				return nil
+			})
+			if err != nil {
+				testutil.AnnotatedFatal(t, fmt.Sprintf("timed-out checking trace (%s)", timeout), err)
+			}
+		})
 	})
 }
 
-func hasTraceWithProcesses(traces *traces, ps []string) bool {
+func hasTraceWithProcess(traces *traces, ps string) bool {
 	for _, trace := range traces.Data {
-		if containsProcesses(trace, ps) {
-			return true
+		for _, process := range trace.Processes {
+			if process.ServiceName == ps {
+				return true
+			}
 		}
 	}
 	return false
-}
-
-func containsProcesses(trace trace, ps []string) bool {
-	toFind := make(map[string]struct{})
-	for _, p := range ps {
-		toFind[p] = struct{}{}
-	}
-	for _, p := range trace.Processes {
-		delete(toFind, p.ServiceName)
-	}
-	return len(toFind) == 0
 }
