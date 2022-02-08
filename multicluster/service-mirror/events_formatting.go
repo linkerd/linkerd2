9089cc623e25730a5a0fe63dd596e1779a89ca1f
Oliver Gould <ver@buoyant.io>
2022-02-07 19:16:43 -0800

    multicluster: Fix metadata prefix matching (#7825)
    When formatting event metadata, the method `formatMetadata` checks if a
    given string in the metadata map contains a given prefix. However it
    does this using `strings.Contains` rather than `strings.HasPrefix`,
    which will return true if the given prefix string is located anywhere in
    the target string to search.
    
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- multicluster/service-mirror/events_formatting.go
+++ multicluster/service-mirror/events_formatting.go
@@ -21,7 +21,7 @@ func formatMetadata(meta map[string]string) string {
 	var metadata []string
 
 	for k, v := range meta {
-		if strings.Contains(k, consts.Prefix) || strings.Contains(k, consts.ProxyConfigAnnotationsPrefix) {
+		if strings.HasPrefix(k, consts.Prefix) || strings.HasPrefix(k, consts.ProxyConfigAnnotationsPrefix) {
 			metadata = append(metadata, fmt.Sprintf("%s=%s", k, v))
 		}
 	}
