c1f0dc61fa0c5c132efb085440ced83290ff22b6
Kevin Leimkuhler <kleimkuhler@icloud.com>
2022-02-17 17:45:43 -0700

    Issue individual warnings for nodes missing podCIDRs (#7862)
    Closes #7760
    
    This introduces a new warning check that ensures we can verify the cluster networks. This check is separate from the existing `cluster networks contains all node podCIDRs` because unlike this existing check which results in an error, this new check is just a warning.
    
    When a node is missing the field
    
    ```
    $ linkerd check -o short
    Linkerd core checks
    ===================
    
    linkerd-existence
    -----------------
    ‼ cluster networks can be verified
        the following nodes do not expose a podCIDR:
            k3d-k3s-default-agent-0
            k3d-k3s-default-server-0
            k3d-k3s-default-agent-1
        see https://linkerd.io/2/checks/#l5d-cluster-networks-verified for hints
    ```
    
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -739,6 +739,27 @@ func (hc *HealthChecker) allCategories() []*Category {
 					},
 				},
 				{
+					description: "cluster networks can be verified",
+					hintAnchor:  "l5d-cluster-networks-verified",
+					warning:     true,
+					check: func(ctx context.Context) error {
+						nodes, err := hc.kubeAPI.GetNodes(ctx)
+						if err != nil {
+							return err
+						}
+						var warningNodes []string
+						for _, node := range nodes {
+							if node.Spec.PodCIDR == "" {
+								warningNodes = append(warningNodes, node.Name)
+							}
+						}
+						if len(warningNodes) > 0 {
+							return fmt.Errorf("the following nodes do not expose a podCIDR:\n\t%s", strings.Join(warningNodes, "\n\t"))
+						}
+						return nil
+					},
+				},
+				{
 					description: "cluster networks contains all node podCIDRs",
 					hintAnchor:  "l5d-cluster-networks-cidr",
 					check: func(ctx context.Context) error {
@@ -1876,6 +1897,7 @@ func (hc *HealthChecker) checkClusterNetworks(ctx context.Context) error {
 			badPodCIDRS = append(badPodCIDRS, podCIDR)
 		}
 	}
+	// If none of the nodes exposed a podCIDR then we cannot verify the clusterNetworks.
 	if !podCIDRExists {
 		// DigitalOcean for example, doesn't expose spec.podCIDR (#6398)
 		return SkipError{Reason: podCIDRUnavailableSkipReason}
