cb50e45f19aac5cf558740c2a363f35d5069beff
Oliver Gould <ver@buoyant.io>
2022-03-02 23:02:06 +0000

    Remove unused type
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- policy-controller/k8s/api/src/policy.rs
+++ policy-controller/k8s/api/src/policy.rs
@@ -3,10 +3,3 @@ pub mod server_authorization;
 
 pub use self::server::{Server, ServerSpec};
 pub use self::server_authorization::{ServerAuthorization, ServerAuthorizationSpec};
-
-#[derive(Default, serde::Deserialize, serde::Serialize, Clone, Debug, schemars::JsonSchema)]
-pub struct TargetRef {
-    pub group: Option<String>,
-    pub kind: String,
-    pub name: Option<String>,
-}
