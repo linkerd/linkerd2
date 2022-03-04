155138a0d800ef2038d8e2a1138239b9e8970d2a
Oliver Gould <ver@buoyant.io>
2022-03-04 05:34:41 +0000

    Kill admission boilerplate
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- policy-controller/src/admission.rs
+++ policy-controller/src/admission.rs
@@ -35,6 +35,10 @@ type AdmissionRequest = kube::core::admission::AdmissionRequest<DynamicObject>;
 type AdmissionResponse = kube::core::admission::AdmissionResponse;
 type AdmissionReview = kube::core::admission::AdmissionReview<DynamicObject>;
 
+trait Validate {
+    fn validate(&self, ns: &str, name: &str, index: &Index) -> Result<()>;
+}
+
 // === impl AdmissionService ===
 
 impl hyper::service::Service<Request<Body>> for AdmissionService {
@@ -86,21 +90,30 @@ impl hyper::service::Service<Request<Body>> for AdmissionService {
     }
 }
 
+fn json_response(rsp: AdmissionReview) -> Result<Response<Body>, Error> {
+    let bytes = serde_json::to_vec(&rsp)?;
+    Ok(Response::builder()
+        .status(http::StatusCode::OK)
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(Body::from(bytes))
+        .expect("admission review response must be valid"))
+}
+
 fn admit(req: AdmissionRequest, index: &SharedIndex) -> Result<AdmissionResponse> {
     if is_kind::<AuthorizationPolicy>(&req) {
-        return admit_authz_policy(req);
+        return admit_kind::<AuthorizationPolicySpec>(req, index);
     }
 
     if is_kind::<MeshTLSAuthentication>(&req) {
-        return admit_meshtls_authn(req);
+        return admit_kind::<MeshTLSAuthenticationSpec>(req, index);
     }
 
     if is_kind::<NetworkAuthentication>(&req) {
-        return admit_network_authn(req);
+        return admit_kind::<NetworkAuthenticationSpec>(req, index);
     }
 
     if is_kind::<Server>(&req) {
-        return admit_server(req, index);
+        return admit_kind::<ServerSpec>(req, index);
     };
 
     bail!(
@@ -118,127 +131,22 @@ where
     *req.kind.group == *T::group(&()) && *req.kind.kind == *T::kind(&())
 }
 
-// === AuthorizationPolicy ===
-
-fn admit_authz_policy(req: AdmissionRequest) -> Result<AdmissionResponse> {
+fn admit_kind<T: DeserializeOwned + Validate>(
+    req: AdmissionRequest,
+    index: &SharedIndex,
+) -> Result<AdmissionResponse> {
+    let kind = req.kind.kind.clone();
     let rsp = AdmissionResponse::from(&req);
     let (ns, name, spec) =
-        parse_spec(req).map_err(|e| e.context("failed to deserialize AuthorizationPolicy"))?;
-    match validate_authz_policy(spec) {
+        parse_spec::<T>(req).map_err(|e| e.context(format!("failed to deserialize {}", kind)))?;
+    match spec.validate(&ns, &name, &*index.read()) {
         Ok(()) => Ok(rsp),
         Err(error) => {
-            info!(%error, %ns, %name, "denying AuthorizationPolicy");
+            info!(%error, %ns, %name, %kind, "denied");
             Ok(rsp.deny(error))
         }
     }
 }
-
-fn validate_authz_policy(spec: AuthorizationPolicySpec) -> Result<()> {
-    if spec.target_ref.group.as_deref() != Some(&*Server::group(&()))
-        || *spec.target_ref.kind != *Server::kind(&())
-    {
-        bail!("targetRef must be a policy.linkerd.io Server");
-    }
-
-    Ok(())
-}
-
-// === MeshTLSAuthentication ===
-
-fn admit_meshtls_authn(req: AdmissionRequest) -> Result<AdmissionResponse> {
-    let rsp = AdmissionResponse::from(&req);
-    let (ns, name, spec) =
-        parse_spec(req).map_err(|e| e.context("failed to deserialize MeshTLSAuthentication"))?;
-    match validate_meshtls_authn(spec) {
-        Ok(()) => Ok(rsp),
-        Err(error) => {
-            info!(%error, %ns, %name, "denying MeshTLSAuthentication");
-            Ok(rsp.deny(error))
-        }
-    }
-}
-
-fn validate_meshtls_authn(_spec: MeshTLSAuthenticationSpec) -> Result<()> {
-    // TODO parse identity strings
-
-    Ok(())
-}
-
-// === NetworkAuthentication ===
-
-fn admit_network_authn(req: AdmissionRequest) -> Result<AdmissionResponse> {
-    let rsp = AdmissionResponse::from(&req);
-    let (ns, name, spec) =
-        parse_spec(req).map_err(|e| e.context("failed to deserialize NetworkAuthentication"))?;
-    match validate_network_authn(spec) {
-        Ok(()) => Ok(rsp),
-        Err(error) => {
-            info!(%error, %ns, %name, "denying NetworkAuthentication");
-            Ok(rsp.deny(error))
-        }
-    }
-}
-
-fn validate_network_authn(spec: NetworkAuthenticationSpec) -> Result<()> {
-    use std::str::FromStr;
-
-    for net in spec.networks.iter() {
-        let cidr = match ipnet::IpNet::from_str(&*net.cidr) {
-            Ok(cidr) => cidr,
-            Err(e) => bail!(anyhow::Error::new(e).context("invalid 'cidr'")),
-        };
-        for except in net.except.iter().flatten() {
-            let except = match ipnet::IpNet::from_str(&*except) {
-                Ok(except) => except,
-                Err(e) => bail!(anyhow::Error::new(e).context("invalid 'except' network")),
-            };
-            if !cidr.contains(&except) {
-                bail!("cidr '{}' does not include exception '{}'", cidr, except);
-            }
-        }
-    }
-
-    Ok(())
-}
-
-// === Server ===
-
-fn admit_server(req: AdmissionRequest, index: &SharedIndex) -> Result<AdmissionResponse> {
-    let rsp = AdmissionResponse::from(&req);
-    let (ns, name, spec) =
-        parse_spec(req).map_err(|e| e.context("failed to deserialize Server"))?;
-    match validate_server(&ns, &name, spec, &*index.read()) {
-        Ok(()) => Ok(rsp),
-        Err(error) => {
-            info!(%error, %ns, %name, "denying server");
-            Ok(rsp.deny(error))
-        }
-    }
-}
-
-/// Validates a new server (`review`) against existing `servers`.
-fn validate_server(ns: &str, name: &str, spec: ServerSpec, index: &Index) -> Result<()> {
-    if let Some(nsidx) = index.get_ns(ns) {
-        for (srvname, srv) in nsidx.servers.iter() {
-            // If the port and pod selectors select the same resources, fail the admission of the
-            // server. Ignore existing instances of this Server (e.g., if the server's metadata is
-            // changing).
-            if *srvname != name
-                // TODO(ver) this isn't rigorous about detecting servers that select the same port if one port
-                // specifies a numeric port and the other specifies the port's name.
-                && *srv.port() == spec.port
-                // TODO(ver) We can probably detect overlapping selectors more effectively.
-                && *srv.pod_selector() == spec.pod_selector
-            {
-                bail!("identical server spec already exists");
-            }
-        }
-    }
-
-    Ok(())
-}
-
-// === utils ===
 
 fn parse_spec<T: DeserializeOwned>(req: AdmissionRequest) -> Result<(String, String, T)> {
     let obj = req
@@ -262,11 +170,71 @@ fn parse_spec<T: DeserializeOwned>(req: AdmissionRequest) -> Result<(String, Str
     Ok((ns, name, spec))
 }
 
-fn json_response(rsp: AdmissionReview) -> Result<Response<Body>, Error> {
-    let bytes = serde_json::to_vec(&rsp)?;
-    Ok(Response::builder()
-        .status(http::StatusCode::OK)
-        .header(http::header::CONTENT_TYPE, "application/json")
-        .body(Body::from(bytes))
-        .expect("admission review response must be valid"))
+impl Validate for AuthorizationPolicySpec {
+    fn validate(&self, _: &str, _: &str, _: &Index) -> Result<()> {
+        if self.target_ref.group.as_deref() != Some(&*Server::group(&()))
+            || *self.target_ref.kind != *Server::kind(&())
+        {
+            bail!("invalid targetRef kind");
+        }
+
+        Ok(())
+    }
+}
+
+impl Validate for MeshTLSAuthenticationSpec {
+    fn validate(&self, _: &str, _: &str, _: &Index) -> Result<()> {
+        // TODO parse identity strings
+
+        Ok(())
+    }
+}
+
+impl Validate for NetworkAuthenticationSpec {
+    fn validate(&self, _: &str, _: &str, _: &Index) -> Result<()> {
+        use std::str::FromStr;
+
+        for net in self.networks.iter() {
+            let cidr = match ipnet::IpNet::from_str(&*net.cidr) {
+                Ok(cidr) => cidr,
+                Err(e) => bail!(anyhow::Error::new(e).context("invalid 'cidr'")),
+            };
+            for except in net.except.iter().flatten() {
+                let except = match ipnet::IpNet::from_str(&*except) {
+                    Ok(except) => except,
+                    Err(e) => bail!(anyhow::Error::new(e).context("invalid 'except' network")),
+                };
+                if !cidr.contains(&except) {
+                    bail!("cidr '{}' does not include exception '{}'", cidr, except);
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Validate for ServerSpec {
+    /// Validates a new server (`review`) against existing `servers`.
+    fn validate(&self, ns: &str, name: &str, index: &Index) -> Result<()> {
+        if let Some(nsidx) = index.get_ns(ns) {
+            for (srvname, srv) in nsidx.servers.iter() {
+                // If the port and pod selectors select the same resources, fail the admission of
+                // the server. Ignore existing instances of this Server (e.g., if the server's
+                // metadata is changing).
+                if *srvname != name
+                    // TODO(ver) this isn't rigorous about detecting servers that select the same
+                    // port if one port specifies a numeric port and the other specifies the port's
+                    // name.
+                    && *srv.port() == self.port
+                    // TODO(ver) We can probably detect overlapping selectors more effectively.
+                    && *srv.pod_selector() == self.pod_selector
+                {
+                    bail!("identical server spec already exists");
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
