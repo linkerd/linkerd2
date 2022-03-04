5a70b2962c3475626858d62f03c102494180c327
Oliver Gould <ver@buoyant.io>
2022-03-04 03:05:53 +0000

    split admission into smaller fns

--- policy-controller/src/admission.rs
+++ policy-controller/src/admission.rs
@@ -1,11 +1,11 @@
-use crate::api::policy::{AuthorizationPolicySpec, ServerSpec};
+use crate::api::policy::{AuthorizationPolicy, AuthorizationPolicySpec, Server, ServerSpec};
 use anyhow::{anyhow, bail, Result};
 use futures::future;
 use hyper::{body::Buf, http, Body, Request, Response};
 use k8s_openapi::serde::de::DeserializeOwned;
 use kube::{
     core::{DynamicObject, GroupVersionKind},
-    ResourceExt,
+    Resource, ResourceExt,
 };
 use linkerd_policy_controller_k8s_index::{Index, SharedIndex};
 use std::task;
@@ -66,10 +66,10 @@ impl hyper::service::Service<Request<Body>> for AdmissionService {
             let rsp = match review.try_into() {
                 Ok(req) => {
                     debug!(?req);
-                    admit(req, index)
+                    admit(req, &index)
                 }
                 Err(error) => {
-                    warn!(%error, "Invalid admission request");
+                    warn!(%error, "invalid admission request");
                     AdmissionResponse::invalid(error)
                 }
             };
@@ -81,89 +81,78 @@ impl hyper::service::Service<Request<Body>> for AdmissionService {
     }
 }
 
-fn admit(req: AdmissionRequest, index: SharedIndex) -> AdmissionResponse {
+fn admit(req: AdmissionRequest, index: &SharedIndex) -> AdmissionResponse {
     let GroupVersionKind {
         group,
         version,
         kind,
     } = &req.kind;
 
-    if group != "policy.linkerd.io" {
-        warn!(%group, %version, %kind, "unsupported resource type");
-        return AdmissionResponse::invalid(format_args!(
-            "Unsupported resource type: {}.{}.{}",
-            group, version, kind
-        ));
+    if *group == *AuthorizationPolicy::group(&()) && *kind == *AuthorizationPolicy::kind(&()) {
+        return admit_authz_policy(req);
     }
 
+    if *group == *Server::group(&()) && kind == &*Server::kind(&()) {
+        return admit_server(req, index);
+    }
+
+    warn!(%group, %version, %kind, "unsupported resource type");
+    AdmissionResponse::invalid(format_args!(
+        "unsupported resource type: {}.{}.{}",
+        group, version, kind
+    ))
+}
+
+// === AuthorizationPolicy ===
+
+fn admit_authz_policy(req: AdmissionRequest) -> AdmissionResponse {
     let rsp = AdmissionResponse::from(&req);
-    match kind.as_str() {
-        "AuthorizationPolicy" => {
-            let (ns, name, spec) = match parse_spec(req) {
-                Ok(s) => s,
-                Err(error) => {
-                    warn!(%error, "failed to deserialize server from admission request");
-                    return AdmissionResponse::invalid(error);
-                }
-            };
-            match validate_authz_policy(spec) {
-                Ok(()) => rsp,
-                Err(error) => {
-                    info!(%error, %ns, %name, "denying AuthorizationPolicy");
-                    rsp.deny(error)
-                }
-            }
+    let (ns, name, spec) = match parse_spec(req) {
+        Ok(s) => s,
+        Err(error) => {
+            warn!(%error, "failed to deserialize server from admission request");
+            return AdmissionResponse::invalid(error);
         }
-        "Server" => {
-            // Parse the server instance under review before doing anything with the API--i.e., if
-            // this fails we don't have to waste the API calls.
-            let (ns, name, spec) = match parse_spec(req) {
-                Ok(s) => s,
-                Err(error) => {
-                    warn!(%error, "failed to deserialize server from admission request");
-                    return AdmissionResponse::invalid(error);
-                }
-            };
-            match validate_server(&ns, &name, spec, &*index.read()) {
-                Ok(()) => rsp,
-                Err(error) => {
-                    info!(%error, %ns, %name, "denying Server");
-                    rsp.deny(error)
-                }
-            }
-        }
-        _ => {
-            warn!(%group, %version, %kind, "Unsupported resource type");
-            AdmissionResponse::invalid(format_args!(
-                "Unsupported resource type: {}.{}.{}",
-                group, version, kind
-            ))
+    };
+
+    match validate_authz_policy(spec) {
+        Ok(()) => rsp,
+        Err(error) => {
+            info!(%error, %ns, %name, "denying AuthorizationPolicy");
+            rsp.deny(error)
         }
     }
 }
 
-fn json_response(rsp: AdmissionReview) -> Result<Response<Body>, Error> {
-    let bytes = serde_json::to_vec(&rsp)?;
-    Ok(Response::builder()
-        .status(http::StatusCode::OK)
-        .header(http::header::CONTENT_TYPE, "application/json")
-        .body(Body::from(bytes))
-        .expect("admission review response must be valid"))
+fn validate_authz_policy(spec: AuthorizationPolicySpec) -> Result<()> {
+    if spec.target_ref.group.as_deref() != Some(&*Server::group(&()))
+        || *spec.target_ref.kind != *Server::kind(&())
+    {
+        bail!("targetRef must be a policy.linkerd.io Server");
+    }
+
+    Ok(())
 }
 
-fn parse_spec<T: DeserializeOwned>(req: AdmissionRequest) -> Result<(String, String, T)> {
-    let obj = req.object.ok_or_else(|| anyhow!("missing server"))?;
-    let ns = obj
-        .namespace()
-        .ok_or_else(|| anyhow!("no 'namespace' field set on server"))?;
-    let name = obj.name();
-    let data = obj
-        .data
-        .get("spec")
-        .cloned()
-        .ok_or_else(|| anyhow!("no 'spec' field set on server"))?;
-    let spec = serde_json::from_value(data)?;
-    Ok((ns, name, spec))
+// === Server ===
+
+fn admit_server(req: AdmissionRequest, index: &SharedIndex) -> AdmissionResponse {
+    let rsp = AdmissionResponse::from(&req);
+    let (ns, name, spec) = match parse_spec(req) {
+        Ok(s) => s,
+        Err(error) => {
+            warn!(%error, "failed to deserialize server from admission request");
+            return AdmissionResponse::invalid(error);
+        }
+    };
+
+    match validate_server(&ns, &name, spec, &*index.read()) {
+        Ok(()) => rsp,
+        Err(error) => {
+            info!(%error, %ns, %name, "denying server");
+            rsp.deny(error)
+        }
+    }
 }
 
 /// Validates a new server (`review`) against existing `servers`.
@@ -188,13 +177,28 @@ fn validate_server(ns: &str, name: &str, spec: ServerSpec, index: &Index) -> Res
     Ok(())
 }
 
-/// Validates a new server (`review`) against existing `servers`.
-fn validate_authz_policy(spec: AuthorizationPolicySpec) -> Result<()> {
-    if spec.target_ref.group.as_deref() != Some("policy.linkerd.io")
-        || spec.target_ref.kind != "Server"
-    {
-        bail!("invalid authorization policy target kind");
-    }
+// === utils ===
 
-    Ok(())
+fn parse_spec<T: DeserializeOwned>(req: AdmissionRequest) -> Result<(String, String, T)> {
+    let obj = req.object.ok_or_else(|| anyhow!("missing server"))?;
+    let ns = obj
+        .namespace()
+        .ok_or_else(|| anyhow!("no 'namespace' field set on server"))?;
+    let name = obj.name();
+    let data = obj
+        .data
+        .get("spec")
+        .cloned()
+        .ok_or_else(|| anyhow!("no 'spec' field set on server"))?;
+    let spec = serde_json::from_value(data)?;
+    Ok((ns, name, spec))
+}
+
+fn json_response(rsp: AdmissionReview) -> Result<Response<Body>, Error> {
+    let bytes = serde_json::to_vec(&rsp)?;
+    Ok(Response::builder()
+        .status(http::StatusCode::OK)
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(Body::from(bytes))
+        .expect("admission review response must be valid"))
 }
