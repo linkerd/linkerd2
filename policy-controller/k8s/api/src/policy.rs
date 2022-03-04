14cb663c3f299dbcd1dd15a7ec9f0f4011ce2537
Oliver Gould <ver@buoyant.io>
2022-03-04 15:38:32 +0000

    Generalize TargetRef to include an optional namespace
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- policy-controller/k8s/api/src/policy.rs
+++ policy-controller/k8s/api/src/policy.rs
@@ -18,9 +18,11 @@ pub struct TargetRef {
     pub group: Option<String>,
     pub kind: String,
     pub name: Option<String>,
+    pub namespace: Option<String>,
 }
 
 impl TargetRef {
+    /// Checks whether the target references the given resource type
     pub fn targets_kind<T>(&self) -> bool
     where
         T: kube::Resource,
@@ -28,5 +30,66 @@ impl TargetRef {
     {
         let dt = Default::default();
         self.group.as_deref() == Some(&*T::group(&dt)) && *self.kind == *T::kind(&dt)
+    }
+
+    /// Checks whether the target references the given namespaced resource
+    pub fn targets_resource<T>(&self, resource: T, default_ns: &str) -> bool
+    where
+        T: kube::Resource,
+        T::DynamicType: Default,
+    {
+        use kube::ResourceExt;
+
+        if !self.targets_kind::<T>() {
+            return false;
+        }
+
+        if let Some(rns) = resource.namespace() {
+            if rns != self.namespace.as_deref().unwrap_or(default_ns) {
+                // If the resource specifies a namespace other than the target or the default
+                // namespace, that's a deal-breaker.
+                return false;
+            }
+        } else if let Some(ns) = self.namespace.as_deref() {
+            if ns != default_ns {
+                // If the resource does not specify a namespace but the target specifies a resource
+                // other than the local default, that's a deal-breaker.
+                return false;
+            }
+        }
+
+        if let Some(name) = self.name.as_deref() {
+            if name != resource.name() {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    /// Checks whether the target references the given cluster-level resource
+    pub fn targets_cluster_resource<T>(&self, resource: T) -> bool
+    where
+        T: kube::Resource,
+        T::DynamicType: Default,
+    {
+        use kube::ResourceExt;
+
+        if !self.targets_kind::<T>() {
+            return false;
+        }
+
+        if self.namespace.is_some() || resource.namespace().is_some() {
+            // If the reference or the resource has a namespace, that's a deal-breaker.
+            return false;
+        }
+
+        if let Some(name) = self.name.as_deref() {
+            if name != resource.name() {
+                return false;
+            }
+        }
+
+        true
     }
 }
--- policy-controller/k8s/api/src/policy/authorization_policy.rs
+++ policy-controller/k8s/api/src/policy/authorization_policy.rs
@@ -18,13 +18,6 @@ use super::TargetRef;
 #[serde(rename_all = "camelCase")]
 pub struct AuthorizationPolicySpec {
     pub target_ref: TargetRef,
-    pub required_authentication_refs: Vec<RequiredAuthenticationRef>,
-}
 
-#[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize, schemars::JsonSchema)]
-pub struct RequiredAuthenticationRef {
-    #[serde(flatten)]
-    pub target_ref: TargetRef,
-
-    pub namespace: Option<String>,
+    pub required_authentication_refs: Vec<TargetRef>,
 }
