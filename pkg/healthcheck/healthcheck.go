d185301f80b8f37d8414e6719ad036f8ab16d276
Kevin Leimkuhler <kleimkuhler@icloud.com>
2022-03-09 09:27:43 -0700

    cli: skip pods that have `NodeShutdown` status (#8032)
    Closes #8010.
    
    Pods that have `NodeShutdown` status should be skipped during validation as they will not have a running proxy container.
    
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -2809,9 +2809,10 @@ func CheckPodsRunning(pods []corev1.Pod, namespace string) error {
 	for _, pod := range pods {
 		status := k8s.GetPodStatus(pod)
 
-		// Skip validating meshed pods that are in the `Completed` or
-		// `Shutdown` state as they do not have a running proxy
-		if status == "Completed" || status == "Shutdown" {
+		// Skip validating pods that have a status which indicates there would
+		// be no running proxy container.
+		switch status {
+		case "Completed", "NodeShutdown", "Shutdown":
 			continue
 		} else if status != string(corev1.PodRunning) && status != "Evicted" {
 			return fmt.Errorf("pod \"%s\" status is %s", pod.Name, pod.Status.Phase)
