acf65c41646e867254cef6d0344340ab695c409e
Kevin Leimkuhler <kleimkuhler@icloud.com>
2022-03-26 01:08:54 +0000

    Run top command
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- test/integration/viz/serviceprofiles/serviceprofiles_test.go
+++ test/integration/viz/serviceprofiles/serviceprofiles_test.go
@@ -188,9 +188,9 @@ func testMetrics(t *testing.T) {
 		cmd = []string{"viz", "stat", "--namespace", testNamespace, testDownstreamDeploy}
 		out, _ = TestHelper.LinkerdRun(cmd...)
 		t.Logf("stat %s: %s", testDownstreamDeploy, out)
-		// cmd = []string{"viz", "top", "--namespace", testNamespace, testDownstreamDeploy}
-		// out, _ = TestHelper.LinkerdRun(cmd...)
-		// t.Logf("top: %s", out)
+		cmd = []string{"viz", "top", "--namespace", testNamespace, testDownstreamDeploy}
+		out, _ = TestHelper.LinkerdRun(cmd...)
+		t.Logf("top: %s", out)
 		if !(*stat.ActualSuccess > 0.00 && *stat.ActualSuccess < 100.00) {
 			return fmt.Errorf("expected Actual Success to be greater than 0%% and less than 100%% due to pre-seeded failure rate. But got %0.2f", *stat.ActualSuccess)
 		}
