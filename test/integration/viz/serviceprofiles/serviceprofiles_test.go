5856ec50959be4ff7c0c0c62d49de543c64edb97
Oliver Gould <ver@buoyant.io>
2022-03-28 19:58:59 +0000

    more testing; revert proxy bump
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- test/integration/viz/serviceprofiles/serviceprofiles_test.go
+++ test/integration/viz/serviceprofiles/serviceprofiles_test.go
@@ -180,7 +180,7 @@ func testMetrics(t *testing.T) {
 	}
 
 	assertRouteStat(testUpstreamDeploy, testNamespace, testDownstreamDeploy, t, func(stat *cmd2.JSONRouteStats) error {
-		fmt.Printf("Output: %+v\n", stat)
+		t.Logf("Output: %+v", stat)
 		if !(*stat.ActualSuccess > 0.00 && *stat.ActualSuccess < 100.00) {
 			return fmt.Errorf("expected Actual Success to be greater than 0%% and less than 100%% due to pre-seeded failure rate. But got %0.2f", *stat.ActualSuccess)
 		}
