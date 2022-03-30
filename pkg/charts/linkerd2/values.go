550724de57cebdecec845c44000abdbefb5df52d
Michal Romanowski <michal.rom089@gmail.com>
2022-03-30 10:11:14 +0200

    Convert 4 spaces to tabs
    Signed-off-by: Michal Romanowski <michal.rom089@gmail.com>

--- pkg/charts/linkerd2/values.go
+++ pkg/charts/linkerd2/values.go
@@ -31,7 +31,7 @@ type (
 		ControllerUID                int64               `json:"controllerUID"`
 		EnableH2Upgrade              bool                `json:"enableH2Upgrade"`
 		EnablePodAntiAffinity        bool                `json:"enablePodAntiAffinity"`
-	    NodeAffinity                 map[string]string   `json:"nodeAffinity"`
+		NodeAffinity                 map[string]string   `json:"nodeAffinity"`
 		WebhookFailurePolicy         string              `json:"webhookFailurePolicy"`
 		DisableHeartBeat             bool                `json:"disableHeartBeat"`
 		HeartbeatSchedule            string              `json:"heartbeatSchedule"`
