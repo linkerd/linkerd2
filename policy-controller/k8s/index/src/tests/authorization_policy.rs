fb44badf33681cede186140f51450ed660d7f736
Alex Leong <alex@buoyant.io>
2022-05-03 13:12:27 -0700

    Allow AuthorizationPolicy to target namespace (#8314)
    There is no convenient way to create an `AuthorizationPolicy` that targets all
    `Server` resources in a `Namespace`. A distinct policy resource must be created
    for each `Server`. This is cumbersome.
    
    This change updates the policy controller to allow `AuthorizationPolicy`
    resources to target `Namespace` resources. Policies may only target the
    namespace in which the policy resource exists. The admission controller rejects
    resources created that targets other namespaces.
    
    Fixes #8297

--- policy-controller/k8s/index/src/tests/authorization_policy.rs
+++ policy-controller/k8s/index/src/tests/authorization_policy.rs
@@ -43,7 +43,95 @@ fn links_authorization_policy_with_mtls_name() {
     test.index.write().apply(mk_authorization_policy(
         "ns-0",
         "authz-foo",
+        Some("srv-8080"),
+        vec![
+            NamespacedTargetRef {
+                group: Some("policy.linkerd.io".to_string()),
+                kind: "NetworkAuthentication".to_string(),
+                name: "net-foo".to_string(),
+                namespace: None,
+            },
+            NamespacedTargetRef {
+                group: Some("policy.linkerd.io".to_string()),
+                kind: "MeshTLSAuthentication".to_string(),
+                namespace: Some("ns-1".to_string()),
+                name: "mtls-bar".to_string(),
+            },
+        ],
+    ));
+    test.index.write().apply(mk_network_authentication(
+        "ns-0".to_string(),
+        "net-foo".to_string(),
+        vec![k8s::policy::network_authentication::Network {
+            cidr: "10.0.0.0/8".parse().unwrap(),
+            except: None,
+        }],
+    ));
+    test.index.write().apply(mk_meshtls_authentication(
+        "ns-1",
+        "mtls-bar",
+        Some("foo.bar".to_string()),
+        None,
+    ));
+    assert!(rx.has_changed().unwrap());
+    assert_eq!(
+        *rx.borrow(),
+        InboundServer {
+            reference: ServerRef::Server("srv-8080".to_string()),
+            authorizations: hashmap!(
+                AuthorizationRef::AuthorizationPolicy("authz-foo".to_string()) => authz
+            )
+            .into_iter()
+            .collect(),
+            protocol: ProxyProtocol::Http1,
+        },
+    );
+}
+
+#[test]
+fn authorization_targets_namespace() {
+    let test = TestConfig::default();
+
+    let mut pod = mk_pod("ns-0", "pod-0", Some(("container-0", None)));
+    pod.labels_mut()
+        .insert("app".to_string(), "app-0".to_string());
+    test.index.write().apply(pod);
+
+    let mut rx = test
+        .index
+        .write()
+        .pod_server_rx("ns-0", "pod-0", 8080)
+        .expect("pod-0.ns-0 should exist");
+    assert_eq!(*rx.borrow_and_update(), test.default_server());
+
+    test.index.write().apply(mk_server(
+        "ns-0",
         "srv-8080",
+        Port::Number(8080),
+        None,
+        Some(("app", "app-0")),
+        Some(k8s::policy::server::ProxyProtocol::Http1),
+    ));
+    assert!(rx.has_changed().unwrap());
+    assert_eq!(
+        *rx.borrow_and_update(),
+        InboundServer {
+            reference: ServerRef::Server("srv-8080".to_string()),
+            authorizations: Default::default(),
+            protocol: ProxyProtocol::Http1,
+        },
+    );
+
+    let authz = ClientAuthorization {
+        networks: vec!["10.0.0.0/8".parse::<IpNet>().unwrap().into()],
+        authentication: ClientAuthentication::TlsAuthenticated(vec![IdentityMatch::Exact(
+            "foo.bar".to_string(),
+        )]),
+    };
+    test.index.write().apply(mk_authorization_policy(
+        "ns-0",
+        "authz-foo",
+        Option::<&str>::None,
         vec![
             NamespacedTargetRef {
                 group: Some("policy.linkerd.io".to_string()),
@@ -91,7 +179,7 @@ fn links_authorization_policy_with_mtls_name() {
 fn mk_authorization_policy(
     ns: impl ToString,
     name: impl ToString,
-    server: impl ToString,
+    server: Option<impl ToString>,
     authns: impl IntoIterator<Item = NamespacedTargetRef>,
 ) -> k8s::policy::AuthorizationPolicy {
     k8s::policy::AuthorizationPolicy {
@@ -101,10 +189,17 @@ fn mk_authorization_policy(
             ..Default::default()
         },
         spec: k8s::policy::AuthorizationPolicySpec {
-            target_ref: LocalTargetRef {
-                group: Some("policy.linkerd.io".to_string()),
-                kind: "Server".to_string(),
-                name: server.to_string(),
+            target_ref: match server {
+                Some(server) => LocalTargetRef {
+                    group: Some("policy.linkerd.io".to_string()),
+                    kind: "Server".to_string(),
+                    name: server.to_string(),
+                },
+                None => LocalTargetRef {
+                    group: Some("core".to_string()),
+                    kind: "Namespace".to_string(),
+                    name: ns.to_string(),
+                },
             },
             required_authentication_refs: authns.into_iter().collect(),
         },
