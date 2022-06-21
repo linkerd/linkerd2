917bde68b66b1d8fd1ff652ff210622c7b6e26b6
Jacob Henner <code@ventricle.us>
2022-06-21 18:43:19 -0400

    Remove kube-system exclusions from watchers
    Watch events for objects in the kube-system namespace were previously
    ignored. In certain situations, this would cause the destination service
    to return invalid (outdated) endpoints for services in kube-system -
    including unmeshed services.
    
    It was suggested [1] that kube-system events were ignored to avoid
    handling frequent Endpoint updates - specifically from controllers using
    Endpoints for leader elections [2]. As of Kubernetes 1.20, these
    controllers default to using Leases instead of Endpoints for their
    leader elections [3], obviating the need to exclude (or filter) updates
    from kube-system. The exclusions have been removed accordingly.
    
    [1]: https://github.com/linkerd/linkerd2/pull/4133#issuecomment-594983588
    [2]: https://github.com/kubernetes/kubernetes/issues/86286
    [3]: https://github.com/kubernetes/kubernetes/pull/94603
    
    Signed-off-by: Jacob Henner <code@ventricle.us>

--- multicluster/service-mirror/cluster_watcher.go
+++ multicluster/service-mirror/cluster_watcher.go
@@ -28,7 +28,6 @@ import (
 
 const (
 	eventTypeSkipped = "ServiceMirroringSkipped"
-	kubeSystem       = "kube-system"
 )
 
 type (
@@ -812,10 +811,6 @@ func (rcsw *RemoteClusterServiceWatcher) Start(ctx context.Context) error {
 		cache.ResourceEventHandlerFuncs{
 			// AddFunc only relevant for exported headless endpoints
 			AddFunc: func(obj interface{}) {
-				if obj.(metav1.Object).GetNamespace() == kubeSystem {
-					return
-				}
-
 				ep, ok := obj.(*corev1.Endpoints)
 				if !ok {
 					rcsw.log.Errorf("error processing endpoints object: got %#v, expected *corev1.Endpoints", ep)
@@ -835,10 +830,6 @@ func (rcsw *RemoteClusterServiceWatcher) Start(ctx context.Context) error {
 			},
 			// AddFunc relevant for all kind of exported endpoints
 			UpdateFunc: func(old, new interface{}) {
-				if new.(metav1.Object).GetNamespace() == kubeSystem {
-					return
-				}
-
 				epOld, ok := old.(*corev1.Endpoints)
 				if !ok {
 					rcsw.log.Errorf("error processing endpoints object: got %#v, expected *corev1.Endpoints", epOld)
@@ -864,10 +855,6 @@ func (rcsw *RemoteClusterServiceWatcher) Start(ctx context.Context) error {
 	rcsw.localAPIClient.NS().Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				if obj.(metav1.Object).GetName() == kubeSystem {
-					return
-				}
-
 				rcsw.eventsQueue.Add(&OnLocalNamespaceAdded{obj.(*corev1.Namespace)})
 			},
 		},
