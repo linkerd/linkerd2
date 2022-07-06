2a760d824a7bec450da59a33d86886923ad8beb0
Krzysztof Dryś <krzysztofdrys@gmail.com>
2022-07-06 15:41:02 +0200

    logging

--- bin/_test-helpers.sh
+++ bin/_test-helpers.sh
@@ -323,6 +323,7 @@ external_image_load() {
   if [[ "$test_name" = deep ]]; then
     images_load+=("buoyantio/booksapp:v0.0.5" "buoyantio/booksapp-traffic:v0.0.3" "cr.l5d.io/linkerd/debug:edge-20.9.2" "nginx:alpine" "buoyantio/slow_cooker:1.3.0" )
   fi
+  echo "Preloading images ${images_load[@]} to cluster $cluster_name"
   "$bindir"/image-load --k3d --cluster "$cluster_name" --preload "${images_load[@]}"
   exit_on_err "error calling '$bindir/image-load'"
   esac
