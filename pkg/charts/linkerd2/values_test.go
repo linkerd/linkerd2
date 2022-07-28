9dd51d3897057173242e19065e36e19a16cb09b9
Matei David <matei@buoyant.io>
2022-07-28 07:45:19 +0300

    Add `iptablesMode` flag to proxy-init (#8887)
    This change introduces a new value to be used at install (or upgrade)
    time. The value (`proxyInit.iptablesMode=nft|legacy`) is responsible
    for starting the proxy-init container in nft or legacy mode.
    
    By default, the init container will use iptables-nft. When the mode is set to
    `nft`, it will instead use iptables-nft. Most modern Linux distributions
    support both, but a subset (such as RHEL based families) only support
    iptables-nft and nf_tables.
    
    Signed-off-by: Matei David <matei@buoyant.io>

--- pkg/charts/linkerd2/values_test.go
+++ pkg/charts/linkerd2/values_test.go
@@ -116,6 +116,7 @@ func TestNewValues(t *testing.T) {
 			Await:                  true,
 		},
 		ProxyInit: &ProxyInit{
+			IptablesMode:        "nft",
 			IgnoreInboundPorts:  "4567,4568",
 			IgnoreOutboundPorts: "4567,4568",
 			LogLevel:            "",
