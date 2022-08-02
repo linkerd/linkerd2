f435767ce94658159582b105cd079d6d803cb868
Alex Leong <alex@buoyant.io>
2022-08-02 02:25:15 +0000

    Add support for routes and authorization policies to viz authz command
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- viz/cmd/authz.go
+++ viz/cmd/authz.go
@@ -73,8 +73,10 @@ func NewCmdAuthz() *cobra.Command {
 			}
 
 			cols := []table.Column{
+				table.NewColumn("ROUTE").WithLeftAlign(),
 				table.NewColumn("SERVER").WithLeftAlign(),
-				table.NewColumn("AUTHZ").WithLeftAlign(),
+				table.NewColumn("AUTHORIZATION_POLICY").WithLeftAlign(),
+				table.NewColumn("SERVER_AUTHORIZATION").WithLeftAlign(),
 				table.NewColumn("SUCCESS"),
 				table.NewColumn("RPS"),
 				table.NewColumn("LATENCY_P50"),
@@ -89,61 +91,6 @@ func NewCmdAuthz() *cobra.Command {
 				os.Exit(1)
 			}
 			for _, server := range servers {
-				sazs, err := k8s.ServerAuthorizationsForServer(cmd.Context(), k8sAPI, options.namespace, server)
-				if err != nil {
-					fmt.Fprint(os.Stderr, err.Error())
-					os.Exit(1)
-				}
-				for _, saz := range sazs {
-					requestParams := util.StatsSummaryRequestParams{
-						StatsBaseRequestParams: util.StatsBaseRequestParams{
-							TimeWindow:    options.timeWindow,
-							ResourceName:  saz,
-							ResourceType:  k8s.ServerAuthorization,
-							Namespace:     options.namespace,
-							AllNamespaces: false,
-						},
-						ToNamespace: options.namespace,
-					}
-					requestParams.ToName = server
-					requestParams.ToType = k8s.Server
-
-					req, err := util.BuildStatSummaryRequest(requestParams)
-					if err != nil {
-						return err
-					}
-					resp, err := requestStatsFromAPI(client, req)
-					if err != nil {
-						fmt.Fprint(os.Stderr, err.Error())
-						os.Exit(1)
-					}
-
-					for _, row := range respToRows(resp) {
-						if row.Stats == nil {
-							rows = append(rows, table.Row{
-								server,
-								saz,
-								"-",
-								"-",
-								"-",
-								"-",
-								"-",
-							})
-						} else {
-							rows = append(rows, table.Row{
-								server,
-								saz,
-								fmt.Sprintf("%.2f%%", getSuccessRate(row.Stats.GetSuccessCount(), row.Stats.GetFailureCount())*100),
-								fmt.Sprintf("%.1frps", getRequestRate(row.Stats.GetSuccessCount(), row.Stats.GetFailureCount(), row.TimeWindow)),
-								fmt.Sprintf("%dms", row.Stats.LatencyMsP50),
-								fmt.Sprintf("%dms", row.Stats.LatencyMsP95),
-								fmt.Sprintf("%dms", row.Stats.LatencyMsP99),
-							})
-						}
-					}
-				}
-
-				// Unauthorized
 				requestParams := util.StatsSummaryRequestParams{
 					StatsBaseRequestParams: util.StatsBaseRequestParams{
 						TimeWindow:    options.timeWindow,
@@ -152,8 +99,6 @@ func NewCmdAuthz() *cobra.Command {
 						Namespace:     options.namespace,
 						AllNamespaces: false,
 					},
-					ToNamespace:   options.namespace,
-					LabelSelector: options.labelSelector,
 				}
 
 				req, err := util.BuildStatSummaryRequest(requestParams)
@@ -165,10 +110,66 @@ func NewCmdAuthz() *cobra.Command {
 					fmt.Fprint(os.Stderr, err.Error())
 					os.Exit(1)
 				}
+
 				for _, row := range respToRows(resp) {
+					var authzp, saz, route string
+					if row.GetSrvStats().GetAuthz() != nil {
+						if row.GetSrvStats().GetAuthz().Type == "authorizationpolicy" {
+							authzp = row.GetSrvStats().GetAuthz().Name
+						} else if row.GetSrvStats().GetAuthz().Type == "serverauthorization" {
+							saz = row.GetSrvStats().GetAuthz().Name
+						}
+					}
+					if row.GetSrvStats().GetRoute() != nil {
+						route = row.GetSrvStats().GetRoute().Name
+					}
+					if row.Stats != nil {
+						rows = append(rows, table.Row{
+							route,
+							server,
+							authzp,
+							saz,
+							fmt.Sprintf("%.2f%%", getSuccessRate(row.Stats.GetSuccessCount(), row.Stats.GetFailureCount())*100),
+							fmt.Sprintf("%.1frps", getRequestRate(row.Stats.GetSuccessCount(), row.Stats.GetFailureCount(), row.TimeWindow)),
+							fmt.Sprintf("%dms", row.Stats.LatencyMsP50),
+							fmt.Sprintf("%dms", row.Stats.LatencyMsP95),
+							fmt.Sprintf("%dms", row.Stats.LatencyMsP99),
+						})
+					}
+				}
+
+				// Unauthorized
+				requestParams = util.StatsSummaryRequestParams{
+					StatsBaseRequestParams: util.StatsBaseRequestParams{
+						TimeWindow:    options.timeWindow,
+						ResourceName:  server,
+						ResourceType:  k8s.Server,
+						Namespace:     options.namespace,
+						AllNamespaces: false,
+					},
+					ToNamespace:   options.namespace,
+					LabelSelector: options.labelSelector,
+				}
+
+				req, err = util.BuildStatSummaryRequest(requestParams)
+				if err != nil {
+					return err
+				}
+				resp, err = requestStatsFromAPI(client, req)
+				if err != nil {
+					fmt.Fprint(os.Stderr, err.Error())
+					os.Exit(1)
+				}
+				for _, row := range respToRows(resp) {
+					var route string
+					if row.GetSrvStats().GetRoute() != nil {
+						route = row.GetSrvStats().GetRoute().Name
+					}
 					if row.SrvStats != nil && row.SrvStats.DeniedCount > 0 {
 						rows = append(rows, table.Row{
+							route,
 							server,
+							"[UNAUTHORIZED]",
 							"[UNAUTHORIZED]",
 							"-",
 							fmt.Sprintf("%.1frps", getRequestRate(row.SrvStats.DeniedCount, 0, row.TimeWindow)),
