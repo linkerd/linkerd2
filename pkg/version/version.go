63aecdec0e8a13c44eb7be0cc30c48f162f9b50f
Oliver Gould <ver@buoyant.io>
2022-08-16 17:30:47 +0000

    fixup comment
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -13,9 +13,8 @@ import (
 var Version = undefinedVersion
 
 // ProxyInitVersion is the pinned version of the proxy-init, from
-// https://github.com/linkerd/linkerd2-proxy-init
-// This has to be kept in sync with the constraint version for
-// github.com/linkerd/linkerd2-proxy-init in /go.mod
+// https://github.com/linkerd/linkerd2-proxy-init This has to be kept in sync
+// with the default version in the control plane's values.yaml.
 var ProxyInitVersion = "v2.0.0"
 
 const (
