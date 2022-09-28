f9e03fb1b3dd27fd279aa9670c489b127dbb438d
Alex Leong <alex@buoyant.io>
2022-09-28 22:47:42 +0000

    Add diagnostics policy command
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- cli/cmd/diagnostics.go
+++ cli/cmd/diagnostics.go
@@ -29,6 +29,7 @@ This command provides subcommands to diagnose the functionality of Linkerd.`,
 	diagnosticsCmd.AddCommand(newCmdControllerMetrics())
 	diagnosticsCmd.AddCommand(newCmdEndpoints())
 	diagnosticsCmd.AddCommand(newCmdMetrics())
+	diagnosticsCmd.AddCommand(newCmdPolicy())
 
 	return diagnosticsCmd
 }
