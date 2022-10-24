2f71019261b8a2ce1184101235e9fd1dca985b89
Alejandro Pedraza <alejandro@buoyant.io>
2022-10-24 09:33:39 -0500

    Fix "cluster networks contains all services" fails with services with no ClusterIP (#9662)
    Fixes #9661
    
    This excludes any service with no ClusterIP from this check, which
    includes the services of type ExternalName.

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -1946,7 +1946,8 @@ func (hc *HealthChecker) checkClusterNetworksContainAllServices(ctx context.Cont
 		return err
 	}
 	for _, svc := range svcs.Items {
-		if svc.Spec.ClusterIP != "None" && !clusterNetworksContainIP(clusterIPNets, svc.Spec.ClusterIP) {
+		clusterIP := svc.Spec.ClusterIP
+		if clusterIP != "" && clusterIP != "None" && !clusterNetworksContainIP(clusterIPNets, svc.Spec.ClusterIP) {
 			return fmt.Errorf("the Linkerd clusterNetworks [%q] do not include svc %s/%s (%s)", hc.linkerdConfig.ClusterNetworks, svc.Namespace, svc.Name, svc.Spec.ClusterIP)
 		}
 	}
