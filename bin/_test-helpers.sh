ff9b133c7cd56c8cf7a1892574fabae969ba468d
Alejandro Pedraza <alejandro@buoyant.io>
2023-01-11 10:36:13 -0500

    Revert "Temporary changes to trigger tests in k8s 1.21"
    This reverts commit 3e1fdd0e5e8d888a16c519bb04c045cb37352f81.

--- bin/_test-helpers.sh
+++ bin/_test-helpers.sh
@@ -244,7 +244,7 @@ setup_cluster() {
 
   test_setup
   if [ -z "$skip_cluster_create" ]; then
-    "$bindir"/k3d cluster create --image='+v1.21' "$@"
+    "$bindir"/k3d cluster create "$@"
     image_load "$name"
   fi
   check_cluster
