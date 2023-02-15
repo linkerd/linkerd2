8fee890c00416d0fb727b9a0b5bf232bc8cdfc9e
Kevin Leimkuhler <kleimkuhler@icloud.com>
2023-02-15 15:58:07 +0000

    Fix comments
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- policy-controller/k8s/status/src/http_route.rs
+++ policy-controller/k8s/status/src/http_route.rs
@@ -5,9 +5,9 @@ use linkerd_policy_controller_k8s_api::{
     policy::{self, Server},
 };
 
-/// ParentReference represents an HTTPRoute's parent reference from its spec.
+/// Represents an HTTPRoute's parent reference from its spec.
 ///
-/// This is separate from the policy controller index's InboundParentRef
+/// This is separate from the policy controller index's `InboundParentRef`
 /// because it does not validate that the parent reference is not in another
 /// namespace. This is something that should be relaxed in the future in the
 /// policy controller's index and we could then consider consolidating these
