77491bc3be000eb4040ba53c1358eb762e48f50d
Alex Leong <alex@buoyant.io>
2023-03-16 00:45:19 +0000

    Add comments
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- policy-controller/src/index_list.rs
+++ policy-controller/src/index_list.rs
@@ -3,6 +3,9 @@ use std::sync::Arc;
 use kubert::index::IndexNamespacedResource;
 use parking_lot::RwLock;
 
+/// IndexList represents a list of indexes for a specific resource type.
+/// IndexList itself can then act as an index for that resource and fans updates
+/// out to each index in the list by cloning the update.
 pub struct IndexList<A, T> {
     index: Arc<RwLock<A>>,
     tail: Option<T>,
@@ -30,6 +33,10 @@ where
 }
 
 impl<A, T> IndexList<A, T> {
+    // The second type parameter in the return value here can be anything that
+    // implements IndexNamespacedResource<R> since it will just be None. Ideally
+    // the type should be ! (bottom) but A is conveniently available so we use
+    // that.
     pub fn new(index: Arc<RwLock<A>>) -> IndexList<A, A> {
         IndexList { index, tail: None }
     }
@@ -46,6 +53,9 @@ impl<A, T> IndexList<A, T> {
     }
 }
 
+// This constructor is more ergonomic to use than IndexList::<A, A>::new because
+// the compiler is more easily able to infer A and doesn't require callers to
+// fill in explicit type parameters.
 pub fn new<A>(index: Arc<RwLock<A>>) -> IndexList<A, A> {
     IndexList::<A, A>::new(index)
 }
