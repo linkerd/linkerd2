2b6760c82491b83e6321c6b60714d2e3addf309c
Matei David <matei@buoyant.io>
2023-03-29 12:31:33 +0100

    Add status support for BackendReferences (#10601)
    A route may have two conditions in a parent status: a condition that
    states whether it has been accepted by the parents, and a condition that
    states whether all backend references -- that traffic matched against
    route is sent to -- have resolved successfully. Currently, the policy
    controller does not support the latter.
    
    This change introduces support for checking and setting a backendRef
    specific condition. A successful condition (ResolvedRefs = True) is met
    when all backend references point to a supported type, and that type
    exists in the cluster. Currently, only Service objects are supported. A
    nonexistent object, or an unsupported kind will reject the entire
    condition; the particular reason will be reflected in the condition's
    message.
    
    Since statuses are set on a route's parents, the same condition will
    apply to _all_ parents in a route (since there is no way to elicit
    different backends for different parents).
    
    If a route does not have any backend references, then the parent
    reference type will be used. As such, any parents that are not Services
    will automatically get an invalid backend condition (exception to the
    rule in the third paragraph where a condition is shared by all parents).
    When the parent is supported (i.e a Service) we needn't check its
    existence since the parent condition will already reflect that.
    
    ---
    
    Signed-off-by: Matei David <matei@buoyant.io>
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>
    Co-authored-by: Oliver Gould <ver@buoyant.io>

--- policy-controller/k8s/status/src/http_route.rs
+++ policy-controller/k8s/status/src/http_route.rs
@@ -19,39 +19,224 @@ pub enum ParentReference {
     UnknownKind,
 }
 
-pub(crate) fn make_parents(http_route: policy::HttpRoute) -> Vec<ParentReference> {
+#[derive(Clone, Eq, PartialEq)]
+pub enum BackendReference {
+    Service(ResourceId),
+    Unknown,
+}
+
+pub(crate) fn make_parents(http_route: &policy::HttpRoute) -> Vec<ParentReference> {
     let namespace = http_route
         .metadata
         .namespace
+        .as_deref()
         .expect("HTTPRoute must have a namespace");
     http_route
         .spec
         .inner
         .parent_refs
-        .into_iter()
+        .iter()
         .flatten()
-        .map(|parent_ref| ParentReference::from_parent_ref(parent_ref, &namespace))
+        .map(|pr| ParentReference::from_parent_ref(pr, namespace))
+        .collect()
+}
+
+pub(crate) fn make_backends(http_route: &policy::HttpRoute) -> Vec<BackendReference> {
+    let namespace = http_route
+        .metadata
+        .namespace
+        .as_deref()
+        .expect("HTTPRoute must have a namespace");
+    http_route
+        .spec
+        .rules
+        .iter()
+        .flatten()
+        .flat_map(|rule| rule.backend_refs.iter().flatten())
+        .filter_map(|http_backend_ref| http_backend_ref.backend_ref.as_ref())
+        .map(|br| BackendReference::from_backend_ref(&br.inner, namespace))
         .collect()
 }
 
 impl ParentReference {
-    fn from_parent_ref(parent_ref: gateway::ParentReference, default_namespace: &str) -> Self {
-        if policy::httproute::parent_ref_targets_kind::<Server>(&parent_ref) {
+    fn from_parent_ref(parent_ref: &gateway::ParentReference, default_namespace: &str) -> Self {
+        if policy::httproute::parent_ref_targets_kind::<Server>(parent_ref) {
             // If the parent reference does not have a namespace, default to using
             // the HTTPRoute's namespace.
-            let namespace = parent_ref
-                .namespace
-                .unwrap_or_else(|| default_namespace.to_string());
-            ParentReference::Server(ResourceId::new(namespace, parent_ref.name))
-        } else if policy::httproute::parent_ref_targets_kind::<Service>(&parent_ref) {
+            let namespace = parent_ref.namespace.as_deref().unwrap_or(default_namespace);
+            ParentReference::Server(ResourceId::new(
+                namespace.to_string(),
+                parent_ref.name.clone(),
+            ))
+        } else if policy::httproute::parent_ref_targets_kind::<Service>(parent_ref) {
             // If the parent reference does not have a namespace, default to using
             // the HTTPRoute's namespace.
-            let namespace = parent_ref
-                .namespace
-                .unwrap_or_else(|| default_namespace.to_string());
-            ParentReference::Service(ResourceId::new(namespace, parent_ref.name), parent_ref.port)
+            let namespace = parent_ref.namespace.as_deref().unwrap_or(default_namespace);
+            ParentReference::Service(
+                ResourceId::new(namespace.to_string(), parent_ref.name.clone()),
+                parent_ref.port,
+            )
         } else {
             ParentReference::UnknownKind
         }
+    }
+}
+
+impl BackendReference {
+    fn from_backend_ref(
+        backend_ref: &gateway::BackendObjectReference,
+        default_namespace: &str,
+    ) -> Self {
+        if policy::httproute::backend_ref_targets_kind::<linkerd_policy_controller_k8s_api::Service>(
+            backend_ref,
+        ) {
+            let namespace = backend_ref
+                .namespace
+                .as_deref()
+                .unwrap_or(default_namespace);
+            BackendReference::Service(ResourceId::new(
+                namespace.to_string(),
+                backend_ref.name.clone(),
+            ))
+        } else {
+            BackendReference::Unknown
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use linkerd_policy_controller_k8s_api::{policy, ObjectMeta};
+
+    fn mk_default_http_backends(
+        backend_refs: Vec<gateway::BackendObjectReference>,
+    ) -> Option<Vec<gateway::HttpBackendRef>> {
+        Some(
+            backend_refs
+                .into_iter()
+                .map(|backend_ref| gateway::HttpBackendRef {
+                    backend_ref: Some(gateway::BackendRef {
+                        inner: backend_ref,
+                        weight: None,
+                    }),
+                    filters: None,
+                })
+                .collect(),
+        )
+    }
+
+    #[test]
+    fn backendrefs_from_route() {
+        let http_route = policy::HttpRoute {
+            metadata: ObjectMeta {
+                namespace: Some("foo".to_string()),
+                name: Some("foo".to_string()),
+                ..Default::default()
+            },
+            spec: policy::HttpRouteSpec {
+                inner: gateway::CommonRouteSpec { parent_refs: None },
+                hostnames: None,
+                rules: Some(vec![
+                    policy::httproute::HttpRouteRule {
+                        matches: None,
+                        filters: None,
+                        backend_refs: mk_default_http_backends(vec![
+                            gateway::BackendObjectReference {
+                                group: None,
+                                kind: None,
+                                name: "ref-1".to_string(),
+                                namespace: Some("default".to_string()),
+                                port: None,
+                            },
+                            gateway::BackendObjectReference {
+                                group: None,
+                                kind: None,
+                                name: "ref-2".to_string(),
+                                namespace: None,
+                                port: None,
+                            },
+                        ]),
+                    },
+                    policy::httproute::HttpRouteRule {
+                        matches: None,
+                        filters: None,
+                        backend_refs: mk_default_http_backends(vec![
+                            gateway::BackendObjectReference {
+                                group: Some("Core".to_string()),
+                                kind: Some("Service".to_string()),
+                                name: "ref-3".to_string(),
+                                namespace: Some("default".to_string()),
+                                port: None,
+                            },
+                        ]),
+                    },
+                    policy::httproute::HttpRouteRule {
+                        matches: None,
+                        filters: None,
+                        backend_refs: None,
+                    },
+                ]),
+            },
+            status: None,
+        };
+
+        let result = make_backends(&http_route);
+        assert_eq!(
+            3,
+            result.len(),
+            "expected only three BackendReferences from route"
+        );
+        result.into_iter().for_each(|backend_ref| {
+            assert!(matches!(backend_ref, BackendReference::Service(_)));
+        })
+    }
+
+    #[test]
+    fn backendrefs_from_multiple_types() {
+        let http_route = policy::HttpRoute {
+            metadata: ObjectMeta {
+                namespace: Some("default".to_string()),
+                name: Some("foo".to_string()),
+                ..Default::default()
+            },
+            spec: policy::HttpRouteSpec {
+                inner: gateway::CommonRouteSpec { parent_refs: None },
+                hostnames: None,
+                rules: Some(vec![policy::httproute::HttpRouteRule {
+                    matches: None,
+                    filters: None,
+                    backend_refs: mk_default_http_backends(vec![
+                        gateway::BackendObjectReference {
+                            group: None,
+                            kind: None,
+                            name: "ref-1".to_string(),
+                            namespace: None,
+                            port: None,
+                        },
+                        gateway::BackendObjectReference {
+                            group: Some("policy.linkerd.io".to_string()),
+                            kind: Some("Server".to_string()),
+                            name: "ref-2".to_string(),
+                            namespace: None,
+                            port: None,
+                        },
+                    ]),
+                }]),
+            },
+            status: None,
+        };
+
+        let result = make_backends(&http_route);
+        assert_eq!(
+            2,
+            result.len(),
+            "expected only two BackendReferences from route"
+        );
+        let mut iter = result.into_iter();
+        let known = iter.next().unwrap();
+        assert!(matches!(known, BackendReference::Service(_)));
+        let unknown = iter.next().unwrap();
+        assert!(matches!(unknown, BackendReference::Unknown))
     }
 }
