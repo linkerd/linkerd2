11ddd743d1560fd94e312594198b01c916751780
Eliza Weisman <eliza@buoyant.io>
2023-05-04 16:13:46 -0700

    bump again to v1.1.3

--- pkg/healthcheck/healthcheck_test.go
+++ pkg/healthcheck/healthcheck_test.go
@@ -2422,7 +2422,7 @@ spec:
       serviceAccountName: linkerd-cni
       containers:
       - name: install-cni
-        image: cr.l5d.io/linkerd/cni-plugin:v1.1.2
+        image: cr.l5d.io/linkerd/cni-plugin:v1.13
         env:
         - name: DEST_CNI_NET_DIR
           valueFrom:
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -16,7 +16,7 @@ var Version = undefinedVersion
 // https://github.com/linkerd/linkerd2-proxy-init This has to be kept in sync
 // with the default version in the control plane's values.yaml.
 var ProxyInitVersion = "v2.2.1"
-var LinkerdCNIVersion = "v1.1.2"
+var LinkerdCNIVersion = "v1.13"
 
 const (
 	// undefinedVersion should take the form `channel-version` to conform to
