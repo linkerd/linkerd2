41b700141b358744093cd4ef9e0e96e41ca47c0e
Shashank <shashanksingh819@gmail.com>
2023-09-26 13:44:32 +0530

    added timeout flag to multicluster check command
    Signed-off-by: Shashank <shashanksingh819@gmail.com>

--- multicluster/cmd/check.go
+++ multicluster/cmd/check.go
@@ -46,12 +46,14 @@ const (
 type checkOptions struct {
 	wait   time.Duration
 	output string
+	timeout time.Duration
 }
 
 func newCheckOptions() *checkOptions {
 	return &checkOptions{
 		wait:   300 * time.Second,
 		output: healthcheck.TableOutput,
+		timeout: 10 * time.Second,
 	}
 }
 
@@ -107,6 +109,7 @@ non-zero exit code.`,
 	}
 	cmd.Flags().StringVarP(&options.output, "output", "o", options.output, "Output format. One of: table, json, short")
 	cmd.Flags().DurationVar(&options.wait, "wait", options.wait, "Maximum allowed time for all tests to pass")
+	cmd.Flags().DurationVar(&options.timeout, "timeout", options.timeout, "timeout duration between running checks")
 	cmd.Flags().Bool("proxy", false, "")
 	cmd.Flags().MarkHidden("proxy")
 	cmd.Flags().StringP("namespace", "n", "", "")
@@ -151,7 +154,7 @@ func configureAndRunChecks(wout io.Writer, werr io.Writer, options *checkOptions
 	}
 
 	hc := newHealthChecker(linkerdHC)
-	category := multiclusterCategory(hc, options.wait)
+	category := multiclusterCategory(hc, options.timeout)
 	hc.AppendCategories(category)
 	success, warning := healthcheck.RunChecks(wout, werr, hc, options.output)
 	healthcheck.PrintChecksResult(wout, options.output, success, warning)
