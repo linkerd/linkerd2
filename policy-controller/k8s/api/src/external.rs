585bcf30a8fdd11d4b3cd36bc3d0b04075683e47
Matei David <matei@buoyant.io>
2023-10-12 10:41:13 +0000

    Updated external resource
    Signed-off-by: Matei David <matei@buoyant.io>

--- policy-controller/k8s/api/src/external.rs
+++ policy-controller/k8s/api/src/external.rs
@@ -5,18 +5,37 @@ use serde::{Deserialize, Serialize};
 /// Describes an interface exposed by a VM
 #[derive(Clone, Debug, PartialEq, Eq, CustomResource, Deserialize, Serialize, JsonSchema)]
 #[kube(
-    group = "linkerd.io",
+    group = "multicluster.linkerd.io",
     version = "v1alpha1",
     kind = "ExternalWorkload",
+    status = "ExternalWorkloadStatus",
     namespaced
 )]
 pub struct ExternalWorkloadSpec {
     pub address: String,
     pub ports: Vec<PortSpec>,
+    pub identity: String,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
 pub struct PortSpec {
     pub port: std::num::NonZeroU16,
     protocol: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
+pub struct ExternalWorkloadStatus {
+    pub conditions: Vec<Condition>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct Condition {
+    last_probe_time: Option<String>,
+    last_transition_time: Option<String>,
+    status: Option<String>,
+    #[serde(rename = "type")]
+    typ: Option<String>,
+    reason: Option<String>,
+    message: Option<String>,
 }
