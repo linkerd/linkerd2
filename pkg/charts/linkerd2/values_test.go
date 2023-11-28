226c4ec311f521c46469dd16ecd2584f1f499c4b
Alejandro Pedraza <alejandro@buoyant.io>
2023-11-28 14:58:02 -0500

    Create top-level kubeAPI config

--- pkg/charts/linkerd2/values_test.go
+++ pkg/charts/linkerd2/values_test.go
@@ -173,13 +173,15 @@ func TestNewValues(t *testing.T) {
 		},
 		Identity: &Identity{
 			ServiceAccountTokenProjection: true,
-			KubeAPIClientQPS:              100,
-			KubeAPIClientBurst:            200,
 			Issuer: &Issuer{
 				ClockSkewAllowance: "20s",
 				IssuanceLifetime:   "24h0m0s",
 				TLS:                &IssuerTLS{},
 				Scheme:             "linkerd.io/tls",
+			},
+			KubeAPI: &KubeAPI{
+				ClientQPS:   100,
+				ClientBurst: 200,
 			},
 		},
 		NodeSelector: map[string]string{
