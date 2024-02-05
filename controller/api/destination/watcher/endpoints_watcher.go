10daa97133208910800c4fac24caa3c1c5ded699
Alejandro Pedraza <alejandro@buoyant.io>
2024-02-05 16:20:33 -0500

    @adleong's feedback

--- controller/api/destination/watcher/endpoints_watcher.go
+++ controller/api/destination/watcher/endpoints_watcher.go
@@ -154,6 +154,27 @@ var endpointsVecs = newEndpointsMetricsVecs()
 
 var undefinedEndpointPort = Port(0)
 
+// shallowCopy returns a shallow copy of addr, in the sense that the Pod and
+// ExternalWorkload fields of the Addresses map values still point to the
+// locations of the original variable
+func (addr AddressSet) shallowCopy() AddressSet {
+	addresses := make(map[ID]Address)
+	for k, v := range addr.Addresses {
+		addresses[k] = v
+	}
+
+	labels := make(map[string]string)
+	for k, v := range addr.Labels {
+		labels[k] = v
+	}
+
+	return AddressSet{
+		Addresses:          addresses,
+		Labels:             labels,
+		LocalTrafficPolicy: addr.LocalTrafficPolicy,
+	}
+}
+
 // NewEndpointsWatcher creates an EndpointsWatcher and begins watching the
 // k8sAPI for pod, service, and endpoint changes. An EndpointsWatcher will
 // watch on Endpoints or EndpointSlice resources, depending on cluster configuration.
@@ -1146,8 +1167,9 @@ func (pp *portPublisher) resolveTargetPort(subset corev1.EndpointSubset) Port {
 func (pp *portPublisher) updateLocalTrafficPolicy(localTrafficPolicy bool) {
 	pp.localTrafficPolicy = localTrafficPolicy
 	pp.addresses.LocalTrafficPolicy = localTrafficPolicy
+	addresses := pp.addresses.shallowCopy()
 	for _, listener := range pp.listeners {
-		listener.Add(pp.addresses)
+		listener.Add(addresses)
 	}
 }
 
@@ -1212,7 +1234,7 @@ func (pp *portPublisher) noEndpoints(exists bool) {
 func (pp *portPublisher) subscribe(listener EndpointUpdateListener) {
 	if pp.exists {
 		if len(pp.addresses.Addresses) > 0 {
-			listener.Add(pp.addresses)
+			listener.Add(pp.addresses.shallowCopy())
 		} else {
 			listener.NoEndpoints(true)
 		}
@@ -1312,8 +1334,9 @@ func (pp *portPublisher) updateServer(server *v1beta2.Server, isAdd bool) {
 		}
 	}
 	if updated {
+		addresses := pp.addresses.shallowCopy()
 		for _, listener := range pp.listeners {
-			listener.Add(pp.addresses)
+			listener.Add(addresses)
 		}
 		pp.metrics.incUpdates()
 	}
@@ -1322,19 +1345,6 @@ func (pp *portPublisher) updateServer(server *v1beta2.Server, isAdd bool) {
 ////////////
 /// util ///
 ////////////
-
-// WithPort sets the port field in all addresses of an address set.
-func (as *AddressSet) WithPort(port Port) AddressSet {
-	wp := AddressSet{
-		Addresses: map[PodID]Address{},
-		Labels:    as.Labels,
-	}
-	for id, addr := range as.Addresses {
-		addr.Port = port
-		wp.Addresses[id] = addr
-	}
-	return wp
-}
 
 // getTargetPort returns the port specified as an argument if no service is
 // present. If the service is present and it has a port spec matching the
