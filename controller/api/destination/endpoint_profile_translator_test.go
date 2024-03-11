75b24a245850ff051b9952f2b57e2f5d2f3e0780
wafuwafu13 <jaruwafu@gmail.com>
2024-03-11 21:26:22 +0000

    test: exclude ErrServerClosed
    Signed-off-by: wafuwafu13 <jaruwafu@gmail.com>

--- controller/api/destination/endpoint_profile_translator_test.go
+++ controller/api/destination/endpoint_profile_translator_test.go
@@ -1,8 +1,10 @@
 package destination
 
 import (
+	"errors"
 	"testing"
 	"time"
+	"net/http"
 
 	pb "github.com/linkerd/linkerd2-proxy-api/go/destination"
 	"github.com/linkerd/linkerd2/controller/api/destination/watcher"
@@ -113,8 +115,9 @@ func TestEndpointProfileTranslator(t *testing.T) {
 		// The queue should be full and the next update should fail.
 		t.Logf("Queue length=%d capacity=%d", translator.queueLen(), updateQueueCapacity)
 		if err := translator.Update(podAddr); err == nil {
-			t.Fatalf("Expected update to fail; queue=%d; capacity=%d", translator.queueLen(), updateQueueCapacity)
-
+			if !errors.Is(err, http.ErrServerClosed) {
+			  t.Fatalf("Expected update to fail; queue=%d; capacity=%d", translator.queueLen(), updateQueueCapacity)
+			}
 		}
 
 		select {
