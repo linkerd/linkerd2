cbf2a91016218b4fc6c8b6c77852b1b2ef08724f
Alex Leong <alex@buoyant.io>
2024-03-26 13:42:47 -0700

    Add status queue metrics (#12256)
    The policy controller uses a queue to hold HttpRoute status patches before they are applied to the Kube API.  In order to have better observaility into the state of this queue, we add metrics to count enqueues, dequeues, drops, and overflows.
    
    ```
    # HELP resource_status_patch_dequeues_total Counter of patches dequeued from the updates channel.
    # TYPE resource_status_patch_dequeues_total counter
    resource_status_patch_dequeues_total_total 1430
    # HELP resource_status_patch_drops_total Counter of patches dropped because we are not the leader.
    # TYPE resource_status_patch_drops_total counter
    resource_status_patch_drops_total_total 0
    # HELP resource_status_patch_enqueues_total Counter of patches enqueued to the updates channel.
    # TYPE resource_status_patch_enqueues_total counter
    resource_status_patch_enqueues_total_total 1430
    # HELP resource_status_patch_channel_full_total Counter of patches dropped because the updates channel is full.
    # TYPE resource_status_patch_channel_full_total counter
    resource_status_patch_channel_full_total_total 0
    ```
    
    Signed-off-by: Alex Leong <alex@buoyant.io>
    Co-authored-by: Oliver Gould <ver@buoyant.io>

--- policy-controller/src/main.rs
+++ policy-controller/src/main.rs
@@ -126,8 +126,9 @@ async fn main() -> Result<()> {
     };
 
     let mut prom = <Registry>::default();
-    let status_metrics =
-        status::ControllerMetrics::register(prom.sub_registry_with_prefix("resource_status"));
+    let resource_status = prom.sub_registry_with_prefix("resource_status");
+    let status_metrics = status::ControllerMetrics::register(resource_status);
+    let status_index_metrcs = status::IndexMetrics::register(resource_status);
 
     let mut runtime = kubert::Runtime::builder()
         .with_log(log_level, log_format)
@@ -174,7 +175,12 @@ async fn main() -> Result<()> {
     // Build the status index which will maintain information necessary for
     // updating the status field of policy resources.
     let (updates_tx, updates_rx) = mpsc::channel(STATUS_UPDATE_QUEUE_SIZE);
-    let status_index = status::Index::shared(hostname.clone(), claims.clone(), updates_tx);
+    let status_index = status::Index::shared(
+        hostname.clone(),
+        claims.clone(),
+        updates_tx,
+        status_index_metrcs,
+    );
 
     // Spawn resource watches.
 
