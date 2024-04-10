1959a1df52ff1e8f2527b5456eb96d51e5840d18
Alex Leong <alex@buoyant.io>
2024-04-10 09:21:41 -0700

    fix panic in service mirror controller (#12406)
    If the service mirror controller encounters an error listing mirror services during endpoints repair, this error is logged but processing continues.  This causes a nil pointer dereference which results in a panic.
    
    Instead, we abort the endpoints repair with a retryable error so that the repair endpoints event is re-enqueued and the panic is avoided.
    
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- multicluster/service-mirror/cluster_watcher.go
+++ multicluster/service-mirror/cluster_watcher.go
@@ -1030,7 +1030,7 @@ func (rcsw *RemoteClusterServiceWatcher) repairEndpoints(ctx context.Context) er
 	// Repair mirror service endpoints.
 	mirrorServices, err := rcsw.getMirrorServices()
 	if err != nil {
-		rcsw.log.Errorf("Failed to list mirror services: %s", err)
+		return RetryableError{[]error{fmt.Errorf("Failed to list mirror services: %w", err)}}
 	}
 	for _, svc := range mirrorServices.Items {
 		svc := svc
@@ -1057,6 +1057,7 @@ func (rcsw *RemoteClusterServiceWatcher) repairEndpoints(ctx context.Context) er
 			endpoints, err = rcsw.localAPIClient.Client.CoreV1().Endpoints(svc.Namespace).Get(ctx, svc.Name, metav1.GetOptions{})
 			if err != nil {
 				rcsw.log.Errorf("Failed to get local endpoints %s/%s: %s", svc.Namespace, svc.Name, err)
+				continue
 			}
 		}
 		updatedEndpoints := endpoints.DeepCopy()
