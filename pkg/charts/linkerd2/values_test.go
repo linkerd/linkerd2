246c62e7d3b3740bbb97c2aa74f68925c322f714
Oliver Gould <ver@buoyant.io>
2024-04-29 13:51:21 -0700

    feat: Configure default HTTP/2 server keep-alives (#12498)
    HTTP/2 keep-alives enable HTTP/2 servers to issue PING messages to clients to
    ensure that the connections are still healthy. This is especially useful when
    the OS loses a FIN packet, which causes the connection resources to be held by
    the proxy until we attempt to write to the socket again. Keep-alives provide a
    mechanism to issue periodic writes on the socket to test the connection health.
    
    5760ed2 updated the proxy to support HTTP/2 server configuration and 9e241a7
    updated the proxy's Helm templating to support aribtrary configuration.
    Together, these changes enable configuring default HTTP/2 server keep-alives
    for all data planes in a cluster.
    
    The default keep-alive configuration uses a 10s interval and a 3s timeout. These
    are chosen to be conservative enough that they don't trigger false positive
    timeouts, but frequent enough that these connections may be garbage collected
    in a timely fashion.

--- pkg/charts/linkerd2/values_test.go
+++ pkg/charts/linkerd2/values_test.go
@@ -161,6 +161,22 @@ func TestNewValues(t *testing.T) {
 					Lifetime:       "1h",
 				},
 			},
+			Inbound: ProxyParams{
+				"server": ProxyScopeParams{
+					"http2": ProxyProtoParams{
+						"keepAliveInterval": "10s",
+						"keepAliveTimeout":  "3s",
+					},
+				},
+			},
+			Outbound: ProxyParams{
+				"server": ProxyScopeParams{
+					"http2": ProxyProtoParams{
+						"keepAliveInterval": "10s",
+						"keepAliveTimeout":  "3s",
+					},
+				},
+			},
 		},
 		ProxyInit: &ProxyInit{
 			IptablesMode:        "legacy",
