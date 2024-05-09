b4feef4179ae1ff914c07d8fe4d2b474df84653e
Mark S <the@wondersmith.dev>
2024-05-09 14:42:14 -0400

    style: apply module name changes
    Signed-off-by: Mark S <the@wondersmith.dev>

--- policy-controller/core/src/lib.rs
+++ policy-controller/core/src/lib.rs
@@ -1,11 +1,11 @@
 #![deny(warnings, rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
-pub mod http_route;
 mod identity_match;
 pub mod inbound;
 mod network_match;
 pub mod outbound;
+pub mod routes;
 
 pub use self::{identity_match::IdentityMatch, network_match::NetworkMatch};
 pub use ipnet::{IpNet, Ipv4Net, Ipv6Net};
