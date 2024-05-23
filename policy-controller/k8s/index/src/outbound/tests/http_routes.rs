d81b1f0cfe4cd4ddb2ca3be3f3ced1ea036f2e98
Alex Leong <alex@buoyant.io>
2024-05-23 14:10:00 -0700

    Reindex outbound policy backends when a service changes (#12635)
    If an HTTPRoute references a backend service that does not exist, the policy controller synthesizes a FailureInjector in the outbound policy so that requests to that backend will fail with a 500 status code.  However, we do not update the policy when backend services are created or deleted, which can result in an outbound policy that synthesizes 500s for backends, even if the backend currently exists (or vice versa).
    
    This is often papered over because when a backend service is created or deleted, this will trigger the HTTPRoute's ResolvedRef status condition to change which will cause a reindex of the HTTPRotue and a recomputation of the backends.  However, depending on the specific order that these updates are processed, the outbound policy can still be left with the incorrect backend state.
    
    In order to be able to update the backend of an outbound policy when backend services are created or deleted, we change the way these backends are represented in the index.  Previously, we had represented backends which were services that did not exist as `Backend::Invalid`.  However, this discards the necessary backend information necessary to recreate the backend if the service is created.  Instead, we update this to represent these backends as a `Backend::Service` but with a new field `exists` set to false.  This allows us to update this field as backend services are created or deleted.
    
    Signed-off-by: Alex Leong <alex@buoyant.io>

---
+++ policy-controller/k8s/index/src/outbound/tests/http_routes.rs
@@ -0,0 +1,173 @@
+use k8s_gateway_api::BackendRef;
+use kube::Resource;
+use linkerd_policy_controller_core::{
+    http_route::GroupKindNamespaceName,
+    outbound::{Backend, WeightedService},
+    POLICY_CONTROLLER_NAME,
+};
+use tracing::Level;
+
+use super::*;
+
+#[test]
+fn backend_service() {
+    tracing_subscriber::fmt()
+        .with_max_level(Level::TRACE)
+        .init();
+
+    let test = TestConfig::default();
+    // Create apex service.
+    let apex = mk_service("ns", "apex", 8080);
+    test.index.write().apply(apex);
+
+    // Create httproute.
+    let route = mk_route("ns", "route", 8080, "apex", "backend");
+    test.index.write().apply(route);
+
+    let mut rx = test
+        .index
+        .write()
+        .outbound_policy_rx(
+            "apex".to_string(),
+            "ns".to_string(),
+            8080.try_into().unwrap(),
+            "ns".to_string(),
+        )
+        .expect("apex.ns should exist");
+
+    {
+        let policy = rx.borrow_and_update();
+        let backend = policy
+            .http_routes
+            .get(&GroupKindNamespaceName {
+                group: k8s::policy::HttpRoute::group(&()),
+                kind: k8s::policy::HttpRoute::kind(&()),
+                namespace: "ns".into(),
+                name: "route".into(),
+            })
+            .expect("route should exist")
+            .rules
+            .first()
+            .expect("rule should exist")
+            .backends
+            .first()
+            .expect("backend should exist");
+        let exists = match backend {
+            Backend::Service(WeightedService { exists, .. }) => exists,
+            _ => panic!("backend should be a service"),
+        };
+        // Backend should not exist.
+        assert!(!exists);
+    }
+
+    // Create backend service.
+    let backend = mk_service("ns", "backend", 8080);
+    test.index.write().apply(backend);
+    assert!(rx.has_changed().unwrap());
+
+    {
+        let policy = rx.borrow_and_update();
+        let backend = policy
+            .http_routes
+            .get(&GroupKindNamespaceName {
+                group: k8s::policy::HttpRoute::group(&()),
+                kind: k8s::policy::HttpRoute::kind(&()),
+                namespace: "ns".into(),
+                name: "route".into(),
+            })
+            .expect("route should exist")
+            .rules
+            .first()
+            .expect("rule should exist")
+            .backends
+            .first()
+            .expect("backend should exist");
+        let exists = match backend {
+            Backend::Service(WeightedService { exists, .. }) => exists,
+            _ => panic!("backend should be a service"),
+        };
+        // Backend should exist.
+        assert!(exists);
+    }
+}
+
+fn mk_route(
+    ns: impl ToString,
+    name: impl ToString,
+    port: u16,
+    parent: impl ToString,
+    backend: impl ToString,
+) -> k8s::policy::HttpRoute {
+    use chrono::Utc;
+    use k8s::{policy::httproute::*, Time};
+
+    HttpRoute {
+        metadata: k8s::ObjectMeta {
+            namespace: Some(ns.to_string()),
+            name: Some(name.to_string()),
+            creation_timestamp: Some(Time(Utc::now())),
+            ..Default::default()
+        },
+        spec: HttpRouteSpec {
+            inner: CommonRouteSpec {
+                parent_refs: Some(vec![ParentReference {
+                    group: Some("core".to_string()),
+                    kind: Some("Service".to_string()),
+                    namespace: Some(ns.to_string()),
+                    name: parent.to_string(),
+                    section_name: None,
+                    port: Some(port),
+                }]),
+            },
+            hostnames: None,
+            rules: Some(vec![HttpRouteRule {
+                matches: Some(vec![HttpRouteMatch {
+                    path: Some(HttpPathMatch::PathPrefix {
+                        value: "/foo/bar".to_string(),
+                    }),
+                    headers: None,
+                    query_params: None,
+                    method: Some("GET".to_string()),
+                }]),
+                filters: None,
+                backend_refs: Some(vec![HttpBackendRef {
+                    backend_ref: Some(BackendRef {
+                        weight: None,
+                        inner: BackendObjectReference {
+                            group: Some("core".to_string()),
+                            kind: Some("Service".to_string()),
+                            namespace: Some(ns.to_string()),
+                            name: backend.to_string(),
+                            port: Some(port),
+                        },
+                    }),
+                    filters: None,
+                }]),
+                timeouts: None,
+            }]),
+        },
+        status: Some(k8s::policy::httproute::HttpRouteStatus {
+            inner: k8s::gateway::RouteStatus {
+                parents: vec![k8s::gateway::RouteParentStatus {
+                    parent_ref: ParentReference {
+                        group: Some("core".to_string()),
+                        kind: Some("Service".to_string()),
+                        namespace: Some(ns.to_string()),
+                        name: parent.to_string(),
+                        section_name: None,
+                        port: Some(port),
+                    },
+                    controller_name: POLICY_CONTROLLER_NAME.to_string(),
+                    conditions: vec![k8s::Condition {
+                        last_transition_time: k8s::Time(chrono::DateTime::<chrono::Utc>::MIN_UTC),
+                        message: "".to_string(),
+                        observed_generation: None,
+                        reason: "Accepted".to_string(),
+                        status: "True".to_string(),
+                        type_: "Accepted".to_string(),
+                    }],
+                }],
+            },
+        }),
+    }
+}
