b59149388f9e898cffb775bb19994f494b7b4976
Alejandro Pedraza <alejandro@buoyant.io>
2024-06-13 17:15:41 -0500

    Bump proxy-init to v2.4.1 and cni-plugin to v1.5.1 (#12711)
    Those releases ensure that when IPv6 is enabled, the series of ip6tables commands succeed. If they fail, the proxy-init/linkerd-cni containers should fail as well, instead of ignoring errors.
    
    See linkerd/linkerd2-proxy-init#388

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -15,8 +15,8 @@ var Version = undefinedVersion
 // ProxyInitVersion is the pinned version of the proxy-init, from
 // https://github.com/linkerd/linkerd2-proxy-init This has to be kept in sync
 // with the default version in the control plane's values.yaml.
-var ProxyInitVersion = "v2.4.0"
-var LinkerdCNIVersion = "v1.5.0"
+var ProxyInitVersion = "v2.4.1"
+var LinkerdCNIVersion = "v1.5.1"
 
 const (
 	// undefinedVersion should take the form `channel-version` to conform to
