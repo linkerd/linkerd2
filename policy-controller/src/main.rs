6e38368ef0f9fc0a4106d57cc837cd0c2ae2ebf2
Oliver Gould <ver@buoyant.io>
2024-11-14 11:13:02 -0800

    chore(policy): fix HttpLocalRateLimit type casing (#13324)
    Our other types (e.g. HttpRoute) follow the Rust convention of using "Http"
    instead of "HTTP".
    
    This commit updates the policy controller to reflect this convention.
    
    No user-facing changes.

--- policy-controller/src/main.rs
+++ policy-controller/src/main.rs
@@ -254,7 +254,7 @@ async fn main() -> Result<()> {
     );
 
     let ratelimit_policies =
-        runtime.watch_all::<k8s::policy::HTTPLocalRateLimitPolicy>(watcher::Config::default());
+        runtime.watch_all::<k8s::policy::HttpLocalRateLimitPolicy>(watcher::Config::default());
     let ratelimit_policies_indexes = IndexList::new(inbound_index.clone())
         .push(status_index.clone())
         .shared();
