c3377aec6c86a15c23ba86bc8fbc1b53a616812a
Oliver Gould <ver@buoyant.io>
2024-12-11 02:13:43 +0000

    chore: simplify protoc scripts
    The bin/protoc script is ancient and not useful, especially in light of tools
    provided by dev containers. Furthermore, it includes a reference to an old gross
    sourcefource downlaod page for unzip.
    
    Furthermore, the protoc-go.sh script does seperate version pinning of build
    dependencies.
    
    To simplify all of this, this change removes the bin/protoc and bin/protoc-diff
    scripts. The "proto" CI workflow is updated to use the protoc-go.sh script
    directly.

--- bin/protoc-go.sh
+++ bin/protoc-go.sh
@@ -2,21 +2,14 @@
 
 set -eu
 
-bindir=$( cd "${0%/*}" && pwd )
-
-if ! command -v protoc-gen-go-grpc >/dev/null ; then
-    go install -mod=readonly google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-    go install -mod=readonly google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
-fi
-
 rm -rf controller/gen/common controller/gen/config viz/metrics-api/gen viz/tap/gen
 mkdir -p controller/gen/common/net viz/metrics-api/gen/viz viz/tap/gen/tap
 
-"$bindir"/protoc -I proto --go_out=paths=source_relative:controller/gen proto/common/net.proto
-"$bindir"/protoc -I proto -I viz/metrics-api/proto --go_out=paths=source_relative:viz/metrics-api/gen viz/metrics-api/proto/viz.proto
-"$bindir"/protoc -I proto -I viz/metrics-api/proto --go-grpc_out=paths=source_relative:viz/metrics-api/gen/viz viz/metrics-api/proto/viz.proto
-"$bindir"/protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go_out=paths=source_relative:viz/tap/gen viz/tap/proto/viz_tap.proto
-"$bindir"/protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go-grpc_out=paths=source_relative:viz/tap/gen/tap viz/tap/proto/viz_tap.proto
+protoc -I proto --go_out=paths=source_relative:controller/gen proto/common/net.proto
+protoc -I proto -I viz/metrics-api/proto --go_out=paths=source_relative:viz/metrics-api/gen viz/metrics-api/proto/viz.proto
+protoc -I proto -I viz/metrics-api/proto --go-grpc_out=paths=source_relative:viz/metrics-api/gen/viz viz/metrics-api/proto/viz.proto
+protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go_out=paths=source_relative:viz/tap/gen viz/tap/proto/viz_tap.proto
+protoc -I proto -I viz/tap/proto -I viz/metrics-api/proto --go-grpc_out=paths=source_relative:viz/tap/gen/tap viz/tap/proto/viz_tap.proto
 
 mv controller/gen/common/net.pb.go   controller/gen/common/net/
 mv viz/metrics-api/gen/viz.pb.go viz/metrics-api/gen/viz/viz.pb.go
