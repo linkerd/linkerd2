881194f7fb7d1dc54eac6fc6fb6b1447932558ab
Alejandro Pedraza <alejandro@buoyant.io>
2025-03-26 10:18:34 -0500

    Extract logic into separate methods for improved readability, remove duped code in creds generation

--- multicluster/cmd/link-gen.go
+++ multicluster/cmd/link-gen.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -56,7 +57,7 @@ each cluster and applied to the other.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 
 			if opts.clusterName == "" {
-				return errors.New("You need to specify cluster name")
+				return errors.New("you need to specify cluster name")
 			}
 
 			configMap, err := getLinkerdConfigMap(cmd.Context())
@@ -67,242 +68,47 @@ each cluster and applied to the other.`,
 				return err
 			}
 
-			rules := clientcmd.NewDefaultClientConfigLoadingRules()
-			rules.ExplicitPath = kubeconfigPath
-			loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
-			config, err := loader.RawConfig()
+			k, err := k8s.NewAPI(kubeconfigPath, kubeContext, impersonate, impersonateGroup, 0)
 			if err != nil {
 				return err
 			}
 
-			if kubeContext != "" {
-				config.CurrentContext = kubeContext
-			}
-
-			k, err := k8s.NewAPI(kubeconfigPath, config.CurrentContext, impersonate, impersonateGroup, 0)
+			kubeconfig, err := getKubeconfig(cmd.Context(), k, opts)
 			if err != nil {
 				return err
 			}
 
-			sa, err := k.CoreV1().ServiceAccounts(opts.namespace).Get(cmd.Context(), opts.serviceAccountName, metav1.GetOptions{})
+			creds, err := getCreds(kubeconfig, opts.clusterName, opts.namespace, nil, nil, opts.output)
 			if err != nil {
 				return err
 			}
 
-			listOpts := metav1.ListOptions{
-				FieldSelector: fmt.Sprintf("type=%s", corev1.SecretTypeServiceAccountToken),
+			destinationLabels := map[string]string{
+				clusterNameLabel: opts.clusterName,
 			}
-			secrets, err := k.CoreV1().Secrets(opts.namespace).List(cmd.Context(), listOpts)
+			destinationAnnotations := map[string]string{
+				trustDomainAnnotation:   configMap.IdentityTrustDomain,
+				clusterDomainAnnotation: configMap.ClusterDomain,
+			}
+			destinationCreds, err := getCreds(kubeconfig, opts.clusterName, controlPlaneNamespace, destinationLabels, destinationAnnotations, opts.output)
 			if err != nil {
 				return err
 			}
 
-			token, err := extractSAToken(secrets.Items, sa.Name)
+			link, err := getLink(cmd.Context(), k, configMap.ClusterDomain, opts)
 			if err != nil {
 				return err
-			}
-
-			context, ok := config.Contexts[config.CurrentContext]
-			if !ok {
-				return fmt.Errorf("could not extract current context from config")
-			}
-
-			context.AuthInfo = opts.serviceAccountName
-			config.Contexts = map[string]*api.Context{
-				config.CurrentContext: context,
-			}
-			config.AuthInfos = map[string]*api.AuthInfo{
-				opts.serviceAccountName: {
-					Token: token,
-				},
-			}
-
-			cluster := config.Clusters[context.Cluster]
-
-			if opts.apiServerAddress != "" {
-				cluster.Server = opts.apiServerAddress
-			}
-
-			config.Clusters = map[string]*api.Cluster{
-				context.Cluster: cluster,
-			}
-
-			kubeconfig, err := clientcmd.Write(config)
-			if err != nil {
-				return err
-			}
-
-			creds := corev1.Secret{
-				Type:     k8s.MirrorSecretType,
-				TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fmt.Sprintf("cluster-credentials-%s", opts.clusterName),
-					Namespace: opts.namespace,
-				},
-				Data: map[string][]byte{
-					k8s.ConfigKeyName: kubeconfig,
-				},
-			}
-
-			var credsOut []byte
-			if opts.output == "yaml" {
-				credsOut, err = yaml.Marshal(creds)
-				if err != nil {
-					return err
-				}
-			} else if opts.output == "json" {
-				credsOut, err = json.Marshal(creds)
-				if err != nil {
-					return err
-				}
-			} else {
-				return fmt.Errorf("output format %s not supported", opts.output)
-			}
-
-			destinationCreds := corev1.Secret{
-				Type:     k8s.MirrorSecretType,
-				TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      fmt.Sprintf("cluster-credentials-%s", opts.clusterName),
-					Namespace: controlPlaneNamespace,
-					Labels: map[string]string{
-						clusterNameLabel: opts.clusterName,
-					},
-					Annotations: map[string]string{
-						trustDomainAnnotation:   configMap.IdentityTrustDomain,
-						clusterDomainAnnotation: configMap.ClusterDomain,
-					},
-				},
-				Data: map[string][]byte{
-					k8s.ConfigKeyName: kubeconfig,
-				},
-			}
-
-			var destinationCredsOut []byte
-			if opts.output == "yaml" {
-				destinationCredsOut, err = yaml.Marshal(destinationCreds)
-				if err != nil {
-					return err
-				}
-			} else if opts.output == "json" {
-				destinationCredsOut, err = json.Marshal(destinationCreds)
-				if err != nil {
-					return err
-				}
-			} else {
-				return fmt.Errorf("output format %s not supported", opts.output)
-			}
-
-			remoteDiscoverySelector, err := metav1.ParseToLabelSelector(opts.remoteDiscoverySelector)
-			if err != nil {
-				return err
-			}
-
-			federatedServiceSelector, err := metav1.ParseToLabelSelector(opts.federatedServiceSelector)
-			if err != nil {
-				return err
-			}
-
-			link := v1alpha3.Link{
-				TypeMeta: metav1.TypeMeta{Kind: "Link", APIVersion: "multicluster.linkerd.io/v1alpha3"},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      opts.clusterName,
-					Namespace: opts.namespace,
-					Annotations: map[string]string{
-						k8s.CreatedByAnnotation: k8s.CreatedByAnnotationValue(),
-					},
-				},
-				Spec: v1alpha3.LinkSpec{
-					TargetClusterName:             opts.clusterName,
-					TargetClusterDomain:           configMap.ClusterDomain,
-					TargetClusterLinkerdNamespace: controlPlaneNamespace,
-					ClusterCredentialsSecret:      fmt.Sprintf("cluster-credentials-%s", opts.clusterName),
-					RemoteDiscoverySelector:       remoteDiscoverySelector,
-					FederatedServiceSelector:      federatedServiceSelector,
-				},
-			}
-
-			// If there is a gateway in the exporting cluster, populate Link
-			// resource with gateway information
-			if opts.enableGateway {
-				gateway, err := k.CoreV1().Services(opts.gatewayNamespace).Get(cmd.Context(), opts.gatewayName, metav1.GetOptions{})
-				if err != nil {
-					return err
-				}
-
-				gwAddresses := []string{}
-				for _, ingress := range gateway.Status.LoadBalancer.Ingress {
-					addr := ingress.IP
-					if addr == "" {
-						addr = ingress.Hostname
-					}
-					if addr == "" {
-						continue
-					}
-					gwAddresses = append(gwAddresses, addr)
-				}
-
-				if opts.gatewayAddresses != "" {
-					link.Spec.GatewayAddress = opts.gatewayAddresses
-				} else if len(gwAddresses) > 0 {
-					link.Spec.GatewayAddress = strings.Join(gwAddresses, ",")
-				} else {
-					return fmt.Errorf("Gateway %s.%s has no ingress addresses", gateway.Name, gateway.Namespace)
-				}
-
-				gatewayIdentity, ok := gateway.Annotations[k8s.GatewayIdentity]
-				if !ok || gatewayIdentity == "" {
-					return fmt.Errorf("Gateway %s.%s has no %s annotation", gateway.Name, gateway.Namespace, k8s.GatewayIdentity)
-				}
-				link.Spec.GatewayIdentity = gatewayIdentity
-
-				probeSpec, err := extractProbeSpec(gateway)
-				if err != nil {
-					return err
-				}
-				link.Spec.ProbeSpec = probeSpec
-
-				gatewayPort, err := extractGatewayPort(gateway)
-				if err != nil {
-					return err
-				}
-
-				// Override with user provided gateway port if present
-				if opts.gatewayPort != 0 {
-					gatewayPort = opts.gatewayPort
-				}
-				link.Spec.GatewayPort = fmt.Sprintf("%d", gatewayPort)
-
-				link.Spec.Selector, err = metav1.ParseToLabelSelector(opts.selector)
-				if err != nil {
-					return err
-				}
-			}
-
-			var linkOut []byte
-			if opts.output == "yaml" {
-				linkOut, err = yaml.Marshal(link)
-				if err != nil {
-					return err
-				}
-			} else if opts.output == "json" {
-				linkOut, err = json.Marshal(link)
-				if err != nil {
-					return err
-				}
-			} else {
-				return fmt.Errorf("output format %s not supported", opts.output)
 			}
 
 			separator := []byte("---\n")
 			if opts.output == "json" {
 				separator = []byte("\n")
 			}
-			stdout.Write(credsOut)
+			stdout.Write(creds)
 			stdout.Write(separator)
-			stdout.Write(destinationCredsOut)
+			stdout.Write(destinationCreds)
 			stdout.Write(separator)
-			stdout.Write(linkOut)
+			stdout.Write(link)
 			stdout.Write(separator)
 
 			return nil
@@ -327,6 +133,207 @@ each cluster and applied to the other.`,
 		cmd, []string{"namespace", "gateway-namespace"},
 		kubeconfigPath, impersonate, impersonateGroup, kubeContext)
 	return cmd
+}
+
+func getKubeconfig(ctx context.Context, k *k8s.KubernetesAPI, opts *linkGenOptions) ([]byte, error) {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	rules.ExplicitPath = kubeconfigPath
+	loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+	config, err := loader.RawConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if kubeContext != "" {
+		config.CurrentContext = kubeContext
+	}
+
+	sa, err := k.CoreV1().ServiceAccounts(opts.namespace).Get(ctx, opts.serviceAccountName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	listOpts := metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("type=%s", corev1.SecretTypeServiceAccountToken),
+	}
+	secrets, err := k.CoreV1().Secrets(opts.namespace).List(ctx, listOpts)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := extractSAToken(secrets.Items, sa.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	context, ok := config.Contexts[config.CurrentContext]
+	if !ok {
+		return nil, fmt.Errorf("could not extract current context from config")
+	}
+
+	context.AuthInfo = opts.serviceAccountName
+	config.Contexts = map[string]*api.Context{
+		config.CurrentContext: context,
+	}
+	config.AuthInfos = map[string]*api.AuthInfo{
+		opts.serviceAccountName: {
+			Token: token,
+		},
+	}
+
+	cluster := config.Clusters[context.Cluster]
+
+	if opts.apiServerAddress != "" {
+		cluster.Server = opts.apiServerAddress
+	}
+
+	config.Clusters = map[string]*api.Cluster{
+		context.Cluster: cluster,
+	}
+
+	return clientcmd.Write(config)
+}
+
+func getCreds(kubeconfig []byte, clusterName, namespace string, labels, annotations map[string]string, output string) ([]byte, error) {
+	creds := corev1.Secret{
+		Type:     k8s.MirrorSecretType,
+		TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        fmt.Sprintf("cluster-credentials-%s", clusterName),
+			Namespace:   namespace,
+			Labels:      labels,
+			Annotations: annotations,
+		},
+		Data: map[string][]byte{
+			k8s.ConfigKeyName: kubeconfig,
+		},
+	}
+
+	var credsOut []byte
+	var err error
+
+	switch output {
+	case "yaml":
+		credsOut, err = yaml.Marshal(creds)
+		if err != nil {
+			return nil, err
+		}
+	case "json":
+		credsOut, err = json.Marshal(creds)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("output format %s not supported", output)
+	}
+
+	return credsOut, nil
+}
+
+func getLink(ctx context.Context, k *k8s.KubernetesAPI, clusterDomain string, opts *linkGenOptions) ([]byte, error) {
+	remoteDiscoverySelector, err := metav1.ParseToLabelSelector(opts.remoteDiscoverySelector)
+	if err != nil {
+		return nil, err
+	}
+
+	federatedServiceSelector, err := metav1.ParseToLabelSelector(opts.federatedServiceSelector)
+	if err != nil {
+		return nil, err
+	}
+
+	link := v1alpha3.Link{
+		TypeMeta: metav1.TypeMeta{Kind: "Link", APIVersion: "multicluster.linkerd.io/v1alpha3"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      opts.clusterName,
+			Namespace: opts.namespace,
+			Annotations: map[string]string{
+				k8s.CreatedByAnnotation: k8s.CreatedByAnnotationValue(),
+			},
+		},
+		Spec: v1alpha3.LinkSpec{
+			TargetClusterName:             opts.clusterName,
+			TargetClusterDomain:           clusterDomain,
+			TargetClusterLinkerdNamespace: controlPlaneNamespace,
+			ClusterCredentialsSecret:      fmt.Sprintf("cluster-credentials-%s", opts.clusterName),
+			RemoteDiscoverySelector:       remoteDiscoverySelector,
+			FederatedServiceSelector:      federatedServiceSelector,
+		},
+	}
+
+	// If there is a gateway in the exporting cluster, populate Link
+	// resource with gateway information
+	if opts.enableGateway {
+		gateway, err := k.CoreV1().Services(opts.gatewayNamespace).Get(ctx, opts.gatewayName, metav1.GetOptions{})
+		if err != nil {
+			return nil, err
+		}
+
+		gwAddresses := []string{}
+		for _, ingress := range gateway.Status.LoadBalancer.Ingress {
+			addr := ingress.IP
+			if addr == "" {
+				addr = ingress.Hostname
+			}
+			if addr == "" {
+				continue
+			}
+			gwAddresses = append(gwAddresses, addr)
+		}
+
+		if opts.gatewayAddresses != "" {
+			link.Spec.GatewayAddress = opts.gatewayAddresses
+		} else if len(gwAddresses) > 0 {
+			link.Spec.GatewayAddress = strings.Join(gwAddresses, ",")
+		} else {
+			return nil, fmt.Errorf("gateway %s.%s has no ingress addresses", gateway.Name, gateway.Namespace)
+		}
+
+		gatewayIdentity, ok := gateway.Annotations[k8s.GatewayIdentity]
+		if !ok || gatewayIdentity == "" {
+			return nil, fmt.Errorf("gateway %s.%s has no %s annotation", gateway.Name, gateway.Namespace, k8s.GatewayIdentity)
+		}
+		link.Spec.GatewayIdentity = gatewayIdentity
+
+		probeSpec, err := extractProbeSpec(gateway)
+		if err != nil {
+			return nil, err
+		}
+		link.Spec.ProbeSpec = probeSpec
+
+		gatewayPort, err := extractGatewayPort(gateway)
+		if err != nil {
+			return nil, err
+		}
+
+		// Override with user provided gateway port if present
+		if opts.gatewayPort != 0 {
+			gatewayPort = opts.gatewayPort
+		}
+		link.Spec.GatewayPort = fmt.Sprintf("%d", gatewayPort)
+
+		link.Spec.Selector, err = metav1.ParseToLabelSelector(opts.selector)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var linkOut []byte
+	switch opts.output {
+	case "yaml":
+		linkOut, err = yaml.Marshal(link)
+		if err != nil {
+			return nil, err
+		}
+	case "json":
+		linkOut, err = json.Marshal(link)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("output format %s not supported", opts.output)
+	}
+
+	return linkOut, nil
 }
 
 func newLinkGenOptionsWithDefault() *linkGenOptions {
