a7f6c3607b2fc80279d1330364a3ecd7551ebdc1
Alex Leong <alex@buoyant.io>
2025-07-28 19:06:53 +0000

    Replace root cmd init function with explicit new function
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- cli/cmd/completion_test.go
+++ cli/cmd/completion_test.go
@@ -5,21 +5,22 @@ import (
 )
 
 func TestCompletion(t *testing.T) {
+	rootCmd := NewRootCmd()
 	t.Run("Returns completion code", func(t *testing.T) {
 
-		_, err := getCompletion("bash", RootCmd)
+		_, err := getCompletion("bash", rootCmd)
 		if err != nil {
 			t.Fatalf("Unexpected error: %+v", err)
 		}
 
-		_, err = getCompletion("zsh", RootCmd)
+		_, err = getCompletion("zsh", rootCmd)
 		if err != nil {
 			t.Fatalf("Unexpected error: %+v", err)
 		}
 	})
 
 	t.Run("Fails with invalid shell type", func(t *testing.T) {
-		out, err := getCompletion("foo", RootCmd)
+		out, err := getCompletion("foo", rootCmd)
 		if err == nil {
 			t.Fatalf("Unexpected success for invalid shell type: %+v", out)
 		}
