1c829fa8c45ef7bb9787215c80dbfe407529cd1b
Scott Fleener <scott@buoyant.io>
2025-11-18 15:39:52 -0500

    fix(tls): Force `aws-lc-rs` onto <1.12
    This forces a specific version of `aws-lc-rs` so we can link against the `aws-lc` 2.0 version with a specific FIPS validation.
    
    Once `aws-lc` 3.0 has a FIPS validation, we can upgrade to the latest versions.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- policy-controller/src/main.rs
+++ policy-controller/src/main.rs
@@ -7,10 +7,12 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    if rustls::crypto::aws_lc_rs::default_provider()
-        .install_default()
-        .is_err()
-    {
+    let mut provider = rustls::crypto::aws_lc_rs::default_provider();
+    provider
+        .kx_groups
+        .insert(0, rustls_post_quantum::X25519MLKEM768);
+
+    if provider.install_default().is_err() {
         anyhow::bail!("No other crypto provider should be installed yet");
     }
 
