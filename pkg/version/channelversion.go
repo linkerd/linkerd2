eec3bcc3c9a7a26271580caf210a7558d6c01d1f
Andrew Seigner <siggy@buoyant.io>
2026-06-02 17:41:23 +0000

    testing
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- pkg/version/channelversion.go
+++ pkg/version/channelversion.go
@@ -12,12 +12,21 @@ type channelVersion struct {
 	channel  string
 	version  string
 	hotpatch *int64
+	fips     bool
 	original string
 }
 
 // hotpatchSuffix is the suffix applied to channel names to indicate that the
 // version string includes a hotpatch number (e.g. dev-0.1.2-3)
 const hotpatchSuffix = "Hotpatch"
+
+// fipsSuffix is the suffix applied to channel names to indicate that the
+// version represents a FIPS-compliant build (e.g. devFIPS: dev-0.1.2-fips)
+const fipsSuffix = "FIPS"
+
+// fipsVersionSuffix is the suffix applied to version string to indicate that
+// the version represents a FIPS-compliant build (e.g. dev-0.1.2-fips)
+const fipsVersionSuffix = "fips"
 
 func (cv channelVersion) String() string {
 	return cv.original
@@ -31,19 +40,29 @@ func (cv channelVersion) updateChannel() string {
 	if cv.hotpatch != nil {
 		return cv.channel + hotpatchSuffix
 	}
+	if cv.fips {
+		return cv.channel + fipsSuffix
+	}
 	return cv.channel
 }
 
-// versionWithHotpatch returns the version string, suffixed with the hotpatch
-// number if it exists.
-func (cv channelVersion) versionWithHotpatch() string {
-	if cv.hotpatch == nil {
-		return cv.version
+// versionWithSuffix returns the version string, suffixed with the hotpatch
+// number or "-fips" if appropriate.
+func (cv channelVersion) versionWithSuffix() string {
+	if cv.hotpatch != nil {
+		return fmt.Sprintf("%s-%d", cv.version, *cv.hotpatch)
 	}
-	return fmt.Sprintf("%s-%d", cv.version, *cv.hotpatch)
+	if cv.fips {
+		return fmt.Sprintf("%s-%s", cv.version, fipsVersionSuffix)
+	}
+	return cv.version
 }
 
-func (cv channelVersion) hotpatchEqual(other channelVersion) bool {
+func (cv channelVersion) suffixEqual(other channelVersion) bool {
+	if cv.fips != other.fips {
+		return false
+	}
+
 	if cv.hotpatch == nil && other.hotpatch == nil {
 		return true
 	}
@@ -56,9 +75,9 @@ func (cv channelVersion) hotpatchEqual(other channelVersion) bool {
 // parseChannelVersion parses a build string into a channelVersion struct. it
 // expects the channel and version to be separated by a hyphen (e.g. dev-0.1.2).
 // the version may additionally include a hotpatch number, which is separated
-// from the base version by another hyphen (e.g. dev-0.1.2-3). if the version is
-// suffixed with any other non-numeric build info strings (e.g. dev-0.1.2-foo),
-// those strings are ignored.
+// from the base version by another hyphen (e.g. dev-0.1.2-3), or "-fips". if
+// the version is suffixed with any other non-numeric build info strings (e.g.
+// dev-0.1.2-foo), those strings are ignored.
 func parseChannelVersion(cv string) (channelVersion, error) {
 	parts := strings.Split(cv, "-")
 	if len(parts) < 2 {
@@ -68,15 +87,20 @@ func parseChannelVersion(cv string) (channelVersion, error) {
 	channel := parts[0]
 	version := parts[1]
 	var hotpatch *int64
+	fips := false
 
 	for _, part := range parts[2:] {
+		if part == fipsVersionSuffix {
+			fips = true
+			break
+		}
 		if i, err := strconv.ParseInt(part, 10, 64); err == nil {
 			hotpatch = &i
 			break
 		}
 	}
 
-	return channelVersion{channel, version, hotpatch, cv}, nil
+	return channelVersion{channel, version, hotpatch, fips, cv}, nil
 }
 
 // IsReleaseChannel returns true if the channel of the version is "edge" or
